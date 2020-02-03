be0a59383d0d9acce899b459714a183a6d8466d0
bvarjavand <bvarjavand@gmail.com>
2020-02-03 15:28:30 -0500

    remove broken imports

--- brainlight/algorithms/__init__.py
+++ brainlight/algorithms/__init__.py
@@ -1,9 +1,5 @@
 import brainlight.algorithms.connect_fragments
 import brainlight.algorithms.generate_fragments
 
-# import brainlight.algorithms.regression
-
 from brainlight.algorithms.connect_fragments import *
 from brainlight.algorithms.generate_fragments import *
-
-# from brainlight.algorithms.regression import *
--- brainlight/preprocessing/__init__.py
+++ brainlight/preprocessing/__init__.py
@@ -1,4 +1,2 @@
-# import brainlight.preprocessing.features
-# from brainlight.preprocessing.features import *
 from brainlight.preprocessing.image_process import *
 from brainlight.preprocessing.preprocess import *
