f45c903126a47ebb1a062dbcfa3df336e5e44d20
bvarjavand <bvarjavand@gmail.com>
2020-02-16 09:26:18 -0500

    try using secret environment variables

--- .aws.sh
+++ .aws.sh
@@ -6,6 +6,6 @@ mkdir -p ~/.cloudvolume/secrets
 cat > ~/.cloudvolume/secrets/aws-secret.json << EOL
 {
 	"AWS_ACCESS_KEY_ID": "AKIARGIZMHKFQLCHAYVA",
-	"AWS_SECRET_ACCESS_KEY": ${AWS}
+	"AWS_SECRET_ACCESS_KEY": ${AWS_SECRET_ACCESS_KEY}
 }
 EOL
