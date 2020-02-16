bda8a8d4432e2b16ab25fc2ec22718a35095cbad
bvarjavand <bvarjavand@gmail.com>
2020-02-16 08:54:40 -0500

    modify travis access to key

--- .aws.sh
+++ .aws.sh
@@ -5,7 +5,7 @@ mkdir -p ~/.cloudvolume/secrets
 
 cat > ~/.cloudvolume/secrets/aws-secret.json << EOL
 {
-	"AWS_ACCESS_KEY_ID": ${AWS_ACCESS_KEY_ID},
-	"AWS_SECRET_ACCESS_KEY": ${AWS_SECRET_ACCESS_KEY}
+	"AWS_ACCESS_KEY_ID":"AKIARGIZMHKFQLCHAYVA",
+	"AWS_SECRET_ACCESS_KEY":${AWS_SECRET_ACCESS_KEY}
 }
 EOL
