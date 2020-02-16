fdccc212ea2b793dee9c84a9065ca46c56729eb0
bvarjavand <bvarjavand@gmail.com>
2020-02-16 08:47:08 -0500

    correct json structure

--- .aws.sh
+++ .aws.sh
@@ -4,7 +4,8 @@ mkdir -p ~/.cloudvolume
 mkdir -p ~/.cloudvolume/secrets
 
 cat > ~/.cloudvolume/secrets/aws-secret.json << EOL
-[default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+{
+	"AWS_ACCESS_KEY_ID": ${AWS_ACCESS_KEY_ID},
+	"AWS_SECRET_ACCESS_KEY": ${AWS_SECRET_ACCESS_KEY}
+}
 EOL
