78eeabdb3932ca477a78edd4cc4a4b7c47fb2bbd
sanika1201 <55053050+sanika1201@users.noreply.github.com>
2020-03-23 11:12:24 -0400

    Code clean up for changed functions
    Modified the MLP_LR_NN function parameters

--- tests/test_log_regression.py
+++ tests/test_log_regression.py
@@ -14,9 +14,10 @@ y_all = df_iter.iloc[:, 63:]
 X_all = StandardScaler().fit_transform(X_all)
 X_sel, X_test, y_sel, y_test = \
         train_test_split(X_all, y_all, test_size=40,random_state=42)
+n_features = X_sel.shape[1]
 classifiers = [MLPClassifier(hidden_layer_sizes = 4,activation = 'logistic', alpha=1, max_iter=1000),
                LogisticRegression(max_iter=2000),
-               MLP_LR_NN(X_sel,y_sel)]
+               MLP_LR_NN(X_sel,y_sel,n_features)]
 names = {"MLP-LR": "black", "LR":"blue", "MLP-relu-LR":'red'}
 
 def test_run_classifiers():
