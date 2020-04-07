6940f524783c82ddcecd62c7bb538a534ca85279
Jenny Trieu <jennytrieu10@gmail.com>
2020-04-07 18:45:43 -0400

    added two new functions to visualize

--- brainlit/viz/visualize.py
+++ brainlit/viz/visualize.py
@@ -1,5 +1,114 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import SimpleITK as sitk
+import napari
+​
+​
+def plot_2d(img, title=None, margin=0.05, dpi=80):
+	    """
+        Plots a 2D image returned from a ngl pipeline
+
+        Parameters
+        ----------
+        img :
+
+        titles : str, default = None
+        
+        margin : int, default = 0.05 
+
+        dpi : int, default = 80
+
+        Returns
+        -------
+        matplotlib figure and axis
+     """
+
+    nda = sitk.GetArrayFromImage(img)
+    spacing = img.GetSpacing()
+​
+    if nda.ndim == 3:
+        c = nda.shape[-1]
+​
+        if c not in (3, 4):
+            nda = nda[nda.shape[0] // 2, :, :]
+​
+    elif nda.ndim == 4:
+        c = nda.shape[-1]
+​
+        if c not in (3, 4):
+            raise RuntimeError("Unable to show 3D-vector Image")
+​
+        nda = nda[nda.shape[0] // 2, :, :, :]
+​
+    xsize = nda.shape[1] * 2
+    ysize = nda.shape[0] * 2
+​
+    figsize = (1 + margin) * xsize / dpi, (1 + margin) * ysize / dpi
+​
+    fig = plt.figure(figsize=figsize, dpi=dpi, tight_layout=True)
+    ax = plt.gca()
+​
+    extent = (0, xsize * spacing[0], ysize * spacing[1], 0)
+​
+    t = ax.imshow(nda, extent=extent, interpolation=None)
+​
+    if nda.ndim == 2:
+        t.set_cmap("gray")
+​
+    if title:
+        plt.title(title)
+​
+    plt.show()
+
+    return ax, fig
+​
+​
+def plot_3d(img, xslices=[], yslices=[], zslices=[], title=None, margin=0.05, dpi=80):
+    img_xslices = [img[s, :, :] for s in xslices]
+    img_yslices = [img[:, s, :] for s in yslices]
+    img_zslices = [img[:, :, s] for s in zslices]
+​
+    maxlen = max(len(img_xslices), len(img_yslices), len(img_zslices))
+​
+    img_null = sitk.Image([0, 0], img.GetPixelID(), img.GetNumberOfComponentsPerPixel())
+​
+    img_slices = []
+    d = 0
+​
+    if len(img_xslices):
+        img_slices += img_xslices + [img_null] * (maxlen - len(img_xslices))
+        d += 1
+​
+    if len(img_yslices):
+        img_slices += img_yslices + [img_null] * (maxlen - len(img_yslices))
+        d += 1
+​
+    if len(img_zslices):
+        img_slices += img_zslices + [img_null] * (maxlen - len(img_zslices))
+        d += 1
+​
+    if maxlen != 0:
+        if img.GetNumberOfComponentsPerPixel() == 1:
+            img = sitk.Tile(img_slices, [maxlen, d])
+        else:
+            img_comps = []
+            for i in range(0, img.GetNumberOfComponentsPerPixel()):
+                img_slices_c = [sitk.VectorIndexSelectionCast(s, i) for s in img_slices]
+                img_comps.append(sitk.Tile(img_slices_c, [maxlen, d]))
+            img = sitk.Compose(img_comps)
+​
+    fig, ax = plot_2d(img, title, margin, dpi)
+
+    return fig, ax
+​
+​
+def napari_viewer(img, labels=None, shapes=None, label_name="Segmentation"):
+    viewer = napari.view_image(np.squeeze(np.array(img)))
+    if labels is not None:
+        viewer.add_labels(labels, name=label_name)
+    if shapes is not None:
+        viewer.add_shapes(data=shapes, shape_type='path', edge_color='blue', name='Skeleton')
+    return viewer
 
 
 def plot_image_2d(imgs, titles=None, rows=1, colorbar=False):
