0985a28a1b0823f092d9e6a423ebd0358283d050
Jenny Trieu <jennytrieu10@gmail.com>
2020-04-11 23:15:30 -0400

    Added new plot3d, plot2d functions to Visualize

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -1,12 +1,40 @@
+import numpy as np
+
 import brainlit
 from brainlit.utils.ngl_pipeline import NeuroglancerSession
+from brainlit.viz.swc import *
 from brainlit.viz import visualize
 import matplotlib
+import SimpleITK as sitk
+import napari
+
 
 ngl_sess = NeuroglancerSession(mip=1)
 img, bbbox, vox = ngl_sess.pull_chunk(2, 300, 1, 1, 1)
 img_slices = [img[:, 100, :], img[:, 99, :], img[:, 97, :], img[:, 96, :]]
 img_list = [img, img]
+
+
+def test_fig_plot_3d():
+    """test if fig output is correct type (matplotlib figure)"""
+    url = "s3://mouse-light-viz/precomputed_volumes/brain1"
+    seg_id = 11
+    mip = 2
+    df = read_s3(url + "_segments", seg_id, mip)
+
+    df['sample'].size  # the number of vertex IDs [1, 2, ..., df['sample'].size]
+    subneuron_df = df[0:5]  # choose vertices to use for the subneuron
+    vertex_list = subneuron_df['sample'].array
+    ngl = NeuroglancerSession(url, mip=mip)
+    buffer = [10, 10, 10]
+    img, bounds, vox_in_img_list = ngl.pull_vertex_list(seg_id, vertex_list, buffer=buffer, expand=True)
+
+    napari.gui_qt()
+
+    fig_2d, axis_2d = visualize.plot_3d(sitk.GetImageFromArray(np.squeeze(img), isVector=False), zslices=range(48, 53),
+                                        title="Downloaded Mouselight Volume", show_plot=False)
+    assert isinstance(fig_2d, matplotlib.figure.Figure)
+    assert isinstance(axis_2d, matplotlib.axes._subplots.Subplot)
 
 
 def test_fig_plot_image_2d_given_one():
