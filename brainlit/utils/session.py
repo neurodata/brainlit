af67d308a1453dac5ba3447d7ea28f27aa18b9fb
bvarjavand <bvarjavand@gmail.com>
2020-08-07 16:58:45 -0700

    add session docscrtings, tests, and extra functionality

--- brainlit/utils/session.py
+++ brainlit/utils/session.py
@@ -8,165 +8,182 @@ from cloudvolume.lib import Bbox
 from pathlib import Path
 from .swc import read_s3, df_to_graph, get_sub_neuron
 import napari
-from typing import Optional
+import networkx as nx
+from typing import Optional, List, Union, Tuple
+from .util import (
+    check_type,
+    check_size,
+    check_precomputed,
+    check_iterable_type,
+    check_iterable_nonnegative,
+)
+
+Bounds = Union[Bbox, Tuple[int, int, int, int, int, int]]
 
 
 class NeuroglancerSession:
-    """
-    Utility class which pulls and pushes data.
+    """Utility class which pulls and pushes data.
 
     Arguments:
-        url : Precompued path either to a file URI or url URI. Defaults to mouselight brain 1.
-        mip : Resolution level to pull and push data at. 0 is the highest resolution.
-        url_segments : Precomputed path to segmentation data.
+        url: Precompued path either to a file URI or url URI. Defaults to mouselight brain1_2.
+        mip: Resolution level to pull and push data at. Defaults to 0, the highest resolution.
+        url_segments: Precomputed path to segmentation data.
 
-    Attributes
-    ----------
-    url : string
-        URL of the s3 bucket to pull from and push to.
-
-    cv : CloudVolume object
-        CloudVolume object instantiated with the specified URL.
-
-    mip : int
-        Resolution level.
-
-    chunk_size : list
-        The chunk size of the volume at the specified mip, given as [x, y, z].
-
-    scales : list
-        The resolution of the volume at the specified mip, given as [x, y, z].
+    Attributes:
+        url: CloudVolumePrecomputedPath to image data.
+        url_segments: CloudVolumePrecomputedPath to segmentation data. Optional, default None.
+            Automatically tries precomputed path url+"_segments".
+        cv (CloudVolumePrecomputed): CloudVolume object for image data.
+        cv_segments (CloudVolumePrecomputed): CloudVolume object for segmentation data. Optional, default None.
+        mip: Resolution level.
+        chunk_size: The chunk size of the volume at the specified mip, given as (x, y, z).
+        scales: The resolution of the volume at the specified mip, given as (x, y, z).
     """
 
     def __init__(
         self,
-        url: str = "s3://mouse-light-viz/precomputed_volumes/brain1",
-        mip: int = 1,
+        url: str = "s3://mouse-light-viz/precomputed_volumes/brain1_2",
+        mip: int = 0,
         url_segments: Optional[str] = None,
     ):
+        check_precomputed(url)
+        check_type(mip, int)
         self.url = url
-        self.cv = CloudVolume(self.url, parallel=False)
+        self.cv = CloudVolume(url, parallel=False)
+        if mip < 0 or mip >= len(self.cv.scales):
+            raise ValueError(f"{mip} should be between 0 and {len(self.cv.scales)}.")
         self.mip = mip
-        self.chunk_size = self.cv.info["scales"][self.mip]["chunk_sizes"][0]
+        self.chunk_size = self.cv.scales[self.mip]["chunk_sizes"][0]
         self.scales = self.cv.scales[self.mip]["resolution"]
 
-        if url_segments == None:
+        self.url_segments = url_segments
+        if url_segments is None:
             try:  # default is to add _segments
-                self.cv_segments = CloudVolume(self.url + "_segments", parallel=False)
+                self.cv_segments = CloudVolume(url + "_segments", parallel=False)
+                self.url_segments = url + "_segments"
             except Error as e:
                 print(e)
+                raise Warning(
+                    f"Segmentation volume not found at {self.url_segments}, defaulting to None."
+                )
                 self.cv_segments = None
         else:
-            self.cv_segments = None
+            check_precomputed(url_segments)
+            self.cv_segments = CloudVolume(url_segments, parallel=False)
 
-    def _get_voxel(self, seg_id, v_id):
+    def _get_voxel(self, seg_id: int, v_id: int) -> Tuple[int, int, int]:
+        """Gets coordinates of segment vertex, in voxel space.
+
+        Arguments:
+            seg_id: The id of the segment to use.
+            v_id: The id of the vertex to use from the given segment.
+
+        Returns:
+            voxel: The voxel coordinates in (x, y, z) voxel space.
+        """
+        check_type(seg_id, int)
+        check_type(v_id, int)
         if self.cv_segments is None:
             raise ValueError("Cannot get voxel without segmentation data")
-        else:
-            vertex = self.cv_segments.skeleton.get(seg_id).vertices[v_id]
-            voxel = np.round(
-                np.divide(vertex, self.cv_segments.scales[self.mip]["resolution"])
-            ).astype(int)
+        seg = self.cv_segments.skeleton.get(seg_id).vertices
+        if v_id < 0 or v_id >= len(seg):
+            raise ValueError(f"{v_id} should be between 0 and {len(seg)}.")
+
+        vertex = seg[v_id]
+        voxel = np.round(
+            np.divide(vertex, self.cv_segments.scales[self.mip]["resolution"])
+        ).astype(int)
         return voxel
 
-    def set_url_segment(self, seg_url):
+    def set_url_segments(self, seg_url: str):
         """Sets the url_segments and cv_segments attributes.
+
+        Arguments:
+            seg_url: CloudvolumePrecomputedPath to segmentation data.
         """
+        check_precomputed(seg_url)
+
         self.url_segments = seg_url
         self.cv_segments = CloudVolume(self.url_segments, parallel=False)
 
-    def get_segments(self, seg_id, bbox):
-        """
-        Get a graph of points within a bounding box.
+    def get_segments(self, seg_id: int, bbox: Optional[Bounds] = None) -> nx.Graph:
+        """Get a graph of a segmentation annotation within a bounding box.
 
-        Parameters
-        ----------
-        seg_id : int
-            The segement number to pull
-        bbox : :object: Bounding box
-            The bounding box object
+        Arguments:
+            seg_id  The segement to pull.
+            bbox: The bounding box object, default None. If None, uses entire volume.
 
-        Returns
-        -------
-        G : :class:`networkx.Graph`
+        Returns:
+            G: A subgraph from the specified segment and bounding box.
         """
+        check_type(seg_id, int)
+        if self.cv_segments is None:
+            raise ValueError("Cannot get segments without segmentation data.")
+
         df = read_s3(self.url_segments, seg_id, self.mip)
         G = df_to_graph(df)
-        box = bbox.to_list()
-        G_sub = get_sub_neuron(G, [box[:3], box[3:]])
-        return G_sub
+        if bbox is not None:
+            if isinstance(bbox, Bbox):
+                bbox = bbox.to_list()
+            check_iterable_type(bbox, int)
+            check_iterable_nonnegative(bbox)
+            G = get_sub_neuron(G, [bbox[:3], bbox[3:]])
+        return G
 
-    def pull_voxel(self, seg_id, v_id, nx=1, ny=1, nz=1):
+    def pull_voxel(
+        self, seg_id: int, v_id: int, radius: int = 1
+    ) -> Tuple[np.ndarray, Bbox, np.ndarray]:
+        """Pull a number of voxels around a specified skeleton vertex.
+
+        Arguments:
+            seg_id: ID of the segment to use, depends on data in s3.
+            v_id: ID of the vertex to use, depends on the segment.
+            radius: Radius of pulled volume around central voxel, in voxels. 
+                Optional, default is 1 (3x3 volume is pulled, centered at the vertex).
+
+        Returns:
+            img: A 2*nx+1 X 2*ny+1 X 2*nz+1 volume.
+            bounds: Bounding box object which contains the bounds of the volume.
+            vox_in_img: List of coordinates which locate the initial point in the volume.
         """
-        Pull a number of voxels around a specified skeleton vertex
+        check_type(radius, int)
+        if radius < 0:
+            raise ValueError(f"{radius} should be nonnegative.")
 
-        Parameters
-        ----------
-        seg_id : int
-            ID of the segment to use, depends on data in s3.
-
-        v_id : int
-            ID of the vertex to use, depends on the segment.
-
-        nx : int, optional (default=1)
-            Number of voxels to pull on either side of the seed in x.
-
-        ny : int, optional (default=1)
-            Number of voxels to pull on either side of the seed in y.
-
-        nz : int, optional (default=1)
-            Number of voxels to pull on either side of the seed in z.
-
-        Returns
-        -------
-        img : ndarray
-            A 2*nx+1 X 2*ny+1 X 2*nz+1 volume.
-
-        bounds : Bbox object
-            Bounding box object which contains the bounds of the volume.
-
-        vox_in_img : ndarray
-            List of coordinates which locate the initial point in the volume.
-        """
-        voxel = self._get_voxel(seg_id, v_id)
+        voxel = self._get_voxel(seg_id, v_id)  # does type checking for seg_id and v_id
         bounds = Bbox(voxel, voxel)
         seed = bounds.to_list()
-        shape = [nx, ny, nz]
+        shape = [radius] * 3
         bounds = Bbox(np.subtract(seed[:3], shape), np.add(np.add(seed[3:], shape), 1))
         img = self.cv.download(bounds, mip=self.mip)
         vox_in_img = voxel - np.array(bounds.to_list()[:3])
         return np.squeeze(np.array(img)), bounds, vox_in_img
 
-    def pull_vertex_list(self, seg_id, v_id_list, buffer=[0, 0, 0], expand=False):
+    def pull_vertex_list(
+        self, seg_id: int, v_id_list: List[int], buffer: int = 0, expand: bool = False,
+    ) -> Tuple[np.ndarray, Bbox, List[Tuple[int, int, int]]]:
+        """Pull a region containing all listed vertices.
+
+        Arguments:
+            seg_id: ID of the segment to use, depends on data in s3.
+            v_id_list: list of vertex IDs to use.
+            buffer: Buffer around the bounding box of seed vertices (on lower and higher bound).
+            expand: Flag whether to expand region to closest set of chunks.
+
+        Returns:
+            img: The image volume containing all vertices.
+            bounds: Bounding box object which contains the bounds of the volume.
+            vox_in_img_list: List of coordinates which locate the vertices in the volume.
         """
-        Pull a region containing all listed vertices.
+        check_type(seg_id, int)
+        check_iterable_type(v_id_list, (int, np.integer))
+        check_type(buffer, int)
+        if buffer < 0:
+            raise ValueError(f"Buffer {buffer} shouild not be negative.")
+        check_type(expand, bool)
 
-        Parameters
-        ----------
-        seg_id : int
-            ID of the segment to use, depends on data in s3.
-
-        v_id_list : list of ints
-            list of vertex IDs to use.
-
-        buffer : list of ints, optional (default=[0, 0, 0])
-            Buffer around the bounding box of seed vertices (on lower and higher bound).
-
-        expand : bool, optional (default=False)
-            Flag whether to expand region to closest combination of chunks.
-
-        Returns
-        -------
-        img : ndarray
-            The image volume containing all vertices.
-
-        bounds : Bbox object
-            Bounding box object which contains the bounds of the volume.
-
-        vox_in_img_list : ndarray, shape nx3
-            List of coordinates which locate the vertices in the volume.
-        """
         voxel_list = np.array([self._get_voxel(seg_id, i) for i in v_id_list])
+        buffer = [buffer, buffer, buffer]
         lower = list(np.min(voxel_list, axis=0) - buffer)
         higher = list(np.max(voxel_list, axis=0) + buffer)
         bounds = Bbox(lower, higher)
@@ -177,103 +194,90 @@ class NeuroglancerSession:
         vox_in_img_list = voxel_list - lower
         return img, bounds, vox_in_img_list
 
-    def pull_chunk(self, seg_id, v_id, nx=0, ny=0, nz=0):
+    def pull_chunk(
+        self, seg_id: int, v_id: int, radius: int = 0
+    ) -> Tuple[np.ndarray, Bbox, Tuple[int, int, int]]:
+        """Pull a number of chunks around a specified skeleton vertex.
+
+        Arguments:
+            seg_id: ID of the segment to use, depends on data in s3.
+            v_id: ID of the vertex to use, depends on the segment.
+            radius: Radius of pulled volume around central chunk, in chunks. 
+                Optional, default is 0 (single chunk which contains the voxel).
+
+        Returns:
+            img: A chunk_size[0]*2*nx X chunk_size[1]*2*ny X chunk_size[2]*2*nz volume.
+            bounds: Bounding box object which contains the bounds of the volume.
+            vox_in_img: List of coordinates which locate the initial point in the volume.
         """
-        Pull a number of chunks around a specified skeleton vertex
+        check_type(seg_id, int)
+        check_type(v_id, int)
+        check_type(radius, int)
+        if radius < 0:
+            raise ValueError(f"Radius of {radius} should be nonnegative.")
 
-        Parameters
-        ----------
-        seg_id : int
-            ID of the segment to use, depends on data in s3.
-
-        v_id : int
-            ID of the vertex to use, depends on the segment.
-
-        nx : int, optional (default=0)
-            Number of chunks to pull on either side of the main chunk in x.
-
-        ny : int, optional (default=0)
-            Number of chunks to pull on either side of the main chunk in y.
-
-        nz : int, optional (default=0)
-            Number of chunks to pull on either side of the main chunk in z.
-
-        Returns
-        -------
-        img : ndarray
-            A chunk_size[0]*2*nx X chunk_size[1]*2*ny X chunk_size[2]*2*nz
-            volume.
-
-        bounds : Bbox object
-            Bounding box object which contains the bounds of the volume.
-
-        vox_in_img : ndarray
-            List of coordinates which locate the initial point in the volume.
-        """
         voxel = self._get_voxel(seg_id, v_id)
         bounds = Bbox(voxel, voxel).expand_to_chunk_size(self.chunk_size)
         seed = bounds.to_list()
         shape = [
-            self.chunk_size[0] * nx,
-            self.chunk_size[1] * ny,
-            self.chunk_size[2] * nz,
+            self.chunk_size[0] * radius,
+            self.chunk_size[1] * radius,
+            self.chunk_size[2] * radius,
         ]
         bounds = Bbox(np.subtract(seed[:3], shape), np.add(seed[3:], shape))
         img = self.cv.download(bounds, mip=self.mip)
         vox_in_img = voxel - np.array(bounds.to_list()[:3])
         return np.squeeze(np.array(img)), bounds, vox_in_img
 
-    def pull_bounds_img(self, bounds):
-        """
-        Pull a volume around a specified bounding box. Works on img channels.
+    def pull_bounds_img(self, bounds: Bounds) -> np.ndarray:
+        """Pull a volume around a specified bounding box. Works on image channels.
 
-        Parameters
-        ----------
-        bounds : Bbox object
-            Tuple containing (x0, y0, z0, x1, y1, z1) bounds
+        Arguments:
+            bounds: Bounding box, or tuple containing (x0, y0, z0, x1, y1, z1) bounds.
 
-        Returns
-        -------
-        img : ndarray
-            pulled volume
+        Returns:
+            img: Volume pulled according to the bounding box.
         """
+        if isinstance(bounds, Bbox):
+            bounds = bounds.to_list()
+        check_iterable_type(bounds, (int, np.integer))
+        check_iterable_nonnegative(bounds)
+
         img = self.cv.download(bounds, mip=self.mip)
         return np.squeeze(np.array(img))
 
-    def pull_bounds_seg(self, bounds):
-        """
-        Pull a volume around a specified bounding box.
+    def pull_bounds_seg(self, bounds: Bounds) -> np.ndarray:
+        """Pull a volume around a specified bounding box.
         Works on annotation channels.
 
-        Parameters
-        ----------
-        bounds : Bbox object
-            Tuple containing (x0, y0, z0, x1, y1, z1) bounds
+        Arguments:
+            bounds: Bounding box, or tuple containing (x0, y0, z0, x1, y1, z1) bounds.
 
-        Returns
-        -------
-        img : ndarray
-            pulled volume
+        Returns:
+            img: Volume pulled according to the bounding box.
         """
+        if isinstance(bounds, Bbox):
+            bounds = bounds.to_list()
+        check_iterable_type(bounds, int)
+        check_iterable_nonnegative(bounds)
+
         img = self.cv[bounds]
         return np.squeeze(np.array(img))
 
-    def push(self, img, bounds):
-        """
-        Push a volume.
+    def push(self, img: np.ndarray, bounds: Bounds):
+        """Push a volume.
 
-        Parameters
-        ----------
-        img : ndarray
-            Volume to push
-
-        bounds : Bbox object
-            Tuple containing (x0, y0, z0, x1, y1, z1) bounds
+        Arguments:
+            img : Volume to push
+            bounds : Bounding box or tuple containing (x0, y0, z0, x1, y1, z1) bounds.
         """
+        if not isinstance(img, np.ndarray):
+            raise TypeError(f"Image should be numpy array..")
+        if (img == 0).all():
+            raise ValueError(f"Should not push an empty volume of all 0.")
+        if isinstance(bounds, Bbox):
+            bounds = bounds.to_list()
+        check_iterable_type(bounds, int)
+        check_iterable_nonnegative(bounds)
+
         self.cv[bounds] = img.astype("uint64")
-
-    def napari_viewer(self, img, labels=None, label_name="Segmentation"):
-        viewer = napari.view_image(np.squeeze(np.array(img)))
-        if labels is not None:
-            viewer.add_labels(labels, name=label_name)
-        return viewer
