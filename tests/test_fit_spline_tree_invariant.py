30635b6df42bd1a46c862f22dd3f523954d7dace
Felix Chen <chih.ting1992tw@gmail.com>
2020-10-17 23:49:01 -0400

    add two tests
    1. test_closed_shape
    2. test_multiple_roots

--- tests/test_fit_spline_tree_invariant.py
+++ tests/test_fit_spline_tree_invariant.py
@@ -1,11 +1,12 @@
 import pytest
-from brainlit.algorithms.connect_fragments.fit_spline import GeometricGraph
+from brainlit.algorithms.connect_fragments import fit_spline
 import networkx as nx
-from pathlib import Path
-from brainlit.utils.swc import read_swc_offset
-import numpy as np
 
-def test_fit_spline_tree_invariant()
+
+def test_closed_shape()
+'''
+tree should not include closed shapes
+'''
 G=GeometricGraph()
 G.add_node(1)
 G.add_node(2)
@@ -13,3 +14,18 @@ G.add_node(3)
 G.add_edge(1,2)
 G.add_edge(2,3)
 G.add_edge(3,1)
+
+def test_multiple_roots()
+'''
+only allow geometric graph with one root
+'''
+G=GeometricGraph()
+G.add_node(1)
+G.add_node(2)
+G.add_node(3)
+G.add_node(4)
+G.add_node(5)
+G.add_edge(1,2)
+G.add_edge(1,3)
+G.add_edge(3,5)
+G.add_edge(4,3)
