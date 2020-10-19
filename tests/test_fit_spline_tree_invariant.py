9f54d845b9151bd235a1a2e573e0b7caf4c5702b
Felix Chen <chih.ting1992tw@gmail.com>
2020-10-19 10:24:46 -0400

    fix indentation error

--- tests/test_fit_spline_tree_invariant.py
+++ tests/test_fit_spline_tree_invariant.py
@@ -1,31 +1,31 @@
 import pytest
-from brainlit.algorithms.connect_fragments import fit_spline
+from brainlit.algorithms.connect_fragments.fit_spline import GeometricGraph
 import networkx as nx
 
 
-def test_closed_shape()
-'''
-tree should not include closed shapes
-'''
-G=GeometricGraph()
-G.add_node(1)
-G.add_node(2)
-G.add_node(3)
-G.add_edge(1,2)
-G.add_edge(2,3)
-G.add_edge(3,1)
+def test_closed_shape():
+    '''
+    tree should not include closed shapes
+    '''
+    G=GeometricGraph()
+    G.add_node(1)
+    G.add_node(2)
+    G.add_node(3)
+    G.add_edge(1,2)
+    G.add_edge(2,3)
+    G.add_edge(3,1)
 
-def test_multiple_roots()
-'''
-only allow geometric graph to have one root
-'''
-G=GeometricGraph()
-G.add_node(1)
-G.add_node(2)
-G.add_node(3)
-G.add_node(4)
-G.add_node(5)
-G.add_edge(1,2)
-G.add_edge(1,3)
-G.add_edge(3,5)
-G.add_edge(4,3)
+def test_multiple_roots():
+    '''
+    only allow geometric graph to have one root
+    '''
+    G=GeometricGraph()
+    G.add_node(1)
+    G.add_node(2)
+    G.add_node(3)
+    G.add_node(4)
+    G.add_node(5)
+    G.add_edge(1,2)
+    G.add_edge(1,3)
+    G.add_edge(3,5)
+    G.add_edge(4,3)
