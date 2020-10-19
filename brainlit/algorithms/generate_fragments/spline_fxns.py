c0d03da1dfa03d56a47a465f20485c7f1829bb91
Jacopo Teneggi <j.teneggi@jetop.com>
2020-10-18 20:34:02 -0400

    Add tutorial

--- brainlit/algorithms/generate_fragments/spline_fxns.py
+++ brainlit/algorithms/generate_fragments/spline_fxns.py
@@ -11,22 +11,93 @@ from brainlit.utils.util import (
 )
 
 
-# def speed(x, tck):
-#     derivs = []
-#     for coord in tck[1]:
-#         tck_coord = list(tck)
-#         tck_coord[1] = coord
-#         tck_coord = tuple(tck_coord)
+def speed(
+    x: np.ndarray,
+    t: np.ndarray,
+    c: np.ndarray,
+    k: np.integer,
+    aux_outputs: bool = False,
+) -> np.ndarray:
+    """Compute the speed of a B-Spline.
 
-#         derivs.append(splev_deriv(x, tck_coord))
+    The speed is the norm of the first derivative of the B-Spline
 
-#     deriv = np.stack(derivs, axis=1)
+    Arguments:
+        x: A 1xL array of parameter values where to evaluate the curve
+        t: A 1xm array representing the knots of the B-spline
+        c: A dxn array representing the control points of the B-spline
+        k: An integer representing the degree of the B-spline:
+        
+    Returns:
+        speed: A 1xL array containing the speed of the B-Spline evaluated at x
+    """
 
-#     speed = np.linalg.norm(deriv, axis=1)
-#     return speed
+    # convert arguments to desired type
+    x = np.ascontiguousarray(x)
+    t = np.ascontiguousarray(t)
+    c = np.ascontiguousarray(c)
+    k = operator.index(k)
+
+    if k < 0:
+        raise ValueError("The order of the spline must be non-negative")
+
+    check_type(t, np.ndarray)
+    t_dim = t.ndim
+    if t_dim != 1:
+        raise ValueError("t must be one-dimensional")
+    if len(t) == 0:
+        raise ValueError("t must be non-empty")
+    check_iterable_type(t, (np.integer, np.float))
+    if (np.diff(t) < 0).any():
+        raise ValueError("t must be a non-decreasing sequence")
+
+    check_type(c, np.ndarray)
+    c_dim = c.ndim
+    if c_dim > 2:
+        raise ValueError("c must be 2D max")
+    if len(c.flatten()) == 0:
+        raise ValueError("c must be non-empty")
+    if c_dim == 1:
+        check_iterable_type(c, (np.integer, np.float))
+        # expand dims so that we can cycle through a single dimension
+        c = np.expand_dims(c, axis=0)
+    if c_dim == 2:
+        for d in c:
+            check_iterable_type(d, (np.integer, np.float))
+    n_dim = len(c)
+
+    check_type(x, np.ndarray)
+    x_dim = x.ndim
+    if x_dim != 1:
+        raise ValueError("x must be one-dimensional")
+    if len(x) == 0:
+        raise ValueError("x must be non-empty")
+    check_iterable_type(x, (np.integer, np.float))
+    L = len(x)
+
+    # evaluate first and second derivatives
+    # deriv, dderiv are (d, L) arrays
+    deriv = np.empty((n_dim, L))
+    for i, dim in enumerate(c):
+        spl = BSpline(t, dim, k)
+        deriv[i, :] = spl.derivative(nu=1)(x) if k - 1 >= 0 else np.zeros(L)
+    # tranpose deriv
+    deriv = deriv.T
+
+    speed = np.linalg.norm(deriv, axis=1)
+    if aux_outputs == False:
+        return speed
+    else:
+        return speed, deriv
 
 
-def curvature(x: np.ndarray, t: np.ndarray, c: np.ndarray, k: np.integer) -> np.ndarray:
+def curvature(
+    x: np.ndarray,
+    t: np.ndarray,
+    c: np.ndarray,
+    k: np.integer,
+    aux_outputs: bool = False,
+) -> np.ndarray:
     """Compute the curvature of a B-Spline.
 
     The curvature measures the failure of a curve to be a straight line. 
@@ -103,7 +174,7 @@ def curvature(x: np.ndarray, t: np.ndarray, c: np.ndarray, k: np.integer) -> np.
         spl = BSpline(t, dim, k)
         deriv[i, :] = spl.derivative(nu=1)(x) if k - 1 >= 0 else np.zeros(L)
         dderiv[i, :] = spl.derivative(nu=2)(x) if k - 2 >= 0 else np.zeros(L)
-    # transpose deriv, dderiv
+    # transpose derivs
     deriv = deriv.T
     dderiv = dderiv.T
     # evaluate the cross product
@@ -116,32 +187,110 @@ def curvature(x: np.ndarray, t: np.ndarray, c: np.ndarray, k: np.integer) -> np.
     if np.isnan(np.sum(curvature)):
         print("torsion nan")
 
-    return curvature
+    if aux_outputs == True:
+        return curvature, deriv, dderiv
+    else:
+        return curvature
 
 
-# def torsion(x, tck):
-#     derivs = []
-#     deriv2s = []
-#     deriv3s = []
-#     for coord in tck[1]:
-#         tck_coord = list(tck)
-#         tck_coord[1] = coord
-#         tck_coord = tuple(tck_coord)
+def torsion(
+    x: np.ndarray,
+    t: np.ndarray,
+    c: np.ndarray,
+    k: np.integer,
+    aux_outputs: bool = False,
+) -> np.ndarray:
+    """Compute the torsion of a B-Spline.
 
-#         derivs.append(splev_deriv(x, tck_coord))
-#         deriv2s.append(splev_deriv2(x, tck_coord))
-#         deriv3s.append(splev_deriv3(x, tck_coord))
+    The torsion measures the failure of a line to be planar.
+    If the curvature k of a curve is not zero, then the torsion is defined as
 
-#     deriv = np.stack(derivs, axis=1)
-#     deriv2 = np.stack(deriv2s, axis=1)
-#     deriv3 = np.stack(deriv3s, axis=1)
+    \tau = -n * b',
 
-#     cross = np.cross(deriv, deriv2)
+    where n is the principal normal vector, and b' the derivative w.r.t. the
+    arc length s of the binormal vector.
 
-#     # Could be more efficient by only computing dot products of corresponding rows
-#     num = np.diag((cross @ deriv3.T))
-#     denom = np.linalg.norm(cross, axis=1) ** 2
+    The torsion can also be computed as
 
-#     torsion = np.nan_to_num(num / denom)
+    \tau = |r'(t), r''(t), r'''(t)|/||r'(t) x r''(t)||^2.
 
-#     return torsion
+    Arguments:
+        x: A 1xL array of parameter values where to evaluate the curve
+        t: A 1xm array representing the knots of the B-spline
+        c: A dxn array representing the control points of the B-spline
+        k: An integer representing the degree of the B-spline:
+        
+    Returns:
+        torsion: A 1xL array containing the torsion of the B-Spline evaluated at x
+    """
+
+    # convert arguments to desired type
+    x = np.ascontiguousarray(x)
+    t = np.ascontiguousarray(t)
+    c = np.ascontiguousarray(c)
+    k = operator.index(k)
+
+    if k < 0:
+        raise ValueError("The order of the spline must be non-negative")
+
+    check_type(t, np.ndarray)
+    t_dim = t.ndim
+    if t_dim != 1:
+        raise ValueError("t must be one-dimensional")
+    if len(t) == 0:
+        raise ValueError("t must be non-empty")
+    check_iterable_type(t, (np.integer, np.float))
+    if (np.diff(t) < 0).any():
+        raise ValueError("t must be a non-decreasing sequence")
+
+    check_type(c, np.ndarray)
+    c_dim = c.ndim
+    if c_dim > 2:
+        raise ValueError("c must be 2D max")
+    if len(c.flatten()) == 0:
+        raise ValueError("c must be non-empty")
+    if c_dim == 1:
+        check_iterable_type(c, (np.integer, np.float))
+        # expand dims so that we can cycle through a single dimension
+        c = np.expand_dims(c, axis=0)
+    if c_dim == 2:
+        for d in c:
+            check_iterable_type(d, (np.integer, np.float))
+    n_dim = len(c)
+
+    check_type(x, np.ndarray)
+    x_dim = x.ndim
+    if x_dim != 1:
+        raise ValueError("x must be one-dimensional")
+    if len(x) == 0:
+        raise ValueError("x must be non-empty")
+    check_iterable_type(x, (np.integer, np.float))
+    L = len(x)
+
+    # evaluate first, second, and third derivatives
+    # deriv, dderiv, ddderiv are (d, L) arrays
+    deriv = np.empty((n_dim, L))
+    dderiv = np.empty((n_dim, L))
+    ddderiv = np.empty((n_dim, L))
+    for i, dim in enumerate(c):
+        spl = BSpline(t, dim, k)
+        deriv[i, :] = spl.derivative(nu=1)(x) if k - 1 >= 0 else np.zeros(L)
+        dderiv[i, :] = spl.derivative(nu=2)(x) if k - 2 >= 0 else np.zeros(L)
+        ddderiv[i, :] = spl.derivative(nu=3)(x) if k - 3 >= 0 else np.zeros(L)
+    # transpose derivs
+    deriv = deriv.T
+    dderiv = dderiv.T
+    ddderiv = ddderiv.T
+
+    cross = np.cross(deriv, dderiv)
+
+    # Could be more efficient by only computing dot products of corresponding rows
+    num = np.diag((cross @ ddderiv.T))
+    denom = np.linalg.norm(cross, axis=1) ** 2
+
+    torsion = np.nan_to_num(num / denom)
+
+    if aux_outputs == True:
+        return torsion, deriv, dderiv, ddderiv
+    else:
+        return torsion
--- tests/test_spline_fxns.py
+++ tests/test_spline_fxns.py
@@ -101,10 +101,10 @@ def test_curvature():
     X = xx ** 3
     Y = np.sin(xx)
     Z = xx ** 2
-    dX = 2 * xx ** 2
+    dX = 3 * xx ** 2
     dY = np.cos(xx)
     dZ = 2 * xx
-    ddX = 4 * xx
+    ddX = 6 * xx
     ddY = -np.sin(xx)
     ddZ = 2 * np.ones(len(xx))
 
@@ -116,7 +116,7 @@ def test_curvature():
     t = tck[0]
     c = tck[1]
     k = tck[2]
-    curvature = spline_fxns.curvature(xx, t, c, k)
+    curvature = spline_fxns.curvature(xx, t, c, k, aux_outputs=False)
 
     expected_deriv = np.array(dC).T
     expected_dderiv = np.array(ddC).T
@@ -125,5 +125,183 @@ def test_curvature():
     denom = np.linalg.norm(expected_deriv, axis=1) ** 3
     expected_curvature = np.nan_to_num(num / denom)
 
-    assert curvature == approx(expected_curvature, abs=0.2)
+    assert curvature == approx(expected_curvature, abs=1e-1)
+
+
+def test_torsion():
+    x = [[]]
+    t = [[]]
+    c = [[[]]]
+    k = 1.5
+
+    # test k must be an integer
+    with raises(TypeError, match=r"'float' object cannot be interpreted as an integer"):
+        spline_fxns.torsion(x, t, c, k)
+    # test k must be non-negative
+    k = -1
+    with raises(ValueError, match=r"The order of the spline must be non-negative"):
+        spline_fxns.torsion(x, t, c, k)
+    # test t must be one-dimensional
+    k = 0
+    with raises(ValueError, match=r"t must be one-dimensional"):
+        spline_fxns.torsion(x, t, c, k)
+    # test t must be non-empty
+    t = []
+    with raises(ValueError, match=r"t must be non-empty"):
+        spline_fxns.torsion(x, t, c, k)
+    # test t must contain integers or floats
+    t = ["abc"]
+    with raises(
+        TypeError,
+        match=r"elements should be \(<class 'numpy.integer'>, <class 'float'>\)",
+    ):
+        spline_fxns.torsion(x, t, c, k)
+    # test t must be non-decreasing
+    t = [2, 1, 0]
+    with raises(ValueError, match=r"t must be a non-decreasing sequence"):
+        spline_fxns.torsion(x, t, c, k)
+    # test c must be no more than 2-dimensional
+    t = [0, 1, 2]
+    with raises(ValueError, match=r"c must be 2D max"):
+        spline_fxns.torsion(x, t, c, k)
+    # test c must be non-empty
+    c = [[]]
+    with raises(ValueError, match=r"c must be non-empty"):
+        spline_fxns.torsion(x, t, c, k)
+    # test c must contain integers or floats (1D case)
+    c = ["acb"]
+    with raises(
+        TypeError,
+        match=r"elements should be \(<class 'numpy.integer'>, <class 'float'>\)",
+    ):
+        spline_fxns.torsion(x, t, c, k)
+    # test c must contain integers or floats (ND case)
+    c = [["abc"], ["def"], ["ghi"]]
+    with raises(
+        TypeError,
+        match=r"elements should be \(<class 'numpy.integer'>, <class 'float'>\)",
+    ):
+        spline_fxns.torsion(x, t, c, k)
+    # test x must be one-dimensional
+    c = [1, 2, 3, 4]
+    with raises(ValueError, match=r"x must be one-dimensional"):
+        spline_fxns.torsion(x, t, c, k)
+    # test x must be non-empty
+    x = []
+    with raises(ValueError, match=r"x must be non-empty"):
+        spline_fxns.torsion(x, t, c, k)
+    # test x must contain integers or floats
+    x = ["abc"]
+    with raises(
+        TypeError,
+        match=r"elements should be \(<class 'numpy.integer'>, <class 'float'>\)",
+    ):
+        spline_fxns.torsion(x, t, c, k)
+
+    # test results in general case
+    xx = np.linspace(-np.pi, np.pi)
+    X = xx ** 3
+    Y = np.sin(xx)
+    Z = xx ** 2
+    dX = 3 * xx ** 2
+    dY = np.cos(xx)
+    dZ = 2 * xx
+    ddX = 6 * xx
+    ddY = -np.sin(xx)
+    ddZ = 2 * np.ones(len(xx))
+    dddX = 6 * np.ones(len(xx))
+    dddY = -np.cos(xx)
+    dddZ = np.zeros(len(xx))
+
+    C = np.array([X, Y, Z])
+    dC = np.array([dX, dY, dZ]).T
+    ddC = np.array([ddX, ddY, ddZ]).T
+    dddC = np.array([dddX, dddY, dddZ]).T
+
+    tck, _ = splprep(C, u=xx, k=5)
+    t = tck[0]
+    c = tck[1]
+    k = tck[2]
+    torsion = spline_fxns.torsion(xx, t, c, k, aux_outputs=False)
+
+    expected_cross = np.cross(dC, ddC)
+    expected_num = np.diag((expected_cross @ dddC.T))
+    expected_denom = np.linalg.norm(expected_cross, axis=1) ** 2
+    expected_torsion = np.nan_to_num(expected_num / expected_denom)
+
+    assert torsion == approx(expected_torsion, abs=1e-1)
+
+
+def test_speed():
+    x = [[]]
+    t = [[]]
+    c = [[[]]]
+    k = 1.5
+
+    # test k must be an integer
+    with raises(TypeError, match=r"'float' object cannot be interpreted as an integer"):
+        spline_fxns.speed(x, t, c, k)
+    # test k must be non-negative
+    k = -1
+    with raises(ValueError, match=r"The order of the spline must be non-negative"):
+        spline_fxns.speed(x, t, c, k)
+    # test t must be one-dimensional
+    k = 0
+    with raises(ValueError, match=r"t must be one-dimensional"):
+        spline_fxns.speed(x, t, c, k)
+    # test t must be non-empty
+    t = []
+    with raises(ValueError, match=r"t must be non-empty"):
+        spline_fxns.speed(x, t, c, k)
+    # test t must contain integers or floats
+    t = ["abc"]
+    with raises(
+        TypeError,
+        match=r"elements should be \(<class 'numpy.integer'>, <class 'float'>\)",
+    ):
+        spline_fxns.speed(x, t, c, k)
+    # test t must be non-decreasing
+    t = [2, 1, 0]
+    with raises(ValueError, match=r"t must be a non-decreasing sequence"):
+        spline_fxns.speed(x, t, c, k)
+    # test c must be no more than 2-dimensional
+    t = [0, 1, 2]
+    with raises(ValueError, match=r"c must be 2D max"):
+        spline_fxns.speed(x, t, c, k)
+    # test c must be non-empty
+    c = [[]]
+    with raises(ValueError, match=r"c must be non-empty"):
+        spline_fxns.speed(x, t, c, k)
+    # test c must contain integers or floats (1D case)
+    c = ["acb"]
+    with raises(
+        TypeError,
+        match=r"elements should be \(<class 'numpy.integer'>, <class 'float'>\)",
+    ):
+        spline_fxns.speed(x, t, c, k)
+    # test c must contain integers or floats (ND case)
+    c = [["abc"], ["def"], ["ghi"]]
+    with raises(
+        TypeError,
+        match=r"elements should be \(<class 'numpy.integer'>, <class 'float'>\)",
+    ):
+        spline_fxns.speed(x, t, c, k)
+    # test x must be one-dimensional
+    c = [1, 2, 3, 4]
+    with raises(ValueError, match=r"x must be one-dimensional"):
+        spline_fxns.speed(x, t, c, k)
+    # test x must be non-empty
+    x = []
+    with raises(ValueError, match=r"x must be non-empty"):
+        spline_fxns.speed(x, t, c, k)
+    # test x must contain integers or floats
+    x = ["abc"]
+    with raises(
+        TypeError,
+        match=r"elements should be \(<class 'numpy.integer'>, <class 'float'>\)",
+    ):
+        spline_fxns.speed(x, t, c, k)
+
+    # test results in general case
+    
 
