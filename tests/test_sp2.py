726e8c4af8026884bc816f8d63741e9a3d2612c2
Felix Chen <chih.ting1992tw@gmail.com>
2020-11-01 00:59:38 -0400

    redeine loc as np.array

--- tests/test_sp2.py
+++ tests/test_sp2.py
@@ -2,31 +2,44 @@ import pytest
 import numpy as np
 from brainlit.algorithms.connect_fragments.fit_spline import GeometricGraph
 import networkx as nx
+import matplotlib.pyplot as plt
 
-# nodes should be added one by one (not as an array)
+
+#adding nodes and edges
+#1. nodes should be defined under nodes
+#2. loc should be np.array not list
+#3. nodes should be added with two arguments: samp, loc
+#4. edges should be added in pairs: samp, parent
+
+
 neuron=GeometricGraph()
-neuron.add_node(1,loc=[100,100,200])
-neuron.add_node(2,loc=[200,0,200])
-neuron.add_node(3,loc=[200,300,200])
-neuron.add_node(4,loc=[300,400,200])
-neuron.add_node(5,loc=[100,500,200])
+neuron.add_node(1,loc=np.array([100,100,200]))
+neuron.add_node(2,loc=np.array([200,0,200]))
+neuron.add_node(3,loc=np.array([200,300,200]))
+neuron.add_node(4,loc=np.array([300,400,200]))
+neuron.add_node(5,loc=np.array([100,500,200]))
+neuron.add_node(6,loc=np.array([100,500,200]))
+soma=[100,100,200]
+#neuron.add_edge(1,-1)
+neuron.add_edge(1,2)
+neuron.add_edge(2,3)
+neuron.add_edge(3,4)
+neuron.add_edge(3,5)
+#neuron.add_edge(3,5)
+#neuron.add_edge(1,5)
+#neuron.add_edge(1,4)
 
-neuron.add_edge(1,-1)
-neuron.add_edge(2,1)
-neuron.add_edge(3,2)
-neuron.add_edge(4,2)
-neuron.add_edge(5,2)
-#x=(0,2,5,6,8)
-#y=(0,2,10,5,2)
-#z=(0,0,0,0,0)
-#samp=(1,2,3,4,5)
-#par=(-1,1,2,3,4)
-print(neuron.nodes[1]["loc"])
 spline_tree = neuron.fit_spline_tree_invariant()
-"""
+
+#print(type(neuron.nodes[1]["loc"]))
+#print(neuron.nodes)
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
+
 spline_tree = neuron.fit_spline_tree_invariant()
+#spline_tree1 = spline_tree.to_undirected()
+
 for node in spline_tree.nodes:
     path = spline_tree.nodes[node]["path"]
     locs = np.zeros((len(path),3))
@@ -47,4 +60,3 @@ ax.set_yticks([])
 ax.set_zticks([])
 plt.axis('off')
 plt.show()
-"""
