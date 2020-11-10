18dd09a1f45207ca99227e972e6789ff425a1aaa
bvarjavand <bvarjavand@gmail.com>
2020-11-09 17:54:05 -0800

    try simplifying

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
-#
 # Configuration file for the Sphinx documentation builder.
 #
-# This file does only contain a selection of the most common options. For a
-# full list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
 
@@ -14,41 +12,46 @@
 #
 import os
 import sys
-import shutil
 
 sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(0, os.path.abspath("sphinxext"))
-from github_link import make_linkcode_resolve
+
 
 # -- Project information -----------------------------------------------------
 
-project = "hyppo"
-copyright = "2018"
-authors = u"Sambit Panda"
-
-# The short X.Y version
-# Find hyppo version.
-PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-for line in open(os.path.join(PROJECT_PATH, "..", "hyppo", "__init__.py")):
-    if line.startswith("__version__ = "):
-        version = line.strip().split()[2][1:-1]
+project = "brainlit"
+copyright = "2020, brainlit"
+authors = "bvarjavand"
 
 # The full version, including alpha/beta/rc tags
-release = "alpha"
+release = "0.0.0"
 
 # -- Extension configuration -------------------------------------------------
 extensions = [
+    "autoapi.extension",
     "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
     "sphinx.ext.autosummary",
     "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
     "sphinx.ext.mathjax",
     "numpydoc",
     "sphinx.ext.ifconfig",
     "sphinx.ext.githubpages",
+    "sphinxcontrib.rawfiles",
     "nbsphinx",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.linkcode",
 ]
+
+nbsphinx_kernel_name = "ndd20"
+nbsphinx_allow_errors = True
+
+autoapi_dirs = ["../brainlit"]
+autoapi_add_toctree_entry = False
+autoapi_generate_api_docs = False
+
+# -- sphinxcontrib.rawfiles
+rawfiles = ["CNAME"]
 
 # -- numpydoc
 # Below is needed to prevent errors
@@ -67,6 +70,7 @@ intersphinx_mapping = {
     "numpy": ("https://docs.scipy.org/doc/numpy", None),
     "python": ("https://docs.python.org/3", None),
     "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
+    "sklearn": ("http://scikit-learn.org/dev", None),
 }
 
 # -- sphinx options ----------------------------------------------------------
@@ -76,10 +80,10 @@ master_doc = "index"
 source_encoding = "utf-8"
 
 # -- Options for HTML output -------------------------------------------------
-# Add any paths that contain templates here, relative to this directory.
+# Add any paths that contain custom static files here, relative to this directory.
 templates_path = ["_templates"]
 html_static_path = ["_static"]
-modindex_common_prefix = ["hyppo."]
+modindex_common_prefix = ["brainlit."]
 
 pygments_style = "sphinx"
 smartquotes = False
@@ -89,124 +93,9 @@ import sphinx_rtd_theme
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_theme_options = {"collapse_navigation": False, "navigation_depth": 3}
-
-html_context = {
-    # Enable the "Edit in GitHub link within the header of each page.
-    "display_github": True,
-    # Set the following variables to generate the resulting github URL for each page.
-    # Format Template: https://{{ github_host|default("github.com") }}/{{ github_user }}/{{ github_repo }}/blob/{{ github_version }}{{ conf_py_path }}{{ pagename }}{{ suffix }}
-    "github_user": "neurodata",
-    "github_repo": "hyppo",
-    "github_version": "master/docs/",
+html_theme_options = {
+    #'includehidden': False,
+    "navigation_depth": 2,
+    "collapse_navigation": False,
+    "navigation_depth": 3,
 }
-
-linkcode_resolve = make_linkcode_resolve(
-    "hyppo",
-    u"https://github.com/neurodata/"
-    "hyppo/blob/{revision}/"
-    "{package}/{path}#L{lineno}",
-)
-
-# Custom sidebar templates, must be a dictionary that maps document names
-# to template names.
-#
-# The default sidebars (for documents that don't match any pattern) are
-# defined by theme itself.  Builtin themes are using these templates by
-# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
-# 'searchbox.html']``.
-#
-# html_sidebars = {}
-
-# -- Options for HTMLHelp output ---------------------------------------------
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = "hyppodoc"
-
-# -- Options for LaTeX output ------------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [(master_doc, "hyppo.tex", "hyppo Documentation", authors, "manual")]
-
-# -- Options for manual page output ------------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, "hyppo", "hyppo Documentation", [authors], 1)]
-
-# -- Options for Texinfo output ----------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (
-        master_doc,
-        "hyppo",
-        "hyppo Documentation",
-        authors,
-        "hyppo",
-        "One line description of project.",
-        "Miscellaneous",
-    )
-]
-
-# -- Options for Epub output -------------------------------------------------
-
-# Bibliographic Dublin Core info.
-epub_title = project
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#
-# epub_identifier = ''
-
-# A unique identification for the text.
-#
-# epub_uid = ''
-
-# A list of files that should not be packed into the epub file.
-epub_exclude_files = ["search.html"]
-
-
-def setup(app):
-    # to hide/show the prompt in code examples:
-    app.add_javascript("js/copybutton.js")
-
-
-# -- Jupyter Notebook --------------------------------------------------------
-
-
-# def all_but_ipynb(dir, contents):
-#     result = []
-#     for c in contents:
-#         if os.path.isfile(os.path.join(dir, c)) and (not c.endswith(".ipynb")):
-#             result += [c]
-#     return result
-
-
-# shutil.rmtree(
-#     os.path.join(PROJECT_PATH, "..", "docs/tutorials/benchmarks"), ignore_errors=True
-# )
-# shutil.copytree(
-#     os.path.join(PROJECT_PATH, "..", "benchmarks"),
-#     os.path.join(PROJECT_PATH, "..", "docs/tutorials/benchmarks"),
-#     ignore=all_but_ipynb,
-# )
