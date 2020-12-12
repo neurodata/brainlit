8db6279147ef10082796dca764fe409e0e257187
shreyasingh1 <shreya.singh1620@gmail.com>
2020-12-12 17:20:30 -0500

    updating init file

--- brainlit/utils/__init__.py
+++ brainlit/utils/__init__.py
@@ -1,5 +1,4 @@
 from brainlit.utils.session import *
 from brainlit.utils.upload import *
 from brainlit.utils.swc import *
-from brainlit.utils.upload_benchmarking import *
 from brainlit.utils.benchmarking_params import *
