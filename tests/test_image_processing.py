78aec29a45f06d3421b05da72059b373980c5662
Thomas Athey <tom.l.athey@gmail.com>
2021-01-19 14:31:00 -0500

    more tests for bresenham

--- tests/test_image_processing.py
+++ tests/test_image_processing.py
@@ -29,7 +29,7 @@ def test_Bresenham3D_bad_input():
 
 
 def test_Bresenham3D():
-    # use examples from https://www.geeksforgeeks.org/bresenhams-algorithm-for-3-d-line-drawing/
+    # use examples adapted from https://www.geeksforgeeks.org/bresenhams-algorithm-for-3-d-line-drawing/
 
     # example 1
     [x1, y1, z1, x2, y2, z2] = [-1, 1, 1, 5, 3, -1]
@@ -42,10 +42,20 @@ def test_Bresenham3D():
     assert t_z == zlist
 
     # example 2
-    [x1, y1, z1, x2, y2, z2] = [-7, 0, -3, 2, -5, -1]
-    xlist = [-7, -6, -5, -4, -3, -2, -1, 0, 1, 2]
-    ylist = [0, -1, -1, -2, -2, -3, -3, -4, -4, -5]
+    [x1, y1, z1, x2, y2, z2] = [0, -7, -3, -5, 2, -1]
+    xlist = [0, -1, -1, -2, -2, -3, -3, -4, -4, -5]
+    ylist = [-7, -6, -5, -4, -3, -2, -1, 0, 1, 2]
     zlist = [-3, -3, -3, -2, -2, -2, -2, -1, -1, -1]
+    t_x, t_y, t_z = Bresenham3D(x1, y1, z1, x2, y2, z2)
+    assert t_x == xlist
+    assert t_y == ylist
+    assert t_z == zlist
+
+    # example 3
+    [x1, y1, z1, x2, y2, z2] = [0, -3, -7, -5, -1, 2]
+    xlist = [0, -1, -1, -2, -2, -3, -3, -4, -4, -5]
+    ylist = [-3, -3, -3, -2, -2, -2, -2, -1, -1, -1]
+    zlist = [-7, -6, -5, -4, -3, -2, -1, 0, 1, 2]
     t_x, t_y, t_z = Bresenham3D(x1, y1, z1, x2, y2, z2)
     assert t_x == xlist
     assert t_y == ylist
