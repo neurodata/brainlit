04e475d145b214f34da5308076fe6e56509638f4
Chih-Ting Chen <chih.ting1992tw@gmail.com>
2021-01-29 22:10:45 -0500

    fit_spline.py code coverage and black
    1. break at the end of while loop to avoid the statement
       `stack.append((tree,curr_spline_num))
    2. remove warning message that notifies the user when removal of
       duplicate nodes occurs, which will not happen.
    3. black

--- brainlit/algorithms/trace_analysis/fit_spline.py
+++ brainlit/algorithms/trace_analysis/fit_spline.py
@@ -144,7 +144,7 @@ class GeometricGraph(nx.Graph):
                 curr_spline_num, path=main_branch, starting_length=tree[2]
             )
             spline_tree.add_edge(parent_num, curr_spline_num)
-            if len(collateral_branches)==0 and len(stack)==0:
+            if len(collateral_branches) == 0 and len(stack) == 0:
                 break
             else:
                 for tree in collateral_branches:
