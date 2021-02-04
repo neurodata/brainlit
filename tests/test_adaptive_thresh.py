16822fc85d379098e0b1b181fed6840e65438106
Chih-Ting Chen <chih.ting1992tw@gmail.com>
2021-02-04 10:16:42 -0500

    black test_adaptive_thresh.py
    only 2 tests are included

--- tests/test_adaptive_thresh.py
+++ tests/test_adaptive_thresh.py
@@ -1,5 +1,16 @@
 import pytest
-from brainlit.algorithms.generate_fragments.adaptive_thresh import get_seed, get_img_T1, thres_from_gmm, fast_marching_seg, level_set_seg, connected_threshold, confidence_connected_threshold, neighborhood_connected_threshold, otsu, gmm_seg 
+from brainlit.algorithms.generate_fragments.adaptive_thresh import (
+    get_seed,
+    get_img_T1,
+    thres_from_gmm,
+    fast_marching_seg,
+    level_set_seg,
+    connected_threshold,
+    confidence_connected_threshold,
+    neighborhood_connected_threshold,
+    otsu,
+    gmm_seg,
+)
 import SimpleITK as sitk
 from sklearn.mixture import GaussianMixture
 import numpy as np
@@ -11,19 +22,21 @@ import matplotlib.pyplot as plt
 
 
 def test_get_seed():
-    
+
     # define voxel
-    voxel = (10.131,30.6001,100)
+    voxel = (10.131, 30.6001, 100)
     numpy_seed, sitk_seed = get_seed(voxel)
     assert numpy_seed == (10, 30, 100)
     assert sitk_seed == (100, 30, 10)
 
 
 def test_get_img_T1():
-    img = np.array([[[100,250],[800,300]],[[1200,2000],[3000,2500]]])
+    img = np.array([[[100, 250], [800, 300]], [[1200, 2000], [3000, 2500]]])
     img_T1, img_T1_255 = get_img_T1(img)
     assert type(img_T1) == sitk.Image
     assert type(img_T1_255) == sitk.Image
-    assert img_T1_255.GetPixelIDTypeAsString() == '8-bit unsigned integer'
+    assert img_T1_255.GetPixelIDTypeAsString() == "8-bit unsigned integer"
 
 
+# def test_thres_from_gmm():
+#    d
