788c32d7731b85e58b7c33b370446ba689ea8c2f
fxu10 <fxu10@jhu.edu>
2021-02-24 23:52:51 -0500

    [skip ci] Partially implemented line to line and line to blob distances, added unit tests

--- brainlit/algorithms/connect_fragments/dynamic_programming_viterbi.py
+++ brainlit/algorithms/connect_fragments/dynamic_programming_viterbi.py
@@ -26,8 +26,10 @@ from skimage import measure
 from skimage import morphology
 import time
 
+import matplotlib.pyplot as plt
+
 class viterbi_algorithm:
-    def __init__(self, image, labels, soma_labels, resolution):
+    def __init__(self, image, labels, soma_labels, resolution=[1,1,1]):
 
         num_components = np.amax(labels)
         self.num_components = num_components
@@ -53,3 +55,178 @@ class viterbi_algorithm:
 
         self.end_points = None
         self.not_lines = None
+    
+    def compute_bounds(self, label, pad):
+        """ Currently zmin and zmax are hardcoded as 0,1 for this simple image """
+        """compute coordinates of bounding box around a masked object, with given padding
+        Args:
+            label (np.array): mask of the object
+            pad (float): padding around object in um
+        Returns:
+            [ints]: integer coordinates of bounding box
+        """
+        labels = self.labels
+        res = self.res
+
+        r = np.any(label, axis=(1, 2))
+        c = np.any(label, axis=(0, 2))
+        z = np.any(label, axis=(0, 1))
+        rmin, rmax = np.where(r)[0][[0, -1]]
+        rmin = np.amax((0, math.floor(rmin - pad / res[0])))
+        rmax = np.amin((labels.shape[0], math.ceil(rmax + (pad + 1) / res[0])))
+        cmin, cmax = np.where(c)[0][[0, -1]]
+        cmin = np.amax((0, math.floor(cmin - (pad) / res[1])))
+        cmax = np.amin((labels.shape[1], math.ceil(cmax + (pad + 1) / res[1])))
+        #zmin, zmax = np.where(z)[0][[0, -1]]
+        #zmin = np.amax((0, math.floor(zmin - (pad) / res[2])))
+        #zmax = np.amin((labels.shape[2], math.ceil(zmax + (pad + 1) / res[2])))
+        zmin = 0
+        zmax = 2
+        return int(rmin), int(rmax), int(cmin), int(cmax), int(zmin), int(zmax)
+    
+        
+    def frags_to_lines_le_skel(self, soma_lbls=[]):
+        """Relies on the assumption that self.labels has values as if it came from measure.label"""
+        labels = self.labels
+
+        end_points = {}
+
+        # Note: we want label 1 onwards, because 0 is background
+        for component in np.unique(labels)[1:]:
+            print(component)
+            # Skip if it is a soma
+            if component in soma_lbls:
+                continue
+            
+            # Mask the current component
+            mask = labels == component
+            
+            # The mask is relatively sparse, so we need to cut out only the 
+            # relevant regions with labels
+            rmin, rmax, cmin, cmax, zmin, zmax = self.compute_bounds(mask, pad=1)
+            mask = mask[rmin:rmax,cmin:cmax,zmin:zmax]
+
+            skel = morphology.skeletonize_3d(mask)
+
+            coords_mask = np.argwhere(mask)
+
+            coords_skel = np.argwhere(skel)
+            print(coords_mask)
+            if len(coords_skel) < 4:
+                coords = coords_mask
+            else:
+                coords = coords_skel
+
+            embedding = SpectralEmbedding(n_components=1).fit_transform(coords)
+
+            amax = np.argmax(embedding)
+            amin = np.argmin(embedding)
+            a = coords[amax, :]
+            b = coords[amin, :]
+
+            end_points[component] = (a, b)
+            
+        print(f"{len(end_points.keys())} out of {len(np.unique(labels)[1:])} are lines")
+
+        self.end_points = end_points
+        components = set(np.unique(labels)[1:])
+        components_lines = set(end_points.keys())
+        self.not_lines = components.difference(components_lines)
+
+    
+    def line_line_dist(self, lbl1, lbl2):
+        """
+        Args:
+            lbl1 ([type]): [non-soma component]
+            lbl2 ([type]): [non-soma component]
+        """
+        if lbl1 == lbl2:
+            raise ValueError(f"Cannot compute distance between {lbl1} and {lbl2}")
+        
+        ends1 = self.end_points[lbl1]
+        ends2 = self.end_points[lbl2]
+        
+        # Compute the euclidean distance between each endpoint
+        d1 = np.linalg.norm(np.subtract(ends1[0], ends2[0]))
+        d2 = np.linalg.norm(np.subtract(ends1[0], ends2[1]))
+        d3 = np.linalg.norm(np.subtract(ends1[1], ends2[0]))
+        d4 = np.linalg.norm(np.subtract(ends1[1], ends2[1]))
+        
+        idx = np.argmin([d1, d2, d3, d4])
+
+        if idx == 0:
+            loc1, loc2 = ends1[0], ends2[0]
+        elif idx == 1:
+            loc1, loc2 = ends1[0], ends2[1]
+        elif idx == 2:
+            loc1, loc2 = ends1[1], ends2[0]
+        elif idx == 3:
+            loc1, loc2 = ends1[1], ends2[1]
+
+        self.connection_mat[0][lbl1, lbl2, :] = loc1
+        self.connection_mat[1][lbl1, lbl2, :] = loc2
+
+        dist_cost = np.amin([d1, d2, d3, d4])
+        return dist_cost, loc1, loc2
+    
+    def line_blob_dist(self, lbl1, lbl2):
+        """
+        Args:
+            lbl1 ([type]): [line component]
+            lbl2 ([type]): [soma component]
+        """
+        if lbl1 == lbl2:
+            raise ValueError(f"Cannot compute distance between {lbl1} and {lbl2}")
+
+        labels = self.labels
+
+        connection_mat = self.connection_mat
+
+        # lbl1 is a line, lbl2 is a soma or blob
+        points = self.end_points[lbl1]
+        nonline_lbl = lbl2
+
+        lowest_cost = np.inf
+
+        label_nonline = labels == nonline_lbl
+        # for all endpoints of the line
+        for point in points:
+            if nonline_lbl in self.somas.keys():
+                coords = self.somas[nonline_lbl]
+            else:
+                coords = np.argwhere(
+                    label_nonline ^ ndi.morphology.binary_erosion(label_nonline)
+                )
+                self.soma_locs[nonline_lbl] = coords
+
+            dists = np.linalg.norm(np.subtract(coords, point))
+            min_dist_ind = np.argmin(dists)
+            #nonline_point = coords[min_dist_ind, :]
+            dist_cost = np.amin(dists)
+            
+            # evaluate only by distances
+            if dist_cost < lowest_cost:
+                lowest_cost = dist_cost
+                #point_lowest = point
+                #nonline_point_lowest = nonline_point
+
+        '''
+        # set connection_mat points
+        connection_mat[0][lbl1, lbl2, :] = point_lowest
+        if labels[point_lowest[0], point_lowest[1], point_lowest[2]] != lbl1:
+            raise ValueError(
+                f"Lowest cost point: {point_lowest} has label {labels[point_lowest[0], point_lowest[1], point_lowest[2]]}, not {lbl1}"
+            )
+        connection_mat[1][lbl1, lbl2, :] = nonline_point_lowest
+        if (
+            labels[
+                nonline_point_lowest[0],
+                nonline_point_lowest[1],
+                nonline_point_lowest[2],
+            ]
+            != lbl2
+        ):
+            raise ValueError("Error in setting connection_mat")
+        '''
+        return lowest_cost#, point_lowest, nonline_point_lowest
+
--- brainlit/algorithms/connect_fragments/tests/test_viterbi.py
+++ brainlit/algorithms/connect_fragments/tests/test_viterbi.py
@@ -4,7 +4,6 @@ Created on Wed Feb 17 23:47:23 2021
 
 @author: frede
 """
-
 import numpy as np
 import pandas as pd
 import tifffile as tf
@@ -19,25 +18,62 @@ import matplotlib.pyplot as plt
 
 ''' Setting up the image environment '''
 
-# 10x10 image, with 1 color channel greyscale
-grid = np.zeros((10,10,1))
-grid[0,9] = 255
-grid[0,0] = 255
-grid[0,1] = 255
-grid[0,2] = 255
-grid[3,3] = 255
-grid[4,3] = 255
-grid[5,3] = 255
-grid[5,4] = 255
-grid[6,4] = 255
-grid[6,5] = 255
-grid[7,7] = 255
-grid[7,8] = 255
-grid[9,9] = 255
+def grid_gen():
+    # 10x10x2 image, with 1 color channel greyscale
+    # The 3rd dimension will simply be a copy of the 1st dimension
+    grid = np.zeros((10,10,2))
+    labels = np.zeros((10,10,2))
+    grid[0,9,0] = 255
+    grid[0,0,0] = 255
+    grid[0,1,0] = 255
+    grid[0,2,0] = 255
+    grid[3,3,0] = 255
+    grid[4,3,0] = 255
+    grid[5,3,0] = 255
+    grid[5,4,0] = 255
+    grid[6,4,0] = 255
+    grid[6,5,0] = 255
+    grid[7,7,0] = 255
+    grid[7,8,0] = 255
+    grid[9,9,0] = 255
+    
+    # Add this back in later. For simplicity, only the 0th layer of grid has labels
+    #grid[:,:,1] = grid[:,:,0] 
+    # Label the components
+    labels, num = measure.label(grid, return_num=True)
+    plt.imshow(labels[:,:,0])
+    # We'll have the bottom-right corner, which is labeled 5, be the soma
+    somas = {5:(9,9,0)}
+    return grid, labels, num, somas
 
-# Label the components
-labels, num = measure.label(grid, return_num=True)
-plt.imshow(labels[:,:,0])
+img, lbls, _ , somas = grid_gen()
+alg = viterbi_algorithm(img, lbls, somas, [1,1])
+# For the timebeing, manually do the endpoints
+# NOTE: no endpoints for 2 or 5 because they are "blobs"
+endpoints = {}
+endpoints[1] = ((0,0,0),(0,2,0))
+endpoints[3] = ((3,3,0),(6,5,0))
+endpoints[4] = ((7,7,0),(7,8,0))
+alg.end_points = endpoints
+    
 
-# We'll have the bottom-right corner, which is labeled 5, be the soma
-somas = [5]
+def testInit():   
+    # alg.x == x will return a boolean matrix. The sum should be equal to
+    #   dim1 * dim2 * dim3 if every position is True
+    assert(np.sum(alg.image == img) == 10 * 10 * 2)
+    assert(np.sum(alg.labels == lbls) == 10 * 10 * 2)
+    
+def testLineToLine():
+    # Compare the output of line_line_dist with manually computed distances
+    assert(np.sqrt((0-3)**2+(2-3)**2) == alg.line_line_dist(1,3)[0])
+    assert(np.sqrt((0-3)**2+(2-3)**2) == alg.line_line_dist(3,1)[0])
+    assert(np.sqrt((6-7)**2+(5-7)**2) == alg.line_line_dist(3,4)[0])
+    assert(np.sqrt((6-7)**2+(5-7)**2) == alg.line_line_dist(4,3)[0])
+    
+def testLineToSoma():
+    # Compare the output of line_line_dist with manually computed distances
+    assert(np.sqrt((8-9)**2+(7-9)**2) == alg.line_blob_dist(4,5))
+
+testInit()
+testLineToLine()
+testLineToSoma()
