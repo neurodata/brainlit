10506c439e8b46b8bb87d20494a115379f95c33a
fxu10 <fxu10@jhu.edu>
2021-03-04 00:28:56 -0500

    [skip ci] implemented intensity cost, compute all dists, and unit tests

--- brainlit/algorithms/connect_fragments/dynamic_programming_viterbi.py
+++ brainlit/algorithms/connect_fragments/dynamic_programming_viterbi.py
@@ -55,6 +55,7 @@ class viterbi_algorithm:
 
         self.end_points = None
         self.not_lines = None
+        self.sigma = 1/20
     
     def compute_bounds(self, label, pad):
         """ Currently zmin and zmax are hardcoded as 0,1 for this simple image """
@@ -85,7 +86,7 @@ class viterbi_algorithm:
         return int(rmin), int(rmax), int(cmin), int(cmax), int(zmin), int(zmax)
     
         
-    def frags_to_lines_le_skel(self, soma_lbls=[]):
+    def frags_to_lines_le_skel(self, nonline_labels=[]):
         """Relies on the assumption that self.labels has values as if it came from measure.label"""
         labels = self.labels
 
@@ -93,9 +94,8 @@ class viterbi_algorithm:
 
         # Note: we want label 1 onwards, because 0 is background
         for component in np.unique(labels)[1:]:
-            print(component)
             # Skip if it is a soma
-            if component in soma_lbls:
+            if component in nonline_labels:
                 continue
             
             # Mask the current component
@@ -105,24 +105,23 @@ class viterbi_algorithm:
             # relevant regions with labels
             rmin, rmax, cmin, cmax, zmin, zmax = self.compute_bounds(mask, pad=1)
             mask = mask[rmin:rmax,cmin:cmax,zmin:zmax]
-
             skel = morphology.skeletonize_3d(mask)
 
             coords_mask = np.argwhere(mask)
 
             coords_skel = np.argwhere(skel)
-            print(coords_mask)
             if len(coords_skel) < 4:
                 coords = coords_mask
             else:
                 coords = coords_skel
 
             embedding = SpectralEmbedding(n_components=1).fit_transform(coords)
-
             amax = np.argmax(embedding)
             amin = np.argmin(embedding)
             a = coords[amax, :]
             b = coords[amin, :]
+            a = np.add(a,[rmin, cmin, zmin])
+            b = np.add(b,[rmin, cmin, zmin])
 
             end_points[component] = (a, b)
             
@@ -163,9 +162,6 @@ class viterbi_algorithm:
         elif idx == 3:
             loc1, loc2 = ends1[1], ends2[1]
 
-        self.connection_mat[0][lbl1, lbl2, :] = loc1
-        self.connection_mat[1][lbl1, lbl2, :] = loc2
-
         dist_cost = np.amin([d1, d2, d3, d4])
         return dist_cost, loc1, loc2
     
@@ -180,28 +176,27 @@ class viterbi_algorithm:
 
         labels = self.labels
 
-        connection_mat = self.connection_mat
-
         # lbl1 is a line, lbl2 is a soma or blob
         line_pts = self.end_points[lbl1]
-        blob_lbl = lbl2
+        blob = lbl2
 
         lowest_cost = np.inf
 
-        label_nonline = labels == blob_lbl
+        label_nonline = labels == blob
         # for all endpoints of the line
         for endpt in line_pts:
-            if blob_lbl in self.somas.keys():
+            if blob in self.somas.keys():
                 # Soma is represented as a single point in space,
                 # cast as a list with 1 tuple object
-                coords = [self.somas[blob_lbl]]
+                coords = self.somas[blob]
             else:
+                # Cut out only the boundary of the blob for distance computation
                 coords = np.argwhere(
                     label_nonline ^ ndi.morphology.binary_erosion(label_nonline)
                 )
-                self.soma_locs[blob_lbl] = coords
+                self.somas[blob] = coords
 
-            dists = np.linalg.norm(np.subtract(coords, endpt))
+            dists = np.linalg.norm(np.subtract(coords, endpt),axis=1)
             dist_cost = np.amin(dists)
             
             # find minimum based on distance cost
@@ -210,15 +205,93 @@ class viterbi_algorithm:
                 endpt_lowest = endpt
                 blob_lowest = coords[np.argmin(dists)]
 
-        # set connection_mat points
-        connection_mat[0][lbl1, lbl2, :] = endpt_lowest
-        if labels[endpt_lowest] != lbl1:
+        if labels[endpt_lowest[0],endpt_lowest[1],endpt_lowest[2]] != lbl1:
             raise ValueError(
                 f"Lowest cost point: {endpt_lowest} has label {labels[endpt_lowest]}, not {lbl1}"
             )
-        connection_mat[1][lbl1, lbl2, :] = blob_lowest
-        if (labels[blob_lowest] != lbl2):
-            raise ValueError("Error in setting connection_mat")
 
         return lowest_cost, endpt_lowest, blob_lowest
+
+    def line_int(self, loc1, loc2, lbl1, lbl2):
+        # Use bresenham3D to "draw" a line in 3D
+        xlist, ylist, zlist = Bresenham3D(
+            loc1[0], loc1[1], loc1[2], loc2[0], loc2[1], loc2[2]
+        )
+        # Calculate the intensity cost along the line
+        ints = self.image[xlist, ylist, zlist]
+
+        # remove first and last voxels, which are part of foreground
+        ints = ints[1:-1]
+
+        mu1 = 2  # np.mean(image[labels == lbl1])
+
+        # Need to check about this
+        #int_cost = (mu1 ** 2 - 2 * mu1 * np.mean(ints)) / self.sigma
+        
+        int_cost = np.mean(ints)
+        
+        return int_cost
+    
+    def compute_all_dists(self, somas):
+        for lbl1 in range(1, self.num_components + 1):
+            for lbl2 in range(lbl1, self.num_components + 1):
+                if lbl2 == lbl1:
+                    continue
+                
+                if lbl1 in self.end_points.keys() and lbl2 in self.end_points.keys():
+                    # Line to line
+                    dist, loc1, loc2 = self.line_line_dist(lbl1, lbl2)
+                    
+                # One of them is a blob
+                elif lbl1 in self.end_points.keys():
+                    # lbl1 to soma (lbl2)
+                    dist, loc1, loc2 = self.line_blob_dist(lbl1, lbl2)
+                elif lbl2 in self.end_points.keys():
+                    # lbl2 to soma (lbl1)
+                    dist, loc2, loc1 = self.line_blob_dist(lbl2, lbl1)
+                
+                # Both are blobs
+                else:
+                    continue
+                    
+                # Distance cost is symmetric
+                self.cost_mat_dist[lbl1, lbl2] = dist
+                self.cost_mat_dist[lbl2, lbl1] = dist
+                
+                int_cost = self.line_int(loc1, loc2, lbl1, lbl2)
+                
+                # Int cost is symmetric
+                self.cost_mat_int[lbl1, lbl2] = int_cost
+                self.cost_mat_int[lbl2, lbl1] = int_cost
+                
+                
+                # Set the forward connection
+                self.connection_mat[0][lbl1, lbl2] = loc1
+                self.connection_mat[1][lbl1, lbl2] = loc2
+
+                # Set the backward connection
+                self.connection_mat[0][lbl2, lbl1] = loc2
+                self.connection_mat[1][lbl2, lbl1] = loc1
+
+                if dist+int_cost < 0:
+                    warnings.warn(
+                        f"Negative cost between {lbl1} to {lbl2} from: dist - {dist}, intensity - {int_cost}"
+                    )
+
+        for soma in somas:
+            # Going from soma to anything else is impossible, 
+            # as we want to connect foreground to somas
+            self.cost_mat_dist[soma, :] = np.inf
+            self.cost_mat_int[soma, :] = np.inf
+            # Soma to soma is 0
+            self.cost_mat_dist[soma, soma] = 0
+            self.cost_mat_int[soma, soma] = 0
+
+
+        for lbl1 in range(1, self.num_components + 1):
+            if lbl1 in somas:
+                denom = 0
+            else:
+                denom = logsumexp(-1 * self.cost_mat_dist[lbl1, 1:])
+            self.cost_mat_dist[lbl1, 1:] = self.cost_mat_dist[lbl1, 1:] + denom
 
--- brainlit/algorithms/connect_fragments/tests/test_viterbi.py
+++ brainlit/algorithms/connect_fragments/tests/test_viterbi.py
@@ -16,47 +16,6 @@ from skimage import io, measure
 # To be removed later, only for testing purposes
 import matplotlib.pyplot as plt
 
-''' Setting up the image environment '''
-
-def grid_gen():
-    # 10x10x2 image, with 1 color channel greyscale
-    # The 3rd dimension will simply be a copy of the 1st dimension
-    grid = np.zeros((10,10,2))
-    labels = np.zeros((10,10,2))
-    grid[0,9,0] = 255
-    grid[0,0,0] = 255
-    grid[0,1,0] = 255
-    grid[0,2,0] = 255
-    grid[3,3,0] = 255
-    grid[4,3,0] = 255
-    grid[5,3,0] = 255
-    grid[5,4,0] = 255
-    grid[6,4,0] = 255
-    grid[6,5,0] = 255
-    grid[7,7,0] = 255
-    grid[7,8,0] = 255
-    grid[9,9,0] = 255
-    
-    # Add this back in later. For simplicity, only the 0th layer of grid has labels
-    #grid[:,:,1] = grid[:,:,0] 
-    # Label the components
-    labels, num = measure.label(grid, return_num=True)
-    plt.imshow(labels[:,:,0])
-    # We'll have the bottom-right corner, which is labeled 5, be the soma
-    somas = {5:(9,9,0)}
-    return grid, labels, num, somas
-
-img, lbls, _ , somas = grid_gen()
-alg = viterbi_algorithm(img, lbls, somas, [1,1])
-# For the timebeing, manually do the endpoints
-# NOTE: no endpoints for 2 or 5 because they are "blobs"
-endpoints = {}
-endpoints[1] = ((0,0,0),(0,2,0))
-endpoints[3] = ((3,3,0),(6,5,0))
-endpoints[4] = ((7,7,0),(7,8,0))
-alg.end_points = endpoints
-    
-
 def testInit():   
     # alg.x == x will return a boolean matrix. The sum should be equal to
     #   dim1 * dim2 * dim3 if every position is True
@@ -69,11 +28,194 @@ def testLineToLine():
     assert(np.sqrt((0-3)**2+(2-3)**2) == alg.line_line_dist(3,1)[0])
     assert(np.sqrt((6-7)**2+(5-7)**2) == alg.line_line_dist(3,4)[0])
     assert(np.sqrt((6-7)**2+(5-7)**2) == alg.line_line_dist(4,3)[0])
+    with pytest.raises(ValueError):
+        alg.line_line_dist(2,2)
     
-def testLineToSoma():
+def testLineToBlob():
     # Compare the output of line_line_dist with manually computed distances
-    assert(np.sqrt((8-9)**2+(7-9)**2) == alg.line_blob_dist(4,5))
+    assert(np.sqrt((8-9)**2+(7-9)**2) == alg.line_blob_dist(4,5)[0])
+
+    # Should go from an endpoint to the closest position on the blob
+    assert(np.sqrt((9-3)**2+(9-15)**2) == alg2.line_blob_dist(2,1)[0])
+    with pytest.raises(ValueError):
+        alg.line_blob_dist(2,2)
+
+def testPathIntensityCost():
+    # Currently a stub, need to double-check what the proper form of the 
+    # intensity score should be
+    assert(0 == alg2.line_int((9,9,0),(12,12,0),2,3))
+    
+def testConnections():
+    # Test if we are getting the correct connections
+    alg.compute_all_dists(alg.somas)
+    c = alg.connection_mat
+    print(alg.connection_mat[0][3][1])
+    print(alg.connection_mat[1][3][1])
+    
+    # 1 to x, x to 1
+    np.testing.assert_equal(c[0][1][2], [0,2,0])
+    np.testing.assert_equal(c[1][1][2], [0,9,0])
+    np.testing.assert_equal(c[0][2][1], [0,9,0])
+    np.testing.assert_equal(c[1][2][1], [0,2,0])
+    
+    np.testing.assert_equal(c[0][1][3], [0,2,0])
+    np.testing.assert_equal(c[1][1][3], [3,3,0])
+    np.testing.assert_equal(c[0][3][1], [3,3,0])
+    np.testing.assert_equal(c[1][3][1], [0,2,0])
+    
+    np.testing.assert_equal(c[0][1][4], [0,2,0])
+    np.testing.assert_equal(c[1][1][4], [7,7,0])
+    np.testing.assert_equal(c[0][4][1], [7,7,0])
+    np.testing.assert_equal(c[1][4][1], [0,2,0])
+    
+    # 2 to x, x to 2
+    np.testing.assert_equal(c[0][2][3], [0,9,0])
+    np.testing.assert_equal(c[1][2][3], [3,3,0])
+    np.testing.assert_equal(c[0][3][2], [3,3,0])
+    np.testing.assert_equal(c[1][3][2], [0,9,0])
+    
+    np.testing.assert_equal(c[0][2][4], [0,9,0])
+    np.testing.assert_equal(c[1][2][4], [7,8,0])
+    np.testing.assert_equal(c[0][4][2], [7,8,0])
+    np.testing.assert_equal(c[1][4][2], [0,9,0])
+
+    # 3 to x, x to 3
+    np.testing.assert_equal(c[0][3][4], [6,5,0])
+    np.testing.assert_equal(c[1][3][4], [7,7,0])
+    np.testing.assert_equal(c[0][4][3], [7,7,0])
+    np.testing.assert_equal(c[1][4][3], [6,5,0])
+    
+    # 4 to x, x to 4 is covered by other test blocks
+    
+    # SOMA CONNECTIONS
+    # 5 to x, x to 5 
+    # NOTE: All of the following 5 to x fail currently.
+    np.testing.assert_equal(c[0][1][5], [0,2,0])
+    np.testing.assert_equal(c[1][1][5], [9,9,0])
+    np.testing.assert_equal(c[0][5][1], [0,0,0])
+    np.testing.assert_equal(c[1][5][1], [0,0,0])
+        
+    np.testing.assert_equal(c[0][3][5], [6,5,0])
+    np.testing.assert_equal(c[1][3][5], [9,9,0])
+    np.testing.assert_equal(c[0][5][3], [0,0,0])
+    np.testing.assert_equal(c[1][5][3], [0,0,0])
+    
+    np.testing.assert_equal(c[0][4][5], [7,8,0])
+    np.testing.assert_equal(c[1][4][5], [9,9,0])
+    np.testing.assert_equal(c[0][5][4], [0,0,0])
+    np.testing.assert_equal(c[1][5][4], [0,0,0])
+    
+        # Cannot go from blob 2 to soma 5
+    np.testing.assert_equal(c[0][2][5], [0,0,0])
+    np.testing.assert_equal(c[1][2][5], [0,0,0])
+    np.testing.assert_equal(c[0][5][2], [0,0,0])
+    np.testing.assert_equal(c[1][5][2], [0,0,0])
+    
+    # For manual debugging purposes
+    connect_points = set()
+    for i in range(1):
+        for j in range(1,5):
+            for k in range(1,5):
+                if not (np.equal([0,0,0], c[i][j][k])).all():
+                    connect_points.add(tuple(c[i][j][k]))
+
+
+''' Setting up the image environment '''
+
+def grid_gen(grid_id=10):
+    if grid_id == 10:
+        ''' Generates a 10x10x2 grid with a small assortment of labels'''
+        # 10x10x2 image, with 1 color channel greyscale
+        grid = np.zeros((10,10,2))
+        labels = np.zeros((10,10,2))
+        grid[0,9,0] = 255
+        grid[0,0,0] = 255
+        grid[0,1,0] = 255
+        grid[0,2,0] = 255
+        grid[3,3,0] = 255
+        grid[4,3,0] = 255
+        grid[5,3,0] = 255
+        grid[5,4,0] = 255
+        grid[6,4,0] = 255
+        grid[6,5,0] = 255
+        grid[7,7,0] = 255
+        grid[7,8,0] = 255
+        grid[9,9,0] = 255
+        
+        # Add this back in later. For simplicity, only the 0th layer of grid has labels
+        #grid[:,:,1] = grid[:,:,0] 
+        # Label the components
+        labels, num = measure.label(grid, return_num=True)
+        plt.imshow(labels[:,:,0])
+        # We'll have the bottom-right corner, which is labeled 5, be the soma
+        somas = {5:[(9,9,0)]}
+    
+    if grid_id == 20:
+        ''' Generates a 20x20x2 grid with a small assortment of labels'''
+        # 20x20x2 image, with 1 color channel greyscale
+        grid = np.zeros((20,20,2))
+        labels = np.zeros((20,20,2))
+        # Create some diagonal lines
+        for i in range(1,10):
+            grid[i,i,0] = 200
+        
+        # Create some diagonal lines
+        for i in range(12,16):
+            grid[i,i,0] = 175
+        
+        # Create a "blob"
+        grid[0,16,0] = 225
+        grid[0,17,0] = 225
+        grid[0,18,0] = 225
+        for i in range(1,4):
+            grid[i,15,0] = 225
+            grid[i,16,0] = 225
+            grid[i,17,0] = 225
+            grid[i,18,0] = 225
+            grid[i,19,0] = 225
+        grid[4,16,0] = 225
+        grid[4,17,0] = 225
+        grid[4,18,0] = 225
+        
+        # Label a soma
+        grid[19,19,0] = 255
+        # Add this back in later. For simplicity, only the 0th layer of grid has labels
+        #grid[:,:,1] = grid[:,:,0] 
+        # Label the components
+        labels, num = measure.label(grid, return_num=True)
+        plt.imshow(labels[:,:,0])
+        # We'll have the bottom-right corner, which is labeled 4, be the soma
+        somas = {4:[(19,19,0)]}
+    
+    return grid, labels, num, somas
+
+img, lbls, _ , somas = grid_gen(10)
+alg = viterbi_algorithm(img, lbls, somas, [1,1,1])
+# For the timebeing, manually do the endpoints
+# NOTE: no endpoints for 2 or 5 because they are "blobs"
+endpoints = {}
+endpoints[1] = ((0,0,0),(0,2,0))
+endpoints[3] = ((3,3,0),(6,5,0))
+endpoints[4] = ((7,7,0),(7,8,0))
+#alg.end_points = alg.frags_to_lines_le_skel()
+alg.end_points = endpoints
+
+img2, lbls2, _ , somas2 = grid_gen(20)
+alg2 = viterbi_algorithm(img2, lbls2, somas2, [1,1,1])
+
+# I tried doing spectral embedding, the endpoints produced were not correct
+alg2.frags_to_lines_le_skel([1,4])
+print(alg2.end_points)
+
+# For the timebeing, manually do the endpoints
+# NOTE: no endpoints for 1 or 4 because they are blobs
+endpoints2 = {}
+endpoints2[2] = ((1,1,0),(9,9,0))
+endpoints2[3] = ((12,12,0),(15,15,0))
+alg2.end_points = endpoints2
 
 testInit()
 testLineToLine()
-testLineToSoma()
+testLineToBlob()
+testPathIntensityCost()
+testConnections()
