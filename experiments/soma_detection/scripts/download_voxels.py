3b559474a596979710b9988247f3bb46ad6dde5a
Jacopo Teneggi <jtenegg1@jh.edu>
2021-03-11 13:21:48 -0500

    Update download_voxels. Add visual inspection

--- experiments/soma_detection/scripts/download_voxels.py
+++ experiments/soma_detection/scripts/download_voxels.py
@@ -11,10 +11,12 @@ import json
 import time
 import pandas as pd
 
+radius = 150
+
 cwd = Path(os.path.abspath(__file__))
 exp_dir = cwd.parents[1]
 data_dir = os.path.join(exp_dir, "data")
-tmp_npy = os.path.join(data_dir, "tmp.npy")
+volumes_dir = os.path.join(data_dir, "volumes", str(radius))
 print(f"Downloading voxels to {data_dir}")
 
 brains = [1, 2]
@@ -38,12 +40,12 @@ for brain in brains:
     brain_url = f"s3://open-neurodata/{brain_prefix}"
     segments_url = f"s3://open-neurodata/{segments_prefix}"
 
-    ngl_sess = NeuroglancerSession(mip=0, url=brain_url, url_segments=segments_url)
+    ngl_sess = NeuroglancerSession(mip=1, url=brain_url, url_segments=segments_url)
 
-    for i, seg_obj in enumerate(bucket.objects.filter(Prefix=skeletons_prefix)):
+    for seg_obj in bucket.objects.filter(Prefix=skeletons_prefix):
         seg_id = os.path.basename(seg_obj.key)
         if seg_id != "info":
-            radius = 150
+            volume_filepath = os.path.join(volumes_dir, brain_name, f"{seg_id}.npy")
             print(
                 f"Pulling volume around root {seg_id} of radius {radius}...",
                 end="",
@@ -53,67 +55,82 @@ for brain in brains:
             img, bbox, vox = ngl_sess.pull_voxel(int(seg_id), 0, radius)
             t1 = time.time()
             dt = np.around(t1 - t0, decimals=3)
-            print(f"done in {dt}s")
+            print(f"done in {dt}s {bbox}")
+            np.save(volume_filepath, img, allow_pickle=True)
+            
+            # print(f"Uploading root volume to AzureBlobStorage...", end="", flush=True)
+            # t0 = time.time()
+            # with open(tmp_npy, "rb") as data:
+            #     blob_name = f"soma-detection/{brain_name}_{seg_id}.npy"
+            #     blob_client = blob_service_client.get_blob_client(
+            #         container=container_name, blob=blob_name
+            #     )
+            #     blob_client.upload_blob(data, overwrite=True)
+            # d.append(
+            #     {
+            #         "brain": brain_name,
+            #         "seg_id": seg_id,
+            #         "channel": "image",
+            #         "label": True,
+            #         "filepath": blob_name,
+            #         "intensity": np.sum(img.flatten()),
+            #     }
+            # )
+            # t1 = time.time()
+            # dt = np.around(t1 - t0, decimals=3)
+            # print(f"done in {dt}s")
 
             pts = bbox.to_list()
             size = bbox.size3()
 
-            # find first y_positive neighbor
-            pts[1] += size[1]
-            pts[4] += size[1]
-            neighbor_bbox = Bbox(pts[:3], pts[3:])
-            print(f"Pulling neighbor...", end="", flush=True)
-            t0 = time.time()
-            neigh_img = ngl_sess.pull_bounds_img(neighbor_bbox)
-            t1 = time.time()
-            dt = np.around(t1 - t0, decimals=3)
-            print(f"done in {dt}s")
+            neighbor_dims = [(0, 3), (1, 4), (2, 5)]
+            neighbor_dirs = [1, -1] 
+            # find 6-connected neighbors
+            for i, neighbor_dim in enumerate(neighbor_dims):
+                m = neighbor_dim[0]
+                n = neighbor_dim[1]
+                for j, neighbor_dir in enumerate(neighbor_dirs):
+                    neigh_pts = pts.copy()
+                    neigh_id = i*2 + j
+                    neighvolume_filepath = os.path.join(volumes_dir, brain_name, f"{seg_id}_neigh{neigh_id}.npy")
+                    neigh_pts[m] += neighbor_dir * size[i]
+                    neigh_pts[n] += neighbor_dir * size[i]
+                    
+                    # handle possible negative coordinates
+                    neigh_pts = [max(0, c) for c in neigh_pts]
+                    
+                    neighbor_bbox = Bbox(neigh_pts[:3], neigh_pts[3:])
+                    print(f"Pulling neighbor #{neigh_id} {neighbor_bbox}...", end="", flush=True)
+                    t0 = time.time()
+                    neigh_img = ngl_sess.pull_bounds_img(neighbor_bbox)
+                    t1 = time.time()
+                    dt = np.around(t1 - t0, decimals=3)
+                    print(f"done in {dt}s")
+                    np.save(neighvolume_filepath, neigh_img, allow_pickle=True)
 
-            print(f"Uploading root volume to AzureBlobStorage...", end="", flush=True)
-            t0 = time.time()
-            np.save(tmp_npy, img, allow_pickle=True)
-            with open(tmp_npy, "rb") as data:
-                blob_name = f"soma-detection/{brain_name}_{seg_id}.npy"
-                blob_client = blob_service_client.get_blob_client(
-                    container=container_name, blob=blob_name
-                )
-                blob_client.upload_blob(data, overwrite=True)
-            d.append(
-                {
-                    "seg_id": seg_id,
-                    "channel": "image",
-                    "label": True,
-                    "filepath": blob_name,
-                    "intensity": np.sum(img.flatten()),
-                }
-            )
-            t1 = time.time()
-            dt = np.around(t1 - t0, decimals=3)
-            print(f"done in {dt}s")
-
-            print(
-                f"Uploading neighbor volume to AzureBlobStorage...", end="", flush=True
-            )
-            t0 = time.time()
-            np.save(tmp_npy, neigh_img, allow_pickle=True)
-            with open(tmp_npy, "rb") as data:
-                blob_name = f"soma-detection/{brain_name}_{seg_id}_neighbor.npy"
-                blob_client = blob_service_client.get_blob_client(
-                    container=container_name, blob=blob_name
-                )
-                blob_client.upload_blob(data, overwrite=True)
-            d.append(
-                {
-                    "seg_id": seg_id,
-                    "channel": "image",
-                    "label": False,
-                    "filepath": blob_name,
-                    "intensity": np.sum(neigh_img.flatten()),
-                }
-            )
-            t1 = time.time()
-            dt = np.around(t1 - t0, decimals=3)
-            print(f"done in {dt}s")
+                    # print(
+                    #     f"Uploading neighbor volume to AzureBlobStorage...", end="", flush=True
+                    # )
+                    # t0 = time.time()
+                    # with open(tmp_npy, "rb") as data:
+                    #     blob_name = f"soma-detection/{brain_name}_{seg_id}_neighbor{neigh_id}.npy"
+                    #     blob_client = blob_service_client.get_blob_client(
+                    #         container=container_name, blob=blob_name
+                    #     )
+                    #     blob_client.upload_blob(data, overwrite=True)
+                    # d.append(
+                    #     {
+                    #         "brain": brain_name,
+                    #         "seg_id": seg_id,
+                    #         "channel": "image",
+                    #         "label": False,
+                    #         "filepath": blob_name,
+                    #         "intensity": np.sum(neigh_img.flatten()),
+                    #     }
+                    # )
+                    # t1 = time.time()
+                    # dt = np.around(t1 - t0, decimals=3)
+                    # print(f"done in {dt}s")
 
 dataset = pd.DataFrame(d)
 dataset.to_csv(os.path.join(data_dir, "dataset.csv"))
