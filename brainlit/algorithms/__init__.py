666db59499b2ce2c40d553635b1e88e12dc6ca4b
Thomas Athey <tom.l.athey@gmail.com>
2021-04-06 11:54:41 -0400

    Update __init__.py

--- brainlit/algorithms/__init__.py
+++ brainlit/algorithms/__init__.py
@@ -1,3 +1,7 @@
+import brainlit.algorithms.generate_fragments
+import brainlit.algorithms.connect_fragments
+import brainlit.algorithms.trace_analysis
+
 from brainlit.algorithms.generate_fragments import *
 from brainlit.algorithms.connect_fragments import *
 from brainlit.algorithms.trace_analysis import *
