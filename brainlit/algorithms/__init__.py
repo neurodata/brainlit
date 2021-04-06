10580065f498a14a8ca339eefa375eba6cbf71b0
Thomas Athey <tom.l.athey@gmail.com>
2021-04-06 11:37:45 -0400

    modify inits to build alg docs

--- brainlit/algorithms/__init__.py
+++ brainlit/algorithms/__init__.py
@@ -1,7 +1,3 @@
-import brainlit.algorithms.generate_fragments
-import brainlit.algorithms.connect_fragments
-import brainlit.algorithms.trace_analysis
-
 from brainlit.algorithms.generate_fragments import *
 from brainlit.algorithms.connect_fragments import *
 from brainlit.algorithms.trace_analysis import *
