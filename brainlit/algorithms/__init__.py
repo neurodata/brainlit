b092310a29a1869850d8769583016de6cb47f880
Thomas Athey <tom.l.athey@gmail.com>
2021-04-27 15:57:35 -0400

    modify inits

--- brainlit/algorithms/__init__.py
+++ brainlit/algorithms/__init__.py
@@ -1,3 +1,6 @@
+from brainlit.algorithms import generate_fragments
 from brainlit.algorithms.generate_fragments import *
+from brainlit.algorithms import connect_fragments
 from brainlit.algorithms.connect_fragments import *
+from brainlit.algorithms import trace_analysis
 from brainlit.algorithms.trace_analysis import *
--- brainlit/algorithms/generate_fragments/__init__.py
+++ brainlit/algorithms/generate_fragments/__init__.py
@@ -1,1 +1,2 @@
-
+from brainlit.algorithms.generate_fragments.tube_seg import *
+from brainlit.algorithms.generate_fragments.adaptive_thresh import *
