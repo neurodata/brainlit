3c2c9aa00b685f51532c0ad4bcdb7a6039522855
Chih-Ting Chen <chih.ting1992tw@gmail.com>
2021-04-30 16:39:50 -0400

    add test_detect_output()

--- brainlit/algorithms/detect_somas/tests/test_detect.py
+++ brainlit/algorithms/detect_somas/tests/test_detect.py
@@ -1,5 +1,9 @@
 import numpy as np
 from brainlit.algorithms.detect_somas import find_somas
+import brainlit
+from brainlit.utils.session import NeuroglancerSession
+from cloudvolume.lib import Bbox
+import os
 from pytest import raises
 
 ##############
@@ -43,3 +47,34 @@ def test_find_somas_bad_input():
     res = [100, 100.0, 0]
     with raises(ValueError, match=r"Resolution must be non-zero at every position"):
         find_somas(volume, res)
+
+
+##################
+### validation ###
+##################
+
+
+def test_detect_output():
+    # download a volume
+    dir = "s3://open-neurodata/brainlit/brain1"
+    dir_segments = "s3://open-neurodata/brainlit/brain1_segments"
+    volume_keys = "4807349.0_3827990.0_2922565.75_4907349.0_3927990.0_3022565.75"
+    mip = 3
+    ngl_sess = NeuroglancerSession(
+        mip=mip, url=dir, url_segments=dir_segments, use_https=False
+    )
+    res = ngl_sess.cv_segments.scales[ngl_sess.mip]["resolution"]
+    volume_coords = np.array(os.path.basename(volume_keys).split("_")).astype(float)
+    volume_vox_min = np.round(np.divide(volume_coords[:3], res)).astype(int)
+    volume_vox_max = np.round(np.divide(volume_coords[3:], res)).astype(int)
+    bbox = Bbox(volume_vox_min, volume_vox_max)
+    img = ngl_sess.pull_bounds_img(bbox)
+    # apply soma detector
+    label, rel_centroids, out = find_somas(img, res)
+    # check output type
+    assert type(label) == bool
+    assert type(rel_centroids) == np.ndarray
+    assert type(out) == np.ndarray
+    # check output dimension
+    assert rel_centroids.shape[1] == 3
+    assert out.shape == (160, 160, 50)
