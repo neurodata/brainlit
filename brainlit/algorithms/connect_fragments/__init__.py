5bfc414649370a098dbd662085e9ab906fc1d28c
fxu10 <fxu10@jhu.edu>
2021-05-03 14:17:52 -0400

    Fixed a naming error in the init file

--- brainlit/algorithms/connect_fragments/__init__.py
+++ brainlit/algorithms/connect_fragments/__init__.py
@@ -1,3 +1,1 @@
-import brainlit.algorithms.connect_fragments
-
-from brainlit.algorithms.connect_fragments.dynamic_pogramming_viterbi import *
+from brainlit.algorithms.connect_fragments.dynamic_programming_viterbi import *
