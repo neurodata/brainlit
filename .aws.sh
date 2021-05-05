3719cf473e262b9897ce964fd7dd9e347b62df99
alishakodibagkar <akodibagkar@gmail.com>
2021-05-05 11:52:33 -0400

    add .aws.sh

---
+++ .aws.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/.cloudvolume
+mkdir -p ~/.cloudvolume/secrets
+
+cat > ~/.cloudvolume/secrets/aws-secret.json << EOL
+{
+    "AWS_ACCESS_KEY_ID": "${AWS_KEY}",
+    "AWS_SECRET_ACCESS_KEY": "${AWS_SEC_KEY}"
+}
+EOL
+
