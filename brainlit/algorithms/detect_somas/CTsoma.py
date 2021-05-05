48ffe6330c2dedca0d7a2e15c9fdef05059b6a24
FelixChihTing <chih.ting1992tw@gmail.com>
2021-05-05 11:10:13 -0400

    add CTsoma.py

--- brainlit/algorithms/detect_somas/CTsoma.py
+++ brainlit/algorithms/detect_somas/CTsoma.py
@@ -10,46 +10,34 @@ from pytest import raises
 from pathlib import Path
 from brainlit.utils.session import NeuroglancerSession
 from cloudvolume.lib import Bbox
-
-# # download a volume
-# dir = "s3://open-neurodata/brainlit/brain1"
-# dir_segments = "s3://open-neurodata/brainlit/brain1_segments"
-# volume_keys = "4807349.0_3827990.0_2922565.75_4907349.0_3927990.0_3022565.75"
-# mip = 1
-# ngl_sess = NeuroglancerSession(mip=mip, url=dir, url_segments=dir_segments, use_https=False)
-# res = ngl_sess.cv_segments.scales[ngl_sess.mip]["resolution"]
-# volume_coords = np.array(os.path.basename(volume_keys).split("_")).astype(float)
-# volume_vox_min = np.round(np.divide(volume_coords[:3], res)).astype(int)
-# volume_vox_max = np.round(np.divide(volume_coords[3:], res)).astype(int)
-# bbox = Bbox(volume_vox_min, volume_vox_max)
-# img = ngl_sess.pull_bounds_img(bbox)
-# # apply soma detector
-# label, rel_centroids, out = find_somas(img, res)
+import glob
+import napari
 
 brain_url = "s3://open-neurodata/brainlit/brain1"
 segments_url = "s3://open-neurodata/brainlit/brain1_segments"
-ngl_sess = NeuroglancerSession(mip=1, url=brain_url, url_segments=segments_url)
-ImgPath = glob.glob('soma_detection/data/brain1/volumes/*.npy')
+ngl_sess = NeuroglancerSession(mip=1, url=brain_url, url_segments=segments_url,use_https=False)
+ImgPath = glob.glob('experiments/soma_detection/data/brain1/volumes/*.npy') # open directory folder to brainlit
 hit = np.zeros(1)    # True number = predicted number\n",
 miss = np.zeros(1)   # True number > prediceted number\n",
 FalsePos = np.zeros(1)   # True number < predicted number\n",
 TotalSoma = 0   # initiate total number of soma\n",
-
 NumSub = len(ImgPath)
-for item in range(NumSub):
-    print(item)
+for item in range(26,27,1):
     volume_coords = np.array(os.path.basename(ImgPath[item]).replace('.npy','').split("_")).astype(float).round().astype(int).tolist()
     res = ngl_sess.cv_segments.scales[ngl_sess.mip]["resolution"]
     _min = volume_coords[:3]
     _max = volume_coords[3:]
     vox_min = np.round(np.divide(_min, res)).astype(int)
     vox_max = np.round(np.divide(_max, res)).astype(int)
-    # obtain soma coordinate\n",
-    contained_coords = np.load(ImgPath[item])
-    # true number of soma in subvolume\n",
-    TrueNumSo = len(contained_coords)
     bbox = Bbox(vox_min, vox_max)
-    img = ngl_sess.pull_bounds_img(bounds=bbox)
-    # with napari.gui_qt():\n",
-    #     viewer = napari.Viewer(ndisplay=3)\n",
-    #     napari.view_image(img)\n",
+    try:
+        img = ngl_sess.pull_bounds_img(bounds=bbox)
+        label,rel_centroids,out = find_somas(img, res)
+        print('Item#',item,'label=',label,'rel_centroids=',rel_centroids)
+        with napari.gui_qt():
+            viewer = napari.view_image(img, name='Original Image', colormap='red',ndisplay=3)
+            viewer.add_labels(out, name='find_soma'+str(item), num_colors=2, opacity=0.3)
+            viewer.add_points(rel_centroids,size=5,symbol="o",face_color=np.array([0.67, 0.7, 1, 0.5]))
+    except:
+        print('Disrupted at item',item)
+    
