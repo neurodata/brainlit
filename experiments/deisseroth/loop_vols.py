4fff40af1e3d471fb67c8bec22426678b6fa1f68
Thomas Athey <tom.l.athey@gmail.com>
2021-09-17 10:57:54 -0400

    path

--- experiments/deisseroth/loop_vols.py
+++ experiments/deisseroth/loop_vols.py
@@ -12,7 +12,7 @@ import os
 
 chunk_size = [256, 256, 300]
 ncpu = 12
-dir = "s3://smartspim-precomputed-volumes/2021_07_01_Sert_Cre_R/Ch_647"
+dir = "s3://smartspim-precomputed-volumes/2021_07_15_Sert_Cre_R/Ch_647"
 progress_file = "/home/tathey1/progress.txt" #"/Users/thomasathey/Documents/mimlab/mouselight/ailey/benchmark_formal/brain4/tracing/progress.txt" 
 files_dir = "/data/tathey1/matt_wright/brain4/tracing/"
 
