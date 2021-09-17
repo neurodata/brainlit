359d88ff53a5987249527d1b3b5f3727fdd8791e
Thomas Athey <tom.l.athey@gmail.com>
2021-09-17 10:35:00 -0400

    shape stuff

--- experiments/deisseroth/loop_vols.py
+++ experiments/deisseroth/loop_vols.py
@@ -70,8 +70,8 @@ def process_chunk(i, j, k):
     pred = pred[1,:,:,:]
 
     mask = np.array(pred > 0.5).astype('uint64')
-    print(f"{k}: {np.sum(mask)}")
-    vol_mask[i:i2,j:j2,k:k2] = mask
+    print(f"{k}: {np.sum(mask)}, {mask.shape}")
+    vol_mask[i:i2,j:j2,k:k2,0] = mask
 
 mip = 0
 vol = CloudVolume(dir, parallel=True, mip=mip, fill_missing=True)
