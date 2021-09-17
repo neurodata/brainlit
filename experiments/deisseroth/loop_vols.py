2aa9c082ff5a9a161b010d4a7d39d5a052706798
Thomas Athey <tom.l.athey@gmail.com>
2021-09-17 10:47:18 -0400

    wrong paths

--- experiments/deisseroth/loop_vols.py
+++ experiments/deisseroth/loop_vols.py
@@ -8,11 +8,13 @@ from tqdm import tqdm
 import h5py
 from joblib import Parallel, delayed
 import multiprocessing
+import os
 
 chunk_size = [256, 256, 300]
 ncpu = 12
 dir = "s3://smartspim-precomputed-volumes/2021_07_01_Sert_Cre_B/Ch_647"
 progress_file = "/home/tathey1/progress.txt" #"/Users/thomasathey/Documents/mimlab/mouselight/ailey/benchmark_formal/brain4/tracing/progress.txt" 
+files_dir = "/data/tathey1/matt_wright/brain4/tracing/"
 
 with open(progress_file) as f:
     for line in f:
@@ -34,16 +36,16 @@ def process_chunk(i, j, k):
     chunk_size = [256, 256, 300]
     mip = 0
     
-    dir_mask = "s3://smartspim-precomputed-volumes/2021_07_01_Sert_Cre_B/axon_mask"
+    dir_mask = "s3://smartspim-precomputed-volumes/2021_07_15_Sert_Cre_R/axon_mask"
     vol_mask = CloudVolume(dir_mask, parallel=1, mip=mip, fill_missing=True)
 
-    dir_fg = "s3://smartspim-precomputed-volumes/2021_07_01_Sert_Cre_B/Ch_647"
+    dir_fg = "s3://smartspim-precomputed-volumes/2021_07_15_Sert_Cre_R/Ch_647"
     vol_fg = CloudVolume(dir_fg, parallel=1, mip=mip, fill_missing=True)
 
-    dir_bg = "s3://smartspim-precomputed-volumes/2021_07_01_Sert_Cre_B/Ch_561"
+    dir_bg = "s3://smartspim-precomputed-volumes/2021_07_15_Sert_Cre_R/Ch_561"
     vol_bg = CloudVolume(dir_bg, parallel=1, mip=mip, fill_missing=True)
 
-    dir_endo = "s3://smartspim-precomputed-volumes/2021_07_01_Sert_Cre_B/Ch_488"
+    dir_endo = "s3://smartspim-precomputed-volumes/2021_07_15_Sert_Cre_R/Ch_488"
     vol_endo = CloudVolume(dir_endo, parallel=1, mip=mip, fill_missing=True)
 
     shape = vol_fg.shape
@@ -84,4 +86,7 @@ for i in tqdm(range(coords[0], shape[0], chunk_size[0])):
         with open(progress_file, 'a') as f:
             f.write('\n')
             f.write(f'{i} {j}')
+        for f in os.listdir(files_dir):
+            os.remove(os.path.join(dir, f))
+        raise ValueError()
 
