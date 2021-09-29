8b59f290315f6b978181e9c511fbf3d301913530
Thomas Athey <tom.l.athey@gmail.com>
2021-09-29 17:12:11 -0400

    tracing

--- experiments/napari_gui/gui_volume.py
+++ experiments/napari_gui/gui_volume.py
@@ -77,6 +77,18 @@ viewer.add_image(viterbi.image_raw, name="image")
 labels_layer = viewer.add_labels(new_labels, name="labels")
 viewer.camera.angles = [0, -90, 180]
 
+def get_layer_labels():
+    labels = []
+    label2state = {}
+    if len(viewer.layers) > 2:
+        for i in range(2,len(viewer.layers)):
+            label_name = viewer.layers[i].name.split(" ")
+            label = int(label_name[1])
+            state = int(label_name[3])
+            labels.append(label)
+            label2state[label] = state
+    return labels, label2state
+
 @labels_layer.mouse_drag_callbacks.append
 def get_connected_component_shape(layer, event):
     data_coordinates = layer.world_to_data(event.position)
@@ -88,30 +100,73 @@ def get_connected_component_shape(layer, event):
         world=True)
     if val is None:
         return
-    if val != 0:
-        print(viewer.layers)
-        print(len(viewer.layers))
-        while len(viewer.layers) > 2:
-            viewer.layers.pop(-1)
-        print(viewer.layers)
-        print(len(viewer.layers))
+    if val != 0: 
+        existing_labels, label2state = get_layer_labels()
 
-        
-        
-        start1 = viterbi.comp_to_states[val][0]
-        pt1 = viterbi.state_to_comp[start1][2]["coord1"]
-        pt2 = viterbi.state_to_comp[start1][2]["coord2"]
-        print(f"pt1: {pt1}")
-        print(f"pt2: {pt2}")
-        viewer.add_image(viterbi.image_raw)
-        print("here")
-        viewer.add_points([pt1], face_color='red', size=7)
-        viewer.add_points([pt2], face_color='orange', size=7)
+        if val in existing_labels and label2state[val] == 0:
+            state_num = 1
+        else:
+            state_num = 0
+
+        state = viterbi.comp_to_states[val][state_num]
+        pt1 = viterbi.state_to_comp[state][2]["coord1"]
+        pt2 = viterbi.state_to_comp[state][2]["coord2"]
+        viewer.add_points([pt1], face_color='red', size=7, name=f"label {val} state {state_num} start")
+        viewer.add_points([pt2], face_color='orange', size=7, name=f"label {val} state {state_num} end")
         msg = (
-            f'clicked at {cords} on blob {val} which is now is displaying endpoints'
+            f'clicked at {cords} on component {val} which is now is displaying endpoints'
         )
+        print(msg)
+
+        if val in existing_labels:
+            existing_labels.reverse()
+            n = len(viewer.layers)
+            for i, existing_label in enumerate(existing_labels):
+                if val == existing_label:
+                    viewer.layers.pop(n-i-3)
+
+
+def drawpath(state1, state2):
+    path_states = nx.shortest_path(viterbi.nxGraph, state1, state2, weight='weight')
+
+    path_comps = []
+    for state in path_states:
+        path_comps.append(viterbi.state_to_comp[state][1])
+    print(f'path sequence: {path_states}')
+    print(f'component sequence: {path_comps}')
+
+    path_mask = 0*new_labels
+    for i, label in enumerate(path_comps):
+        path_mask[new_labels == label] = i+1
+
+    lines = []
+    cumul_cost = 0
+    for s, state in enumerate(path_states):
+        if s>0:
+            dist_cost = viterbi.cost_mat_dist[path_states[s-1], state]
+            int_cost = viterbi.cost_mat_int[path_states[s-1], state]
+            cumul_cost += dist_cost + int_cost
+            print(f"Trans. #{s}: dist cost state {path_states[s-1]}->state {state}, comp {viterbi.state_to_comp[path_states[s-1]][1]}->comp {viterbi.state_to_comp[state][1]}: {dist_cost:.2f}, int cost: {int_cost:.2f}, cum. cost: {cumul_cost:.2f}")
+        if viterbi.state_to_comp[state][0] == "fragment":
+            if s>0:
+                lines.append(list(viterbi.state_to_comp[state][2]["coord1"]))
+            lines.append(list(viterbi.state_to_comp[state][2]["coord2"]))
+        elif viterbi.state_to_comp[path_states[s-1]][0] == "fragment":
+            lines.append(list(viterbi.state_to_comp[path_states[s-1]][2]["soma connection point"])) 
+
+    return lines
+
+@viewer.bind_key('a')
+def accept_image(viewer):
+    if len(viewer.layers) > 4:
+        existing_labels, label2state = get_layer_labels()
+        existing_labels = np.unique(existing_labels)
+        state1 = viterbi.comp_to_states[existing_labels[0]][label2state[existing_labels[0]]]
+        state2 = viterbi.comp_to_states[existing_labels[1]][label2state[existing_labels[1]]]
+
+        lines = drawpath(state1, state2)
+        viewer.add_shapes(lines, shape_type="path", edge_color="blue", edge_width=1)
     else:
-        msg = f'clicked at {cords} on background which is ignored'
-    print(msg)
+        print("Not enough states selected")
     
 napari.run()
