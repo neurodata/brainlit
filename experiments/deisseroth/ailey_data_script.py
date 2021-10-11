91435ac4f8834648873cddf5c3f9155430019bfe
Thomas Athey <tom.l.athey@gmail.com>
2021-10-11 16:45:54 -0400

    mip1

--- experiments/deisseroth/ailey_data_script.py
+++ experiments/deisseroth/ailey_data_script.py
@@ -13,11 +13,11 @@ from parse_ara import *
 import networkx as nx
 
 dir = "precomputed://https://dlab-colm.neurodata.io/2021_07_15_Sert_Cre_R/axon_mask"
-vol_mask_ds = CloudVolume(dir, parallel=1, mip=2, fill_missing=False)
+vol_mask_ds = CloudVolume(dir, parallel=1, mip=1, fill_missing=False)
 print(vol_mask_ds.shape)
 
 data = vol_mask_ds[:,:,:,:]
 data = data[:,:,:,0]
 data = np.swapaxes(data, 0,2)
 
-io.imsave("/data/tathey1/matt_wright/brain4/axon_mask_2.tif", data)
+io.imsave("/data/tathey1/matt_wright/brain4/axon_mask_1.tif", data)
