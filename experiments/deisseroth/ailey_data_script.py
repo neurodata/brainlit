6b6953989189f8648797f76aa0dd048b589c36a2
Thomas Athey <tom.l.athey@gmail.com>
2021-10-13 22:15:40 -0400

    mip1

--- experiments/deisseroth/ailey_data_script.py
+++ experiments/deisseroth/ailey_data_script.py
@@ -1,3 +1,4 @@
+from numpy.core.numeric import full
 from cloudvolume import CloudVolume
 from skimage.transform import downscale_local_mean
 from skimage import io
@@ -11,13 +12,34 @@ from tqdm import tqdm
 import pickle
 from parse_ara import *
 import networkx as nx
+from tqdm import tqdm
+
+path_prefix = "/data/tathey1/matt_wright/brain4/"
+path_prefix = "/Users/thomasathey/Documents/mimlab/mouselight/ailey/wholebrain_results/brain4/register/"
 
 dir = "precomputed://https://dlab-colm.neurodata.io/2021_07_15_Sert_Cre_R/axon_mask"
-vol_mask_ds = CloudVolume(dir, parallel=1, mip=1, fill_missing=False)
+vol_mask_ds = CloudVolume(dir, parallel=1, mip=1, fill_missing=True)
 print(vol_mask_ds.shape)
 
-data = vol_mask_ds[:,:,:,:]
-data = data[:,:,:,0]
-data = np.swapaxes(data, 0,2)
+full_data = np.zeros(vol_mask_ds.shape, dtype='int8')
+full_data = np.squeeze(full_data)
+full_data = np.swapaxes(full_data, 0,2)
 
-io.imsave("/data/tathey1/matt_wright/brain4/axon_mask_1.tif", data)
+for i in tqdm(range(0, vol_mask_ds.shape[2], 10)):
+    z1 = i
+    z2 = i+10
+    if z2 >= vol_mask_ds.shape[2]:
+        z2 = vol_mask_ds.shape[2]
+    chunk = vol_mask_ds[:,:,z1:z2,0]
+    chunk = np.squeeze(chunk)
+    chunk = chunk.astype('int8')
+    chunk = np.swapaxes(chunk, 0,2)
+    full_data[z1:z2,:,:] = chunk
+
+    if i%600 == 0 and i>0:
+        print(f"Saving {i}")
+        io.imsave(path_prefix + "axon_mask_1_" + str(i) + ".tif", full_data)
+        
+
+
+io.imsave(path_prefix + "axon_mask_1.tif", full_data)
