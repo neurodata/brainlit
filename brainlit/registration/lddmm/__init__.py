c54525943faaeb739a257de0e8532b263846b8df
dallen <deallen@loyola.edu>
2021-10-19 15:12:44 -0400

    Editing __init__.py

--- brainlit/registration/lddmm/__init__.py
+++ brainlit/registration/lddmm/__init__.py
@@ -1,2 +1,2 @@
-from ._lddmm import lddmm_register, lddmm_transform_image, lddmm_transform_points
-from ._lddmm_utilities import resample
+from .lddmm import lddmm_register, lddmm_transform_image, lddmm_transform_points
+from .lddmm_utilities import resample
