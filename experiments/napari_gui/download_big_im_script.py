28fc9da0d2c4a85fabe1e58cea936c7273a2b664
Thomas Athey <tom.l.athey@gmail.com>
2021-10-25 11:14:10 -0400

    download

--- experiments/napari_gui/download_big_im_script.py
+++ experiments/napari_gui/download_big_im_script.py
@@ -1,26 +1,11 @@
 from cloudvolume import CloudVolume
-from cloudvolume.exceptions import SkeletonDecodeError
-from itertools import islice
-from brainlit.utils.session import NeuroglancerSession
-from skimage.filters import threshold_otsu, threshold_local
-from brainlit.preprocessing import split_frags, rename_states_consecutively, label_points
-from brainlit.algorithms.connect_fragments import most_probable_neuron_path
-from brainlit.algorithms.connect_fragments import trace_evaluation
-from brainlit.viz import Bresenham3D
-from cloudvolume import Skeleton
 
 dir = "s3://open-neurodata/brainlit/brain1"
-dir_segments = "s3://open-neurodata/brainlit/brain1_segments"
 
-ngl_sess = NeuroglancerSession(mip = 0, url = dir, url_segments=dir_segments)
+vol = CloudVolume(dir, parallel=1, mip=0, fill_missing=True)
 
-res = [0.3,0.3,1]
-threshold = 0.9
-
-block_size = 101
-
-print(f"downloading")
-img, bbox, vox = ngl_sess.pull_vertex_list(2, [0], [1666,1666,500])
+print("downloading")
+img = vol[11210:14542,10811:14143,3977:4977]
 
 print("saving")
 io.imsave("/data/tathey1/mouselight/1mm.tif", img)
