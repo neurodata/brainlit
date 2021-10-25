a518ac4de5efb2efd9ff0aac2c35a2ff014acc72
Thomas Athey <tom.l.athey@gmail.com>
2021-10-25 11:05:14 -0400

    remove imports

--- experiments/napari_gui/download_big_im_script.py
+++ experiments/napari_gui/download_big_im_script.py
@@ -1,34 +1,13 @@
 from cloudvolume import CloudVolume
 from cloudvolume.exceptions import SkeletonDecodeError
 from itertools import islice
-from skimage.transform import downscale_local_mean
-import napari
-from skimage import io
-import random
-import h5py
-from skimage import measure
-from brainlit.preprocessing import removeSmallCCs
-import numpy as np 
-import matplotlib.pyplot as plt 
-import subprocess
-import tables
-from napari_animation import AnimationWidget
-from tqdm import tqdm
-import pickle
-import networkx as nx
-import seaborn as sns
-import pandas as pd
-import brainrender
 from brainlit.utils.session import NeuroglancerSession
 from skimage.filters import threshold_otsu, threshold_local
 from brainlit.preprocessing import split_frags, rename_states_consecutively, label_points
 from brainlit.algorithms.connect_fragments import most_probable_neuron_path
 from brainlit.algorithms.connect_fragments import trace_evaluation
-import similaritymeasures
 from brainlit.viz import Bresenham3D
 from cloudvolume import Skeleton
-from sklearn.metrics import pairwise_distances_argmin_min
-import time
 
 dir = "s3://open-neurodata/brainlit/brain1"
 dir_segments = "s3://open-neurodata/brainlit/brain1_segments"
