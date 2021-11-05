1bf6242d777fdf0db3afdee258cfd74837e9e333
Thomas Athey <tom.l.athey@gmail.com>
2021-11-05 15:52:13 -0400

    count states

--- experiments/napari_gui/process_big_im_script.py
+++ experiments/napari_gui/process_big_im_script.py
@@ -15,90 +15,92 @@ import networkx as nx
 import time
 
 
-data_dir = "/data/tathey1/mouselight/"
-im_path = os.path.join(data_dir, "1.tif")
+# data_dir = "/data/tathey1/mouselight/"
+# im_path = os.path.join(data_dir, "1.tif")
 
-probs_path = os.path.join(data_dir, "1_Probabilities.h5")
-f = h5py.File(probs_path, 'r')
+# probs_path = os.path.join(data_dir, "1_Probabilities.h5")
+# f = h5py.File(probs_path, 'r')
 
 
-res = [0.3,0.3,1]
-soma_coords = [[1665, 1665, 499]]
+# res = [0.3,0.3,1]
+# soma_coords = [[1665, 1665, 499]]
 
-t1 = time.perf_counter()
-print("reading files")
+# t1 = time.perf_counter()
+# print("reading files")
 
-im_og = io.imread(im_path, plugin="tifffile")
-#im_processed = io.imread(probs_path, plugin="tifffile")
-im_processed = f.get('exported_data')
-im_processed = im_processed[:,:,:,1]
+# im_og = io.imread(im_path, plugin="tifffile")
+# #im_processed = io.imread(probs_path, plugin="tifffile")
+# im_processed = f.get('exported_data')
+# im_processed = im_processed[:,:,:,1]
 
-print(f"image shape: {im_og.shape}")
-print(f"read files in {time.perf_counter()-t1} seconds")
-t1 = time.perf_counter()
+# print(f"image shape: {im_og.shape}")
+# print(f"read files in {time.perf_counter()-t1} seconds")
+# t1 = time.perf_counter()
 
-threshold = 0.9
-labels = measure.label(im_processed > threshold)
+# threshold = 0.9
+# labels = measure.label(im_processed > threshold)
 
-print(f"labeled image in {time.perf_counter()-t1} seconds")
-t1 = time.perf_counter()
+# print(f"labeled image in {time.perf_counter()-t1} seconds")
+# t1 = time.perf_counter()
 
-new_labels = image_process.compute_frags(soma_coords, labels, im_processed, threshold, res, chunk_size=[400,400,400], ncpu=12)
+# new_labels = image_process.compute_frags(soma_coords, labels, im_processed, threshold, res, chunk_size=[400,400,400], ncpu=12)
 
-io.imsave("/data/tathey1/mouselight/1_labels.tif", new_labels)
+# io.imsave("/data/tathey1/mouselight/1_labels.tif", new_labels)
 
-print(f"made fragments in {time.perf_counter()-t1} seconds")
-print(f"{np.unique(new_labels).size} fragments")
-t1 = time.perf_counter()
+# print(f"made fragments in {time.perf_counter()-t1} seconds")
+# print(f"{np.unique(new_labels).size} fragments")
+# t1 = time.perf_counter()
 
-_, soma_lbls = image_process.label_points(new_labels, soma_coords, res)
-soma_lbl = soma_lbls[0]
-soma_mask = new_labels == soma_lbl
+# _, soma_lbls = image_process.label_points(new_labels, soma_coords, res)
+# soma_lbl = soma_lbls[0]
+# soma_mask = new_labels == soma_lbl
 
-mpnp = dynamic_programming_viterbi2.most_probable_neuron_path(image=im_og.astype(float), labels=new_labels, soma_lbls=soma_lbls, resolution=(0.3, 0.3, 1), coef_dist=10, coef_curv=1000)
+# mpnp = dynamic_programming_viterbi2.most_probable_neuron_path(image=im_og.astype(float), labels=new_labels, soma_lbls=soma_lbls, resolution=(0.3, 0.3, 1), coef_dist=10, coef_curv=1000)
 
-print(f"made viterbi object in {time.perf_counter()-t1} seconds")
-with open("/data/tathey1/mouselight/1_viterbi_begin.pkl", 'wb') as handle:
-    pickle.dump(mpnp, handle)
-t1 = time.perf_counter()
+# print(f"made viterbi object in {time.perf_counter()-t1} seconds")
+# with open("/data/tathey1/mouselight/1_viterbi_begin.pkl", 'wb') as handle:
+#     pickle.dump(mpnp, handle)
+# t1 = time.perf_counter()
 
-mpnp.frags_to_lines(ncpu=12)
+# mpnp.frags_to_lines(ncpu=12)
 
-print(f"made states in {time.perf_counter()-t1} seconds")
-with open("/data/tathey1/mouselight/1_viterbi_states.pkl", 'wb') as handle:
-    pickle.dump(mpnp, handle)
-t1 = time.perf_counter()
+# print(f"made states in {time.perf_counter()-t1} seconds")
+# with open("/data/tathey1/mouselight/1_viterbi_states.pkl", 'wb') as handle:
+#     pickle.dump(mpnp, handle)
+# t1 = time.perf_counter()
 
-mpnp.reset_dists(type="all")
-mpnp.compute_all_costs_dist_par(point_point_func=mpnp.point_point_dist, point_blob_func=mpnp.point_blob_dist, ncpu=12)
+# mpnp.reset_dists(type="all")
+# mpnp.compute_all_costs_dist_par(point_point_func=mpnp.point_point_dist, point_blob_func=mpnp.point_blob_dist, ncpu=12)
 
-print(f"made dist cost in {time.perf_counter()-t1} seconds")
-t1 = time.perf_counter()
+# print(f"made dist cost in {time.perf_counter()-t1} seconds")
+# t1 = time.perf_counter()
 
-mpnp.compute_all_costs_int_par(ncpu=12)
+# mpnp.compute_all_costs_int_par(ncpu=12)
 
-print(f"made int cost in {time.perf_counter()-t1} seconds")
-t1 = time.perf_counter()
+# print(f"made int cost in {time.perf_counter()-t1} seconds")
+# t1 = time.perf_counter()
 
-mpnp.create_nx_graph()
+# mpnp.create_nx_graph()
 
-with open("/data/tathey1/mouselight/1_viterbi_nx.pkl", 'wb') as handle:
-    pickle.dump(mpnp, handle)
+# with open("/data/tathey1/mouselight/1_viterbi_nx.pkl", 'wb') as handle:
+#     pickle.dump(mpnp, handle)
 
-print(f"made graph in {time.perf_counter()-t1} seconds")
+# print(f"made graph in {time.perf_counter()-t1} seconds")
 
 with open("/data/tathey1/mouselight/1_viterbi_nx.pkl", 'rb') as handle:
     mpnp = pickle.load(handle)
 t1 = time.perf_counter()
 
-path_states = nx.shortest_path(mpnp.nxGraph, 4417, 417, weight='weight')
+print(mpnp.num_states)
 
-print(f"computed path in {time.perf_counter()-t1} seconds")
-t1 = time.perf_counter()
+# path_states = nx.shortest_path(mpnp.nxGraph, 4417, 417, weight='weight')
 
-with open("/data/tathey1/mouselight/1_viterbi.pkl", 'wb') as handle:
-    pickle.dump(mpnp, handle)
+# print(f"computed path in {time.perf_counter()-t1} seconds")
+# t1 = time.perf_counter()
 
-print(f"saved viterbi in {time.perf_counter()-t1} seconds")
-t1 = time.perf_counter()
+# with open("/data/tathey1/mouselight/1_viterbi.pkl", 'wb') as handle:
+#     pickle.dump(mpnp, handle)
+
+# print(f"saved viterbi in {time.perf_counter()-t1} seconds")
+# t1 = time.perf_counter()
 
