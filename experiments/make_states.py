7b3fa23b41397e6d3c25c9c7a6c0403439fc3241
Thomas Athey <tom.l.athey@gmail.com>
2021-11-15 15:32:36 -0500

    chunk size

--- experiments/make_states.py
+++ experiments/make_states.py
@@ -1,4 +1,4 @@
 from brainlit.algorithms.generate_fragments import state_generation
 
-sg = state_generation("/data/tathey1/mouselight/1mm.zarr", "/home/tathey1/ilastik-1.3.3post3-Linux/run_ilastik.sh", "/data/tathey1/mouselight/octopus_exp.ilp")
+sg = state_generation("/data/tathey1/mouselight/1mm.zarr", "/home/tathey1/ilastik-1.3.3post3-Linux/run_ilastik.sh", "/data/tathey1/mouselight/octopus_exp.ilp", parallel=12)
 sg.predict("/data/tathey1/mouselight/data_bin/")
