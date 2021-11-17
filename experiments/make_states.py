3ed4e6c0971d0b1b66d7a5e9974fb90ae41a2c3c
Thomas Athey <tom.l.athey@gmail.com>
2021-11-17 17:40:17 -0500

    less verbosity

--- experiments/make_states.py
+++ experiments/make_states.py
@@ -7,15 +7,15 @@ sg = state_generation(
     "/home/tathey1/ilastik-1.3.3post3-Linux/run_ilastik.sh",
     "/data/tathey1/mouselight/octopus_exp.ilp",
     chunk_size=[300, 300, 300],
-    parallel=12)
+    parallel=12,
     prob_path="/data/tathey1/mouselight/1mm_probs.zarr")
     # fragment_path="/data/tathey1/mouselight/250_labels.zarr",
     # tiered_path="/data/tathey1/mouselight/250_tiered.zarr")
 print(f"create object in {time.perf_counter()-t1} seconds")
 
-t1 = time.perf_counter()
-sg.predict("/data/tathey1/mouselight/data_bin/")
-print(f"computed ilastik predictions in {time.perf_counter()-t1} seconds")
+# t1 = time.perf_counter()
+# sg.predict("/data/tathey1/mouselight/data_bin/")
+# print(f"computed ilastik predictions in {time.perf_counter()-t1} seconds")
 
 t1 = time.perf_counter()
 sg.compute_frags()
