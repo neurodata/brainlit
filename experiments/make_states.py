3c4fa2a9d2b4b70117934678adb613ebcdd1cd0a
Thomas Athey <tom.l.athey@gmail.com>
2021-11-17 13:18:06 -0500

    new states

--- experiments/make_states.py
+++ experiments/make_states.py
@@ -3,27 +3,27 @@ import time
 
 t1 = time.perf_counter()
 sg = state_generation(
-    "/data/tathey1/mouselight/250.zarr",
+    "/data/tathey1/mouselight/1mm.zarr",
     "/home/tathey1/ilastik-1.3.3post3-Linux/run_ilastik.sh",
     "/data/tathey1/mouselight/octopus_exp.ilp",
     chunk_size=[300, 300, 300],
-    parallel=12,
-    prob_path="/data/tathey1/mouselight/250_probs.zarr",
-    fragment_path="/data/tathey1/mouselight/250_labels.zarr",
-    tiered_path="/data/tathey1/mouselight/250_tiered.zarr")
+    parallel=12)
+    # prob_path="/data/tathey1/mouselight/250_probs.zarr",
+    # fragment_path="/data/tathey1/mouselight/250_labels.zarr",
+    # tiered_path="/data/tathey1/mouselight/250_tiered.zarr")
 print(f"create object in {time.perf_counter()-t1} seconds")
 
-# t1 = time.perf_counter()
-# sg.predict("/data/tathey1/mouselight/data_bin/")
-# print(f"computed ilastik predictions in {time.perf_counter()-t1} seconds")
+t1 = time.perf_counter()
+sg.predict("/data/tathey1/mouselight/data_bin/")
+print(f"computed ilastik predictions in {time.perf_counter()-t1} seconds")
 
-# t1 = time.perf_counter()
-# sg.compute_frags()
-# print(f"computed fragments in {time.perf_counter()-t1} seconds")
+t1 = time.perf_counter()
+sg.compute_frags()
+print(f"computed fragments in {time.perf_counter()-t1} seconds")
 
-# t1 = time.perf_counter()
-# sg.compute_image_tiered()
-# print(f"computed tiered image in {time.perf_counter()-t1} seconds")
+t1 = time.perf_counter()
+sg.compute_image_tiered()
+print(f"computed tiered image in {time.perf_counter()-t1} seconds")
 
 t1 = time.perf_counter()
 sg.compute_soma_lbls()
