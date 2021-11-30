5edce1dbcf1b354bd3da6007195ac956db2a3930
Thomas Athey <tom.l.athey@gmail.com>
2021-11-30 15:46:18 -0500

    tests

--- brainlit/algorithms/connect_fragments/tests/test_viterbrain.py
+++ brainlit/algorithms/connect_fragments/tests/test_viterbrain.py
@@ -7,26 +7,74 @@ from numpy.testing import (
     assert_array_equal,
 )
 
+G = nx.DiGraph()
+vb = ViterBrain(
+    G=G,
+    tiered_path="",
+    fragment_path="",
+    resolution=[1, 1, 1],
+    coef_curv=1,
+    coef_dist=1,
+    coef_int=1,
+)
+
 ####################
 ### input checks ###
 ####################
 
+
 def test_frag_frag_dist_bad_input():
     with pytest.raises(ValueError):
-        ViterBrain.frag_frag_dist(pt1=[0,0,0], orientation1=[1,1,0], pt2=[1,0,0], orientation2=[1,0,0])
+        vb.frag_frag_dist(
+            pt1=[0, 0, 0], orientation1=[1, 1, 0], pt2=[1, 0, 0], orientation2=[1, 0, 0]
+        )
     with pytest.raises(ValueError):
-        ViterBrain.frag_frag_dist(pt1=[0,0,0], orientation1=[1,0,0], pt2=[1,0,0], orientation2=[1,1,0])
+        vb.frag_frag_dist(
+            pt1=[0, 0, 0], orientation1=[1, 0, 0], pt2=[1, 0, 0], orientation2=[1, 1, 0]
+        )
     with pytest.raises(ValueError):
-        ViterBrain.frag_frag_dist(pt1=[0,0,0], orientation1=[np.nan,0,0], pt2=[1,0,0], orientation2=[1,1,0])
+        vb.frag_frag_dist(
+            pt1=[0, 0, 0],
+            orientation1=[np.nan, 0, 0],
+            pt2=[1, 0, 0],
+            orientation2=[1, 1, 0],
+        )
     with pytest.raises(ValueError):
-        ViterBrain.frag_frag_dist(pt1=[0,0,0], orientation1=[np.nan,0,0], pt2=[1,0,0], orientation2=[np.nan,1,0])
+        vb.frag_frag_dist(
+            pt1=[0, 0, 0],
+            orientation1=[np.nan, 0, 0],
+            pt2=[1, 0, 0],
+            orientation2=[np.nan, 1, 0],
+        )
 
-def test_frag_soma_dist_bad_input():
-    with pytest.raises(ValueError):
-        ViterBrain.frag_frag_dist(point=[0,0,0], orientation=[1,1,0], pt2=[1,0,0], orientation2=[1,0,0])
-    with pytest.raises(ValueError):
-        ViterBrain.frag_frag_dist(pt1=[0,0,0], orientation1=[1,0,0], pt2=[1,0,0], orientation2=[1,1,0])
-    with pytest.raises(ValueError):
-        ViterBrain.frag_frag_dist(pt1=[0,0,0], orientation1=[np.nan,0,0], pt2=[1,0,0], orientation2=[1,1,0])
-    with pytest.raises(ValueError):
-        ViterBrain.frag_frag_dist(pt1=[0,0,0], orientation1=[np.nan,0,0], pt2=[1,0,0], orientation2=[np.nan,1,0])
+
+############################
+### functionality checks ###
+############################
+
+
+def test_frag_frag_dist():
+    cost = vb.frag_frag_dist(
+        pt1=[0, 0, 0], orientation1=[1, 0, 0], pt2=[1, 0, 0], orientation2=[1, 0, 0]
+    )
+    assert cost == 1.0
+
+    cost = vb.frag_frag_dist(
+        pt1=[0, 0, 0], orientation1=[1, 0, 0], pt2=[2, 0, 0], orientation2=[1, 0, 0]
+    )
+    assert cost == 4.0
+
+    cost = vb.frag_frag_dist(
+        pt1=[0, 0, 0], orientation1=[1, 0, 0], pt2=[1, 0, 0], orientation2=[0, 1, 0]
+    )
+    assert cost == 1.5
+
+    cost = vb.frag_frag_dist(
+        pt1=[0, 0, 0], orientation1=[0, 1, 0], pt2=[1, 0, 0], orientation2=[0, 1, 0]
+    )
+    assert cost == 2.0
+
+    cost = vb.frag_frag_dist(
+        pt1=[0, 0, 0], orientation1=[1, 0, 0], pt2=[2, 0, 0], orientation2=[-1, 0, 0]
+    )
+    assert cost == np.inf
