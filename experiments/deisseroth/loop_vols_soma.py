ffeb59ae6e891d2d4285f0c6da2b81d0b522e60f
Thomas Athey <tom.l.athey@gmail.com>
2021-12-10 10:09:46 -0500

    index bug

--- experiments/deisseroth/loop_vols_soma.py
+++ experiments/deisseroth/loop_vols_soma.py
@@ -62,7 +62,7 @@ def process_chunk(i, j, k):
     f = h5py.File(data_dir + "image_" + str(k) + "_Probabilities.h5", "r")
     pred = f.get("exported_data")
 
-    pred = pred[:,:,:,0]
+    pred = pred[0,:,:,:]
 
     mask = pred > threshold
     labels = measure.label(mask)
