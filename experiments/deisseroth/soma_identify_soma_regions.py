4b70a581853cbcd10019896d1d35c3c3ca3c9525
Thomas Athey <tom.l.athey@gmail.com>
2022-02-09 11:48:41 -0500

    get regions r3

--- experiments/deisseroth/soma_identify_soma_regions.py
+++ experiments/deisseroth/soma_identify_soma_regions.py
@@ -6,24 +6,24 @@ import pickle
 brain = "r3"
 
 atlas_vol = CloudVolume("file:///mnt/data/Neuroglancer_Data/2021_12_02/8607/atlas_to_target/", parallel=1, mip=0, fill_missing=True)
+print(f"size: {atlas_vol.shape} ")
 somas = "/home/user/misc_tommy/somas_brain" + brain + ".txt"
 
 file1 = open(somas, 'r')
 lines = file1.readlines()
 
 coords = []
-for line in tqdm(lines):
+for line in tqdm(lines, desc="parsing coordinates"):
     if line != '\n':
         line = ' '.join(line.split())
         elements = line.split(",")
-        print(elements)
         coord = [elements[0][1:], elements[1], elements[2][:-1]]
         
         coord = [int(round(float(e.strip()))) for e in coord]
         coords.append(coord)
 
 dict = {}
-for coord in tqdm(coords):
+for coord in tqdm(coords, desc="identiifynig rois"):
     roi = atlas_vol[coord[0], coord[1], coord[2]]
     if roi not in dict.keys():
         dict[roi] = 1
