7f1c8ffc961b5737426ab9d4534e41fe959d7415
Thomas Athey <tom.l.athey@gmail.com>
2022-03-21 15:25:56 -0400

    clean up axon

--- experiments/deisseroth/scratch.py
+++ experiments/deisseroth/scratch.py
@@ -1,16 +1,9 @@
-from cloudvolume import CloudVolume
-import numpy as np
-
-path = "file:///mnt/data/Neuroglancer_data/2021_10_06/8557/Ch_647"
-vol = CloudVolume(path, parallel=1, mip=0, fill_missing=False)
-
-im = vol[2500:2540, 4900:4940, 3000:3040]
-
-print(f"647 sum: {np.sum(im)}")
-
-path = "file:///mnt/data/Neuroglancer_data/2021_10_06/8557/Ch_561"
-vol = CloudVolume(path, parallel=1, mip=0, fill_missing=False)
-
-im = vol[2500:2540, 4900:4940, 3000:3040]
-
-print(f"647 sum: {np.sum(im)}")
+[
+    [4781, 3581, 1861],
+    [2982, 482, 1841],
+    [2258, 6504, 1392],
+    [2884, 2434, 1392],
+    [1659, 4108, 1392],
+    [3457, 5082, 1392],
+    [2850, 5163, 3116],
+]
