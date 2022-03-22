968d162a42615637f14142b84b89fda44343df3b
Thomas Athey <tom.l.athey@gmail.com>
2022-03-22 16:02:48 -0400

    loop vols

--- experiments/deisseroth/axon_loop_vols.py
+++ experiments/deisseroth/axon_loop_vols.py
@@ -10,30 +10,31 @@ from joblib import Parallel, delayed
 import multiprocessing
 import os
 
+threshold = 0.5
 chunk_size = [256, 256, 300]
-ncpu = 12
 dir = "s3://smartspim-precomputed-volumes/2022_01_14/8613/Ch_647"
-progress_file = "/home/tathey1/progress_axon.txt" #"/Users/thomasathey/Documents/mimlab/mouselight/ailey/benchmark_formal/brain4/tracing/progress.txt" 
-files_dir = "/data/tathey1/matt_wright/brain5/tracing/"
+data_dir = "/data/tathey1/matt_wright/brain_temp/"
 
-with open(progress_file) as f:
-    for line in f:
-        pass
-    last_line = line
-
-coords_list = last_line.split(' ')
-coords = [int(coord) for coord in coords_list]
+coords = [0, 0]
 
 print(f"Number cpus: {multiprocessing.cpu_count()}")
 
 warnings.filterwarnings("ignore")
 
+mip = 0
+vol = CloudVolume(dir, parallel=True, mip=mip, fill_missing=True)
+shape = vol.shape
 
-def process_chunk(i, j, k):
-    data_dir = "/data/tathey1/matt_wright/brain5/tracing/"
-    #data_dir = "/Users/thomasathey/Documents/mimlab/mouselight/ailey/benchmark_formal/brain4/tracing/"
+corners = []
+for i in tqdm(range(coords[0], shape[0], chunk_size[0])):
+    for j in tqdm(range(coords[1], shape[1], chunk_size[1]), leave=False):  
+        for k in range(0,shape[2],chunk_size[2]):
+            c1 = [i,j,k]
+            c2 = [np.amin([shape[idx], c1[idx]+chunk_size[idx]]) for idx in range(3)]
+            corners.append([c1,c2])
+    coords[1] = 0
 
-    chunk_size = [256, 256, 300]
+def process_chunk(c1, c2, data_dir, threshold):
     mip = 0
 
     dir_base = "s3://smartspim-precomputed-volumes/2022_01_14/8613/"
@@ -50,47 +51,33 @@ def process_chunk(i, j, k):
     dir_endo = dir_base + "Ch_488"
     vol_endo = CloudVolume(dir_endo, parallel=1, mip=mip, fill_missing=True)
 
-    shape = vol_fg.shape
-
-    i2 = np.amin([i+chunk_size[0], shape[0]])
-    j2 = np.amin([j+chunk_size[1], shape[1]])
-    k2 = np.amin([k+chunk_size[2], shape[2]])
-    subvol_fg = np.squeeze(vol_fg[i:i2,j:j2,k:k2])
-    subvol_bg = np.squeeze(vol_bg[i:i2,j:j2,k:k2])
-    subvol_endo = np.squeeze(vol_endo[i:i2,j:j2,k:k2])
+    subvol_fg = np.squeeze(vol_fg[c1[0]:c2[0],c1[1]]:c2[1],c1[1]:c2[1]])
+    subvol_bg = np.squeeze(vol_bg[c1[0]:c2[0],c1[1]]:c2[1],c1[1]:c2[1]])
+    subvol_endo = np.squeeze(vol_endo[c1[0]:c2[0],c1[1]]:c2[1],c1[1]:c2[1]])
 
     image_3channel = np.stack([subvol_bg, subvol_fg, subvol_endo], axis=0)
 
-    fname = data_dir + "/image_" + str(k) + ".h5"
+    fname = data_dir + "/image_" + str(c1[0]) + "_" + str(c1[1]) + "_" + str(c1[2]) + ".h5"
     with h5py.File(fname, "w") as f:
         dset = f.create_dataset("image_3channel", data=image_3channel)
     
     subprocess.run(["/home/tathey1/ilastik-1.3.3post3-Linux/run_ilastik.sh", "--headless", "--project=/data/tathey1/matt_wright/ilastik/model1/matt_benchmark_formal_brain3.ilp", fname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     #subprocess.run(["/Applications/ilastik-1.3.3post3-OSX.app/Contents/ilastik-release/run_ilastik.sh", "--headless", "--project=/Users/thomasathey/Documents/mimlab/mouselight/ailey/benchmark_formal/brain3/matt_benchmark_formal_brain3.ilp", fname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    f = h5py.File(data_dir + "image_" + str(k) + "_Probabilities.h5", "r")
+    fname_prob = data_dir + "image_" + str(k) + "_Probabilities.h5"
+    f = h5py.File(fname_prob, "r")
     pred = f.get("exported_data")
 
     pred = pred[1,:,:,:]
 
-    mask = np.array(pred > 0.3).astype('uint64')
-    print(f"{k}: {np.sum(mask)}, {mask.shape}")
-    vol_mask[i:i2,j:j2,k:k2] = mask
+    mask = np.array(pred > threshold).astype('uint64')
+    vol_mask[c1[0]:c2[0],c1[1]:c2[1],c1[1]:c2[1]] = mask
 
-mip = 0
-vol = CloudVolume(dir, parallel=True, mip=mip, fill_missing=True)
-shape = vol.shape
+    os.remove(fname)
+    os.remove(fname_prob)
+    #print(f"Finished {c1}-{c2}")
 
 
-for i in tqdm(range(coords[0], shape[0], chunk_size[0])):
-    for j in tqdm(range(coords[1], shape[1], chunk_size[1]), leave=False):    
-        Parallel(n_jobs=ncpu)(delayed(process_chunk)(i,j,k) for k in range(0,shape[2],chunk_size[2]))
-        with open(progress_file, 'a') as f:
-            f.write('\n')
-            f.write(f'{i} {j}')
-        for f in os.listdir(files_dir):
-            os.remove(os.path.join(files_dir, f))
-    
-    coords[1] = 0
+Parallel(n_jobs=-5)(delayed(process_chunk)(corner[0],corner[1], data_dir, threshold) for corner in tqdm(corners))
         
 
