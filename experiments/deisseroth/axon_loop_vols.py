152957e7e9d5400ebea6547667d31eb2f890edc7
Thomas Athey <tom.l.athey@gmail.com>
2022-04-01 12:03:38 -0400

    ds

--- experiments/deisseroth/axon_loop_vols.py
+++ experiments/deisseroth/axon_loop_vols.py
@@ -79,7 +79,7 @@ def process_chunk(c1, c2, data_dir, threshold, dir_base):
 for corners_chunk in tqdm(corners_chunks, desc="corner chunks"):
     # for corner in tqdm(corners_chunk):
     #      process_chunk(corner[0],corner[1], data_dir, threshold)
-    Parallel(n_jobs=17)(delayed(process_chunk)(corner[0],corner[1], data_dir, threshold, dir_base) for corner in tqdm(corners_chunk, leave=False))
+    Parallel(n_jobs=16)(delayed(process_chunk)(corner[0],corner[1], data_dir, threshold, dir_base) for corner in tqdm(corners_chunk, leave=False))
     for f in os.listdir(data_dir):
         os.remove(os.path.join(data_dir, f))
 
