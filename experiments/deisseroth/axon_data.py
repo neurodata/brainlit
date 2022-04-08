0948a5b06a78449a251afe5a35e387d779778e27
Thomas Athey <tom.l.athey@gmail.com>
2022-04-08 14:25:03 -0400

    updates

--- experiments/deisseroth/axon_data.py
+++ experiments/deisseroth/axon_data.py
@@ -19,6 +19,12 @@ brain2paths = {
         "bg" : "precomputed://https://dlab-colm.neurodata.io/2021_07_15_Sert_Cre_R/Ch_561",
         "endo" : "precomputed://https://dlab-colm.neurodata.io/2021_07_15_Sert_Cre_R/Ch_488",
         "mask" : "s3://smartspim-precomputed-volumes/2021_07_01_Sert_Cre_B/axon_mask"
+    },
+    "8613": {
+        "ab" : "precomputed://https://dlab-colm.neurodata.io/2022_01_14/8613/Ch_647",
+        "bg" : "precomputed://https://dlab-colm.neurodata.io/2022_01_14/8613/Ch_561",
+        "endo" : "precomputed://https://dlab-colm.neurodata.io/2022_01_14/8613/Ch_488",
+        "mask" : "precomputed://https://dlab-colm.neurodata.io/2022_01_14/8613/axon_mask",
     }
 }
 
