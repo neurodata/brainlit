1a7a489d08e81fc5732c076740b1e29c42441a4a
Thomas Athey <tom.l.athey@gmail.com>
2022-05-05 10:15:35 -0400

    experiments

--- experiments/deisseroth/soma_identify_soma_regions.py
+++ experiments/deisseroth/soma_identify_soma_regions.py
@@ -11,12 +11,13 @@ div_factor = [8, 8, 1]
 atlas_vol = CloudVolume(
     "precomputed://https://dlab-colm.neurodata.io/2022_03_15/8606/atlas_to_target",
     parallel=1,
-    mip=0,
+    mip=0, 
     fill_missing=True,
 )
 print(f"size: {atlas_vol.shape} ")
 
 somas = "/data/tathey1/matt_wright/brainr_results/"
+outpath = "/home/user/misc_tommy/soma_counts_brain" + brain + ".pickle"
 
 coords = []
 if somas[:-4] == ".txt":
@@ -45,11 +46,18 @@ else:
 
                 coord = [int(round(float(e.strip()) / f)) for e, f in zip(coord, div_factor)]
                 coords.append(coord)
+    fname = somas + "all_somas.txt"
+    with open(fname, "w") as f:
+        for coord in coords:
+            f.write(str(coord))
+            f.write("\n")
+
 print(f"{len(coords)} somas detected, first is: {coords[0]}")
 
 
+
 dict = {}
-for coord in tqdm(coords, desc="identiifynig rois"):
+for coord in tqdm(coords, desc="identifynig rois"):
     roi = int(np.squeeze(atlas_vol[coord[0], coord[1], coord[2]]))
     if roi not in dict.keys():
         dict[roi] = 1
@@ -58,6 +66,6 @@ for coord in tqdm(coords, desc="identiifynig rois"):
 
 print(dict)
 with open(
-    "/home/user/misc_tommy/soma_counts_brain" + brain + ".pickle", "wb"
+    outpath, "wb"
 ) as handle:
     pickle.dump(dict, handle)
