0ac085709e3901602b320a678fe084da268ba881
shreyasingh1 <ssingh93@jhu.edu>
2022-05-10 03:50:51 -0400

    labeling bug

--- brainlit/utils/cnn_segmentation/__init__.py
+++ brainlit/utils/cnn_segmentation/__init__.py
@@ -1,4 +1,4 @@
 import brainlit.utils.cnn_segmentation
 
 from brainlit.utils.cnn_segmentation.preprocess import *
-from brainlit.utils.cnn_segmentation.performance import *
+from brainlit.utils.cnn_segmentation.performance_cnn import *
