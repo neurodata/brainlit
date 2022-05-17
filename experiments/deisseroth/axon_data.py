db85008dfbec618f38756fc9a3bb9aa652904839
Thomas Athey <tom.l.athey@gmail.com>
2022-05-17 10:03:26 -0400

    8650

--- experiments/deisseroth/axon_data.py
+++ experiments/deisseroth/axon_data.py
@@ -32,6 +32,13 @@ brain2paths = {
         "endo": "precomputed://s3://smartspim-precomputed-volumes/2022_02_02/8604/Ch_488",
         "mask": "precomputed://s3://smartspim-precomputed-volumes/2022_02_02/8604/axon_mask",
         "val_info": {"url": "https://viz.neurodata.io/?json_url=https://json.neurodata.io/v1?NGStateID=K1JfdZmA4pqr1Q", "layer": "axon_val"}
+    },
+    "8650": {
+        "ab": "precomputed://s3://smartspim-precomputed-volumes/2022_01_21/8650/Ch_647",
+        "bg": "precomputed://s3://smartspim-precomputed-volumes/2022_01_21/8650/Ch_561",
+        "endo": "precomputed://s3://smartspim-precomputed-volumes/2022_01_21/8650/Ch_488",
+        "mask": "precomputed://s3://smartspim-precomputed-volumes/2022_01_21/8650/axon_mask",
+        "val_info": {"url": "https://viz.neurodata.io/?json_url=https://json.neurodata.io/v1?NGStateID=tnXt-hF7Uyuq-w", "layer": "val"}
     }
 }
 
--- experiments/deisseroth/axon_loop_vols.py
+++ experiments/deisseroth/axon_loop_vols.py
@@ -16,8 +16,8 @@ import os
 # dir_base = "s3://smartspim-precomputed-volumes/2021_07_15_Sert_Cre_R/"
 # threshold = 0.6
 
-dir_base = "precomputed://s3://smartspim-precomputed-volumes/2022_02_02/8604/"
-threshold = 0.38
+dir_base = "precomputed://s3://smartspim-precomputed-volumes/2022_01_21/8650/"
+threshold = 0.64
 
 chunk_size = [256, 256, 300]
 
