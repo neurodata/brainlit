df0ab8ab3d8d3c15cf74d9f9b3276cea2c0caaf4
Thomas Athey <tom.l.athey@gmail.com>
2022-05-23 21:26:41 -0400

    identity map

--- brainlit/map_neurons/map_neurons.py
+++ brainlit/map_neurons/map_neurons.py
@@ -1,15 +1,51 @@
 from brainlit.algorithms.trace_analysis.fit_spline import GeometricGraph
 import typing
 import numpy as np
+import copy
+import networkx as nx
+from scipy.interpolate import BSpline, splev, CubicHermiteSpline
 
-class Diffeomorphism():
+class DiffeomorphismAction():
     def __init__(self):
         pass 
 
-    def evaluate(position):
-        return position
+    def evaluate(self, position):
+        transformed_position = np.matmul(np.eye(3)*2, position.T).T
+        return transformed_position
     
-    def D(position, order: int = 1):
-        return np.eye(3)
+    def D(self, position, deriv, order: int = 1):
+        transformed_deriv = np.matmul(np.eye(3)*2, deriv.T).T
+        return transformed_deriv
 
-def transform_GeometricGraph(G: GeometricGraph, Phi: Diffeomorphism):
+def transform_GeometricGraph(G: GeometricGraph, Phi: DiffeomorphismAction):
+    if G.spline_type is not BSpline:
+        raise NotImplementedError("Can only transform bsplines")
+    
+    if not G.spline_tree:
+        G.fit_spline_tree_invariant()
+    G_tranformed = copy.copy(G)
+
+    spline_tree = G_tranformed.spline_tree
+
+
+    # process in reverse dfs order to ensure parents are processed after
+    reverse_dfs = list(reversed(list(nx.topological_sort(spline_tree))))
+
+    for node in reverse_dfs:
+        path = spline_tree.nodes[node]["path"]
+        tck, us = spline_tree.nodes[node]["spline"]
+        positions = np.array(splev(us, tck, der = 0)).T
+        transformed_positions = Phi.evaluate(positions)
+        derivs = np.array(splev(us, tck, der = 1)).T
+        transformed_derivs = Phi.D(positions, derivs, order=1)
+
+        chspline = CubicHermiteSpline(us, transformed_positions, transformed_derivs)
+
+        spline_tree.nodes[node]["spline"] = chspline
+
+        for trace_node, position, deriv in zip(path, transformed_positions, transformed_derivs):
+            G_tranformed.nodes[trace_node]["loc"] = position
+            G_tranformed.nodes[trace_node]["deriv"] = deriv
+
+    return G_tranformed
+
