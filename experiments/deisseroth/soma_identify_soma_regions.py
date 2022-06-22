ac1a1768df4513041e88c53325dddf3d8415b1a6
Thomas Athey <tom.l.athey@gmail.com>
2022-06-22 15:23:32 -0400

    identify

--- experiments/deisseroth/soma_identify_soma_regions.py
+++ experiments/deisseroth/soma_identify_soma_regions.py
@@ -8,7 +8,7 @@ from cloudreg.scripts.transform_points import NGLink
 from cloudreg.scripts.visualization import create_viz_link_from_json
 import random
 
-viz_link = "https://viz.neurodata.io/?json_url=https://json.neurodata.io/v1?NGStateID=my8twEEDBrE0DQ"
+viz_link = "https://viz.neurodata.io/?json_url=https://json.neurodata.io/v1?NGStateID=yBhjHFE3Q43pdg"
 viz_link = NGLink(viz_link.split("json_url=")[-1])
 ngl_json = viz_link._json
 
