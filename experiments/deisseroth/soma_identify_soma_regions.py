de99dbd7fd30a9a699bad72218fe1532b6c25e4f
Thomas Athey <tom.l.athey@gmail.com>
2022-08-05 08:35:17 -0400

    new link

--- experiments/deisseroth/soma_identify_soma_regions.py
+++ experiments/deisseroth/soma_identify_soma_regions.py
@@ -9,10 +9,10 @@ from cloudreg.scripts.visualization import create_viz_link_from_json
 import random
 
 
-somas = "/data/tathey1/matt_wright/8446_brainr_results/"
+somas = "/data/tathey1/matt_wright/brainr_results/"
 
 #viz link that includes atlas_to_target
-viz_link = "https://viz.neurodata.io/?json_url=https://json.neurodata.io/v1?NGStateID=I_qmEGW1kylegw"
+viz_link = "https://viz.neurodata.io/?json_url=https://json.neurodata.io/v1?NGStateID=7g3CvrESEx1TsA"
 viz_link = NGLink(viz_link.split("json_url=")[-1])
 ngl_json = viz_link._json
 
