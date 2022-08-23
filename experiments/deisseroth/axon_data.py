8d0e939936093eec01e905a0a81a2eb7dad60768
Thomas Athey <tom.l.athey@gmail.com>
2022-08-23 16:51:04 -0400

    uopdates

--- experiments/deisseroth/axon_data.py
+++ experiments/deisseroth/axon_data.py
@@ -77,11 +77,11 @@ brain2paths = {
         "mask": "precomputed://s3://smartspim-precomputed-volumes/2022_03_28/8649/axon_mask",
         "transformed_mask": "precomputed://s3://smartspim-precomputed-volumes/2022_03_28/8649/axon_mask_transformed",
         "val_info": {
-            "url": "https://viz.neurodata.io/?json_url=https://json.neurodata.io/v1?NGStateID=yQkGs4XpCLUNqQ",
+            "url": "https://viz.neurodata.io/?json_url=https://json.neurodata.io/v1?NGStateID=_hHz87IjgEa4JQ",
             "layer": "val",
         },
         "train_info": {
-            "url": "https://viz.neurodata.io/?json_url=https://json.neurodata.io/v1?NGStateID=yQkGs4XpCLUNqQ",
+            "url": "https://viz.neurodata.io/?json_url=https://json.neurodata.io/v1?NGStateID=_hHz87IjgEa4JQ",
             "layer": "train",
         },
     },
