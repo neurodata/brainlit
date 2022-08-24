9c3e9c0b02ec4ae718aebc39f3707bcc3b90e7d6
Thomas Athey <tom.l.athey@gmail.com>
2022-08-24 17:00:31 -0400

    update

--- experiments/sriram/read_data.py
+++ experiments/sriram/read_data.py
@@ -2,6 +2,7 @@ import aicspylibczi
 from skimage import io
 import numpy as np
 import zarr
+from tqdm import tqdm
 
 sz = [2, 6814, 8448, 316]
 
@@ -11,7 +12,7 @@ czi = aicspylibczi.CziFile(path)
 print(f"Creating array of shape {sz} from czi file of shape {czi.size}")
 
 zarra = zarr.zeros(sz, chunks=(2,100,100,40), dtype='uint16')
-for z in range(czi.get_dims_shape()[0]['Z'][0]):
+for z in tqdm(range(czi.get_dims_shape()[0]['Z'][0]), desc="Saving slices..."):
     zarra[0,:,:,z] = np.squeeze(czi.read_mosaic(C=0, Z=Z, scale_factor=1))
     zarra[1,:,:,z] = np.squeeze(czi.read_mosaic(C=1, Z=Z, scale_factor=1))
 
