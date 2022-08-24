bfd2122cea5cf3a88b8f85307a41f18d3f8b93de
Thomas Athey <tom.l.athey@gmail.com>
2022-08-24 16:57:09 -0400

    update

--- experiments/sriram/read_data.py
+++ experiments/sriram/read_data.py
@@ -1,15 +1,19 @@
 import aicspylibczi
 from skimage import io
+import numpy as np
+import zarr
 
 path = '/cis/project/sriram/Sriram/SS IUE 175 SNOVA RFP single channel AdipoClear Brain 3 ipsilateral small z two colour Image1.czi'
 czi = aicspylibczi.CziFile(path)
-shape = czi.get_dims_shape()[0]
-print(shape)
+sz = np.squeeze(czi.size)
+print(sz)
+raise ValueError()
 
-Z = int(shape['Z'][0]/2)
-im0 = czi.read_mosaic(C=0, Z=Z, scale_factor=1)
-im1 = czi.read_mosaic(C=1, Z=Z, scale_factor=1)
+zarra = zarr.zeros(sz, chunks=(2,100,100,40), dtype='uint16')
+for z in range(czi.get_dims_shape()[0]['Z'][0]):
+    zarra[0,:,:,z] = np.squeeze(czi.read_mosaic(C=0, Z=Z, scale_factor=1))
+    zarra[1,:,:,z] = np.squeeze(czi.read_mosaic(C=1, Z=Z, scale_factor=1))
+
 
 outpath = "/cis/home/tathey/projects/mouselight/sriram/"
-io.imsave(outpath + "im0.tif", im0)
-io.imsave(outpath + "im1.tif", im1)
+zarr.save(outpath + "somez.zarr", zarra)
