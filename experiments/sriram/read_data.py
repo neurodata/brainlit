b742e89db4f04e41cea198ad17ceab142bc8a26e
Thomas Athey <tom.l.athey@gmail.com>
2022-08-24 17:01:22 -0400

    update

--- experiments/sriram/read_data.py
+++ experiments/sriram/read_data.py
@@ -12,9 +12,10 @@ czi = aicspylibczi.CziFile(path)
 print(f"Creating array of shape {sz} from czi file of shape {czi.size}")
 
 zarra = zarr.zeros(sz, chunks=(2,100,100,40), dtype='uint16')
-for z in tqdm(range(czi.get_dims_shape()[0]['Z'][0]), desc="Saving slices..."):
-    zarra[0,:,:,z] = np.squeeze(czi.read_mosaic(C=0, Z=Z, scale_factor=1))
-    zarra[1,:,:,z] = np.squeeze(czi.read_mosaic(C=1, Z=Z, scale_factor=1))
+num_slices = czi.get_dims_shape()[0]['Z'][0]
+for z in tqdm(range(num_slices), desc="Saving slices..."):
+    zarra[0,:,:,z] = np.squeeze(czi.read_mosaic(C=0, Z=z, scale_factor=1))
+    zarra[1,:,:,z] = np.squeeze(czi.read_mosaic(C=1, Z=z, scale_factor=1))
 
 
 outpath = "/cis/home/tathey/projects/mouselight/sriram/"
