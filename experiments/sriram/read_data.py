2658a877ae2d018629e8b1bfbd49e6d847ac299a
Thomas Athey <tom.l.athey@gmail.com>
2022-08-25 14:33:08 -0400

    write easy volume

--- experiments/sriram/read_data.py
+++ experiments/sriram/read_data.py
@@ -54,6 +54,8 @@ if task == "writeng":
 
         num_slices = czi.get_dims_shape()[0]['Z'][1]
         for z in tqdm(np.arange(num_slices), desc="Saving slices..."):
-            vol[:,:,z] = np.squeeze(czi.read_mosaic(C=0, Z=z, scale_factor=1))
+            im_slice = np.squeeze(czi.read_mosaic(C=0, Z=z, scale_factor=1))
+            print(im_slice.shape)
+            vol[:,:,z] = im_slice
 
 
