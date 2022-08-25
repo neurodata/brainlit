32393cb6065b202494fb26f44b654bc6e04d1434
Thomas Athey <tom.l.athey@gmail.com>
2022-08-25 14:52:19 -0400

    write easy volume

--- experiments/sriram/read_data.py
+++ experiments/sriram/read_data.py
@@ -29,12 +29,11 @@ if task == "writezarr":
 if task == "writeng":
     outpath_prefix = "precomputed://file:///cis/home/tathey/projects/mouselight/sriram/neuroglancer_data/somez/"
 
+    sz = [6814, 8448, 316]
+
     path = '/cis/project/sriram/Sriram/SS IUE 175 SNOVA RFP single channel AdipoClear Brain 3 ipsilateral small z two colour Image1.czi'
     czi = aicspylibczi.CziFile(path)
 
-    dims = czi.shape
-    print(dims)
-    raise ValueError()
 
     for c, suffix in zip([0,1], ["fg", "bg"]):
         outpath = outpath_prefix + suffix
@@ -49,14 +48,14 @@ if task == "writeng":
             # Pick a convenient size for your underlying chunk representation
             # Powers of two are recommended, doesn't need to cover image exactly
             chunk_size      = [ 128, 128, 1 ], # units are voxels
-            volume_size     = [ dims['X'][1], dims['Y'][1], dims['Z'][1]], # e.g. a cubic millimeter dataset
+            volume_size     = sz, # e.g. a cubic millimeter dataset
         )
 
         print(f"Posting info: {info}")
         vol = CloudVolume(outpath, info=info, compress = False)
         vol.commit_info()
 
-        num_slices = dims['Z'][1]
+        num_slices = sz[2]
         for z in tqdm(np.arange(num_slices), desc="Saving slices..."):
             im_slice = np.squeeze(czi.read_mosaic(C=0, Z=z, scale_factor=1))
             im_slice = np.expand_dims(im_slice, axis=2)
