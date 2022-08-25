4ab432ce4afd6b7a5ed280e9beae0cf4ac333f44
Thomas Athey <tom.l.athey@gmail.com>
2022-08-25 15:52:28 -0400

    write easy volume

--- experiments/sriram/read_data.py
+++ experiments/sriram/read_data.py
@@ -5,7 +5,7 @@ import zarr
 from tqdm import tqdm
 from cloudvolume import CloudVolume
 
-task = "writeng"
+task = "readng"
 
 if task == "writezarr":
     sz = [2, 6814, 8448, 316]
@@ -26,7 +26,7 @@ if task == "writezarr":
 
     outpath = "/cis/home/tathey/projects/mouselight/sriram/"
     zarr.save(outpath + "somez.zarr", zarra)
-if task == "writeng":
+elif task == "writeng":
     outpath_prefix = "precomputed://file:///cis/home/tathey/projects/mouselight/sriram/neuroglancer_data/somez/"
 
     sz = [6814, 8448, 316]
@@ -60,5 +60,11 @@ if task == "writeng":
             im_slice = np.squeeze(czi.read_mosaic(C=0, Z=z, scale_factor=1))
             im_slice = np.expand_dims(im_slice, axis=2)
             vol[:,:,z] = im_slice
+elif task == "readng":
+    ng_path = "precomputed://file:///cis/home/tathey/projects/mouselight/sriram/neuroglancer_data/somez/fg"
+    vol = CloudVolume(ng_path, compress = False)
+
+    print(vol.shape)
+    print(vol[500:600,500:600, 150])
 
 
