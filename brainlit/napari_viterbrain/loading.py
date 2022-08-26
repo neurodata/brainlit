0cc66cd0f81052fa04b19c135e19150cbfa8a9f9
sejalsrivastava1 <sejalsrivastava0123@gmail.com>
2022-08-26 10:26:17 -0400

    paths changed

--- brainlit/napari_viterbrain/loading.py
+++ brainlit/napari_viterbrain/loading.py
@@ -11,20 +11,18 @@ scale = [0.3, 0.3, 1]
 
 
 root_dir = Path(os.path.abspath(""))
-data_dir = os.path.join(root_dir, "data", "example")
+data_dir = os.path.join(root_dir, "data", "sample")
 path = os.path.join(data_dir, "3-1-soma_viterbrain.pickle")
 
 with open(path, "rb") as handle:
     viterbi = pickle.load(handle)
 
-im = zarr.open(
-    "/Users/sejalsrivastava/brainlit/experiments/ViterBrain/data/example/3-1-soma.zarr",
-    "r",
-)
-labels = zarr.open(
-    "/Users/sejalsrivastava/brainlit/experiments/ViterBrain/data/example/3-1-soma_labels.zarr",
-    "r",
-)
+path = os.path.join(data_dir, "3-1-soma.zarr")
+im = zarr.open(path, "r")
+
+path = os.path.join(data_dir, "3-1-soma_labels.zarr")
+labels = zarr.open(path, "r")
+
 viewer = napari.Viewer(ndisplay=3)
 viewer.add_image(im, name="image", scale=scale)
 labels_layer = viewer.add_labels(labels, name="labels", scale=scale)
