888e84e9498535c9f272afa32e49108651a3cec2
Thomas Athey <tom.l.athey@gmail.com>
2022-09-29 11:32:43 -0400

    ng ready for big vol

--- experiments/sriram/ng.py
+++ experiments/sriram/ng.py
@@ -1,7 +1,7 @@
-'''
+"""
 copied from https://github.com/google/neuroglancer/blob/master/python/examples/example_action.py
 need to run in interactive mode: python -i ng.py
-'''
+"""
 
 from __future__ import print_function
 
@@ -17,6 +17,7 @@ from cloudvolume import CloudVolume
 
 # python cors_webserver.py -d "/Users/thomasathey/Documents/mimlab/mouselight/brainlit_parent/brainlit/experiments/sriram/sample/ng" -p 9010
 
+
 class ViterBrainViewer(neuroglancer.Viewer):
     def __init__(self, im_url, frag_url, vb_path):
         super().__init__()
@@ -30,28 +31,30 @@ class ViterBrainViewer(neuroglancer.Viewer):
 
         # add layers
         with self.txn() as s:
-            s.layers['image'] = neuroglancer.ImageLayer(
+            s.layers["image"] = neuroglancer.ImageLayer(
                 source=im_url,
             )
-            cv_dict['image'] = CloudVolume(im_url)
-            s.layers['fragments'] = neuroglancer.SegmentationLayer(
+            cv_dict["image"] = CloudVolume(im_url)
+            s.layers["fragments"] = neuroglancer.SegmentationLayer(
                 source=frag_url,
             )
-            cv_dict['fragments'] = CloudVolume(frag_url)
+            cv_dict["fragments"] = CloudVolume(frag_url)
 
         # add keyboard actions
-        self.actions.add('s_select', self.s_select)
-        self.actions.add('t_trace', self.t_trace)
-        self.actions.add('n_newtrace', self.n_newtrace)
-        self.actions.add('c_clearlast', self.c_clearlast)
-        self.actions.add('p_print', self.p_print)
+        self.actions.add("s_select", self.s_select)
+        self.actions.add("t_trace", self.t_trace)
+        self.actions.add("n_newtrace", self.n_newtrace)
+        self.actions.add("c_clearlast", self.c_clearlast)
+        self.actions.add("p_print", self.p_print)
+        self.actions.add("v_cv", self.v_cv)
         with self.config_state.txn() as s:
-            s.input_event_bindings.viewer['shift+keys'] = 's_select'
-            s.input_event_bindings.viewer['shift+keyt'] = 't_trace'
-            s.input_event_bindings.viewer['shift+keyn'] = 'n_newtrace'
-            s.input_event_bindings.viewer['shift+keyc'] = 'c_clearlast'
-            s.input_event_bindings.viewer['shift+keyp'] = 'p_print'
-            s.status_messages['hello'] = 'Welcome to the ViterBrain tracer'
+            s.input_event_bindings.viewer["shift+keys"] = "s_select"
+            s.input_event_bindings.viewer["shift+keyt"] = "t_trace"
+            s.input_event_bindings.viewer["shift+keyn"] = "n_newtrace"
+            s.input_event_bindings.viewer["shift+keyc"] = "c_clearlast"
+            s.input_event_bindings.viewer["shift+keyp"] = "p_print"
+            s.input_event_bindings.viewer["shift+keyv"] = "v_cv"
+            s.status_messages["hello"] = "Welcome to the ViterBrain tracer"
 
         # open vb object
         with open(vb_path, "rb") as handle:
@@ -62,11 +65,9 @@ class ViterBrainViewer(neuroglancer.Viewer):
         self.start_pt = None
         self.end_pt = None
         self.dimensions = neuroglancer.CoordinateSpace(
-            names=['x','y','z'],
-            units='nm',
-            scales=cv_dict['image'].resolution
+            names=["x", "y", "z"], units="nm", scales=cv_dict["image"].resolution
         )
-        self.im_shape = [i for i in cv_dict['image'].shape if i != 1]
+        self.im_shape = [i for i in cv_dict["image"].shape if i != 1]
         self.cur_skel = 0
         self.cur_skel_coords = []
 
@@ -78,48 +79,48 @@ class ViterBrainViewer(neuroglancer.Viewer):
             color (str): hex code of point color.
             coord (list): coordinate of point in voxel units.
         """
-        with self.txn() as vs: #add point
-            vs.layers.append(name=name,
+        with self.txn() as vs:  # add point
+            vs.layers.append(
+                name=name,
                 layer=neuroglancer.LocalAnnotationLayer(
                     dimensions=vs.dimensions,
                     annotation_properties=[
                         neuroglancer.AnnotationPropertySpec(
-                            id='color',
-                            type='rgb',
-                            default='red',
+                            id="color",
+                            type="rgb",
+                            default="red",
                         ),
                         neuroglancer.AnnotationPropertySpec(
-                            id='size',
-                            type='float32',
+                            id="size",
+                            type="float32",
                             default=10,
                         ),
                         neuroglancer.AnnotationPropertySpec(
-                            id='p_int8',
-                            type='int8',
+                            id="p_int8",
+                            type="int8",
                             default=10,
                         ),
                         neuroglancer.AnnotationPropertySpec(
-                            id='p_uint8',
-                            type='uint8',
+                            id="p_uint8",
+                            type="uint8",
                             default=10,
                         ),
                     ],
                     annotations=[
                         neuroglancer.PointAnnotation(
-                            id='1',
+                            id="1",
                             point=coord,
                             props=[color, 5, 6, 7],
                         ),
                     ],
-                    shader='''
+                    shader="""
                             void main() {
                             setColor(prop_color());
                             setPointMarkerSize(prop_size());
                             }
-                            ''',
+                            """,
                 ),
             )
-
 
     def s_select(self, s):
         """Action to add point to neuroglancer
@@ -127,9 +128,9 @@ class ViterBrainViewer(neuroglancer.Viewer):
         Args:
             s (??): action that selects point for tracing.
         """
-        with self.txn() as vs: #add point
+        with self.txn() as vs:  # add point
             layer_names = [l.name for l in vs.layers]
-            print('Selected fragment: %s' % (s.selected_values, ))
+            print("Selected fragment: %s" % (s.selected_values,))
             if "start" in layer_names:
                 if "end" in layer_names:
                     del vs.layers["end"]
@@ -138,43 +139,42 @@ class ViterBrainViewer(neuroglancer.Viewer):
                 self.end_pt = [int(p) for p in s.mouse_voxel_coordinates]
             else:
                 name = "start"
-                color = '#0f0'
+                color = "#0f0"
                 self.start_pt = [int(p) for p in s.mouse_voxel_coordinates]
         self.add_point(name, color, s.mouse_voxel_coordinates)
-            
 
     def t_trace(self, s):
-        with self.txn() as vs: #trace
+        with self.txn() as vs:  # trace
             layer_names = [l.name for l in vs.layers]
             if "start" in layer_names and "end" in layer_names:
                 print(f"Tracing path from {self.start_pt} to {self.end_pt}")
-                path = self.vb.shortest_path(coord1 = self.start_pt, coord2 = self.end_pt)
+                path = self.vb.shortest_path(coord1=self.start_pt, coord2=self.end_pt)
                 self.cur_skel_coords.append(path)
                 trace_layer_name = f"vb_traces_{self.cur_skel}"
                 if trace_layer_name in layer_names:
                     del vs.layers[trace_layer_name]
 
-                coords_list = [inner for outer in self.cur_skel_coords for inner in outer]
+                coords_list = [
+                    inner for outer in self.cur_skel_coords for inner in outer
+                ]
                 skel_source = self.SkeletonSource(self.dimensions)
                 skel_source.add_skeleton(coords_list=coords_list)
                 vs.layers.append(
                     name=trace_layer_name,
-                    layer = neuroglancer.SegmentationLayer(
-                        source = [
+                    layer=neuroglancer.SegmentationLayer(
+                        source=[
                             neuroglancer.LocalVolume(
-                                data = np.zeros(self.im_shape, dtype=np.uint32),
-                                dimensions = self.dimensions
+                                data=np.zeros(self.im_shape, dtype=np.uint32),
+                                dimensions=self.dimensions,
                             ),
-                            skel_source
+                            skel_source,
                         ],
-                        segments = [0]
-                    )
+                        segments=[0],
+                    ),
                 )
-            
-               
 
-                del vs.layers['start']
-                vs.layers['end'].name = 'start'
+                del vs.layers["start"]
+                vs.layers["end"].name = "start"
                 self.start_pt = self.end_pt
             else:
                 print("No start/end layers yet")
@@ -185,7 +185,7 @@ class ViterBrainViewer(neuroglancer.Viewer):
         else:
             self.cur_skel_coords = self.cur_skel_coords[:-1]
 
-            with self.txn() as vs: 
+            with self.txn() as vs:
                 layer_names = [l.name for l in vs.layers]
                 trace_layer_name = f"vb_traces_{self.cur_skel}"
                 if trace_layer_name in layer_names:
@@ -198,32 +198,32 @@ class ViterBrainViewer(neuroglancer.Viewer):
             if len(self.cur_skel_coords) > 0:
                 coord = self.cur_skel_coords[-1][-1]
                 self.start_pt = coord
-                self.add_point("start", "#f00", coord) 
+                self.add_point("start", "#f00", coord)
 
-                coords_list = [inner for outer in self.cur_skel_coords for inner in outer]
+                coords_list = [
+                    inner for outer in self.cur_skel_coords for inner in outer
+                ]
                 skel_source = self.SkeletonSource(self.dimensions)
                 skel_source.add_skeleton(coords_list=coords_list)
 
-                with self.txn() as vs: 
+                with self.txn() as vs:
                     vs.layers.append(
                         name=trace_layer_name,
-                        layer = neuroglancer.SegmentationLayer(
-                            source = [
+                        layer=neuroglancer.SegmentationLayer(
+                            source=[
                                 neuroglancer.LocalVolume(
-                                    data = np.zeros(self.im_shape, dtype=np.uint32),
-                                    dimensions = self.dimensions
+                                    data=np.zeros(self.im_shape, dtype=np.uint32),
+                                    dimensions=self.dimensions,
                                 ),
-                                skel_source
+                                skel_source,
                             ],
-                            segments = [0]
-                        )
+                            segments=[0],
+                        ),
                     )
-
-
 
     def n_newtrace(self, s):
         print(f"Creating new trace #{self.cur_skel+1}")
-        with self.txn() as vs: #trace
+        with self.txn() as vs:  # trace
             layer_names = [l.name for l in vs.layers]
             if "start" in layer_names:
                 del vs.layers["start"]
@@ -237,7 +237,6 @@ class ViterBrainViewer(neuroglancer.Viewer):
         print(f"Printing trace info for vb_trace_{self.cur_skel}:")
         coords_list = [inner for outer in self.cur_skel_coords for inner in outer]
         print(coords_list)
-        
 
     class SkeletonSource(neuroglancer.skeleton.SkeletonSource):
         """Source used to serve skeleton objects generated by ViterBrain tracing.
@@ -245,21 +244,27 @@ class ViterBrainViewer(neuroglancer.Viewer):
         Args:
             neuroglancer (_type_): _description_
         """
+
         def __init__(self, dimensions):
             super().__init__(dimensions)
             self.skels = []
 
         def add_skeleton(self, coords_list):
-            edges = [[i-1,i] for i in range(1, len(coords_list))]
-            self.skels.append(neuroglancer.skeleton.Skeleton(vertex_positions=coords_list,
-                edges = edges))
+            edges = [[i - 1, i] for i in range(1, len(coords_list))]
+            self.skels.append(
+                neuroglancer.skeleton.Skeleton(
+                    vertex_positions=coords_list, edges=edges
+                )
+            )
 
         def get_skeleton(self, i):
             return self.skels[i]
 
 
-
-
-vbviewer = ViterBrainViewer(im_url='precomputed://http://127.0.0.1:9010/im', frag_url='precomputed://http://127.0.0.1:9010/frags', vb_path="/Users/thomasathey/Documents/mimlab/mouselight/brainlit_parent/brainlit/experiments/sriram/sample/3-1-soma_viterbrain.pickle")
+vbviewer = ViterBrainViewer(
+    im_url="precomputed://http://127.0.0.1:9010/im",
+    frag_url="precomputed://http://127.0.0.1:9010/frags",
+    vb_path="/Users/thomasathey/Documents/mimlab/mouselight/brainlit_parent/brainlit/experiments/sriram/sample/3-1-soma_viterbrain.pickle",
+)
 print(vbviewer)
 webbrowser.open_new(vbviewer.get_viewer_url())
--- experiments/sriram/read_data.py
+++ experiments/sriram/read_data.py
@@ -14,19 +14,17 @@ task = "stategen"
 if task == "writezarr":
     sz = [2, 6814, 8448, 316]
 
-    path = '/cis/project/sriram/Sriram/SS IUE 175 SNOVA RFP single channel AdipoClear Brain 3 ipsilateral small z two colour Image1.czi'
+    path = "/cis/project/sriram/Sriram/SS IUE 175 SNOVA RFP single channel AdipoClear Brain 3 ipsilateral small z two colour Image1.czi"
     czi = aicspylibczi.CziFile(path)
 
     print(f"Creating array of shape {sz} from czi file of shape {czi.get_dims_shape()}")
 
-    zarra = zarr.zeros(sz, chunks=(2,100,100,40), dtype='uint16')
-    num_slices = czi.get_dims_shape()[0]['Z'][1]
-
+    zarra = zarr.zeros(sz, chunks=(2, 100, 100, 40), dtype="uint16")
+    num_slices = czi.get_dims_shape()[0]["Z"][1]
 
     for z in tqdm(np.arange(num_slices), desc="Saving slices..."):
-        zarra[0,:,:,z] = np.squeeze(czi.read_mosaic(C=0, Z=z, scale_factor=1))
-        zarra[1,:,:,z] = np.squeeze(czi.read_mosaic(C=1, Z=z, scale_factor=1))
-
+        zarra[0, :, :, z] = np.squeeze(czi.read_mosaic(C=0, Z=z, scale_factor=1))
+        zarra[1, :, :, z] = np.squeeze(czi.read_mosaic(C=1, Z=z, scale_factor=1))
 
     outpath = "/cis/home/tathey/projects/mouselight/sriram/"
     zarr.save(outpath + "somez.zarr", zarra)
@@ -35,35 +33,34 @@ elif task == "writeng":
 
     sz = [6814, 8448, 316]
 
-    path = '/cis/project/sriram/Sriram/SS IUE 175 SNOVA RFP single channel AdipoClear Brain 3 ipsilateral small z two colour Image1.czi'
+    path = "/cis/project/sriram/Sriram/SS IUE 175 SNOVA RFP single channel AdipoClear Brain 3 ipsilateral small z two colour Image1.czi"
     czi = aicspylibczi.CziFile(path)
 
-
-    for c, suffix in zip([0,1], ["fg", "bg"]):
+    for c, suffix in zip([0, 1], ["fg", "bg"]):
         outpath = outpath_prefix + suffix
         info = CloudVolume.create_new_info(
-            num_channels    = 1,
-            layer_type      = 'image',
-            data_type       = 'uint16', # Channel images might be 'uint8'
+            num_channels=1,
+            layer_type="image",
+            data_type="uint16",  # Channel images might be 'uint8'
             # raw, png, jpeg, compressed_segmentation, fpzip, kempressed, zfpc, compresso
-            encoding        = 'raw', 
-            resolution      = [500, 500, 3000], # Voxel scaling, units are in nanometers
-            voxel_offset    = [0, 0, 0], # x,y,z offset in voxels from the origin
+            encoding="raw",
+            resolution=[500, 500, 3000],  # Voxel scaling, units are in nanometers
+            voxel_offset=[0, 0, 0],  # x,y,z offset in voxels from the origin
             # Pick a convenient size for your underlying chunk representation
             # Powers of two are recommended, doesn't need to cover image exactly
-            chunk_size      = [ 128, 128, 1 ], # units are voxels
-            volume_size     = sz, # e.g. a cubic millimeter dataset
+            chunk_size=[128, 128, 1],  # units are voxels
+            volume_size=sz,  # e.g. a cubic millimeter dataset
         )
 
         print(f"Posting info: {info} to {outpath}")
-        vol = CloudVolume(outpath, info=info, compress = False)
+        vol = CloudVolume(outpath, info=info, compress=False)
         vol.commit_info()
 
         num_slices = sz[2]
         for z in tqdm(np.arange(num_slices), desc="Saving slices..."):
             im_slice = np.squeeze(czi.read_mosaic(C=c, Z=z, scale_factor=1))
             im_slice = np.expand_dims(im_slice, axis=2)
-            vol[:,:,z] = im_slice
+            vol[:, :, z] = im_slice
 
     layer_path_prefix = "precomputed://file:///cis/home/tathey/projects/mouselight/sriram/neuroglancer_data/somez/"
     for suffix in ["fg", "bg"]:
@@ -93,35 +90,48 @@ elif task == "writeng":
         tq.execute()
 elif task == "readng":
     ng_path = "precomputed://file:///cis/home/tathey/projects/mouselight/sriram/neuroglancer_data/somez/fg"
-    vol = CloudVolume(ng_path, compress = False)
+    vol = CloudVolume(ng_path, compress=False)
 
     print(vol.shape)
-    print(vol[500:600,500:600, 150])
+    print(vol[500:600, 500:600, 150])
 elif task == "saveilastik":
     z = zarr.open("/cis/home/tathey/projects/mouselight/sriram/somez.zarr")
-    corners = [[4000, 2100, 150], [3000, 2500, 150], [5500, 1000, 100], [2000, 6000, 160], [4000, 7000, 200]]
+    corners = [
+        [4000, 2100, 150],
+        [3000, 2500, 150],
+        [5500, 1000, 100],
+        [2000, 6000, 160],
+        [4000, 7000, 200],
+    ]
     for i, corner in enumerate(tqdm(corners, desc="saving samples...")):
-        im = z[:,corner[0]:corner[0]+220, corner[1]:corner[1]+220,corner[2]:corner[2]+20]
+        im = z[
+            :,
+            corner[0] : corner[0] + 220,
+            corner[1] : corner[1] + 220,
+            corner[2] : corner[2] + 20,
+        ]
 
         fname = f"/cis/home/tathey/projects/mouselight/sriram/ilastik_training/somez_training_{corner[0]}_{corner[1]}_{corner[2]}_.h5"
         with h5py.File(fname, "w") as f:
             dset = f.create_dataset("image_2channel", data=im)
 elif task == "stategen":
-    sg = state_generation(image_path="/cis/home/tathey/projects/mouselight/sriram/somez.zarr",
-            ilastik_program_path="/cis/home/tathey/ilastik-1.3.3post3-Linux/run_ilastik.sh",
-            ilastik_project_path="/cis/home/tathey/projects/mouselight/sriram/ilastik_training/ilastik_somez/somez_neuron-2.ilp",
-            fg_channel=0,
-            prob_path="/cis/home/tathey/projects/mouselight/sriram/somez_probs.zarr",
-            fragment_path="/cis/home/tathey/projects/mouselight/sriram/somez_labels.zarr",
-            tiered_path="/cis/home/tathey/projects/mouselight/sriram/somez_tiered.zarr",
-            states_path="/cis/home/tathey/projects/mouselight/sriram/somez_nx.pickle",
-            chunk_size=[2, 300, 300, 160],
-            resolution=[0.5, 0.5, 3],
-            parallel=4)
+    sg = state_generation(
+        image_path="/cis/home/tathey/projects/mouselight/sriram/somez.zarr",
+        ilastik_program_path="/cis/home/tathey/ilastik-1.3.3post3-Linux/run_ilastik.sh",
+        ilastik_project_path="/cis/home/tathey/projects/mouselight/sriram/ilastik_training/ilastik_somez/somez_neuron-2.ilp",
+        fg_channel=0,
+        prob_path="/cis/home/tathey/projects/mouselight/sriram/somez_probs.zarr",
+        fragment_path="/cis/home/tathey/projects/mouselight/sriram/somez_labels.zarr",
+        tiered_path="/cis/home/tathey/projects/mouselight/sriram/somez_tiered.zarr",
+        states_path="/cis/home/tathey/projects/mouselight/sriram/somez_nx.pickle",
+        chunk_size=[2, 300, 300, 160],
+        resolution=[0.5, 0.5, 3],
+        parallel=4,
+    )
 
     # sg.predict(data_bin="/cis/home/tathey/projects/mouselight/sriram/temp/")
-    #sg.compute_frags()
-    #sg.compute_image_tiered()
+    # sg.compute_frags()
+    # sg.compute_image_tiered()
     # sg.compute_soma_lbls()
     # sg.compute_states()
     sg.compute_edge_weights()
@@ -132,28 +142,24 @@ elif task == "fragng":
     frags = zarr.open(frags_path, "r")
 
     info = CloudVolume.create_new_info(
-        num_channels    = 1,
-        layer_type      = 'segmentation',
-        data_type       = 'uint16', # Channel images might be 'uint8'
+        num_channels=1,
+        layer_type="segmentation",
+        data_type="uint16",  # Channel images might be 'uint8'
         # raw, png, jpeg, compressed_segmentation, fpzip, kempressed, zfpc, compresso
-        encoding        = 'raw', 
-        resolution      = [500, 500, 3000], # Voxel scaling, units are in nanometers
-        voxel_offset    = [0, 0, 0], # x,y,z offset in voxels from the origin
+        encoding="raw",
+        resolution=[500, 500, 3000],  # Voxel scaling, units are in nanometers
+        voxel_offset=[0, 0, 0],  # x,y,z offset in voxels from the origin
         # Pick a convenient size for your underlying chunk representation
         # Powers of two are recommended, doesn't need to cover image exactly
-        chunk_size      = [ 128, 128, 1 ], # units are voxels
-        volume_size     = frags.shape, # e.g. a cubic millimeter dataset
+        chunk_size=[128, 128, 1],  # units are voxels
+        volume_size=frags.shape,  # e.g. a cubic millimeter dataset
     )
 
     print(f"Posting info: {info} to {outpath}")
-    vol = CloudVolume(outpath, info=info, compress = False)
+    vol = CloudVolume(outpath, info=info, compress=False)
     vol.commit_info()
 
     for z in tqdm(np.arange(frags.shape[-1]), desc="Writing fragments..."):
-        slice = frags[:,:,z]
+        slice = frags[:, :, z]
         slice = np.expand_dims(slice, axis=2)
-        vol[:,:,z] = slice
-
-
-
-
+        vol[:, :, z] = slice
