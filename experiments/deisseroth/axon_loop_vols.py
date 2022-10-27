f5ab8e29510f0e414f8d41c792365684c5de1430
Thomas Athey <tom.l.athey@gmail.com>
2022-10-27 17:13:02 -0400

    new thresh

--- experiments/deisseroth/axon_loop_vols.py
+++ experiments/deisseroth/axon_loop_vols.py
@@ -2,7 +2,7 @@
 Inputs
 '''
 dir_base = "precomputed://s3://smartspim-precomputed-volumes/2022_10_24/8788/" #s3 path to directory that contains image data
-threshold = 0.42 #threshold to use for ilastik
+threshold = 0.4 #threshold to use for ilastik
 data_dir = "/data/tathey1/matt_wright/brain_temp/" #directory to store temporary subvolumes for segmentation
 
 '''
