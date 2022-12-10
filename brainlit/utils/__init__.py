b39557e1de4c1e35692dff0b792e7524866c4343
Thomas Athey <tom.l.athey@gmail.com>
2022-12-10 11:51:21 -0500

    doc

--- brainlit/utils/__init__.py
+++ brainlit/utils/__init__.py
@@ -2,4 +2,5 @@ from brainlit.utils.session import *
 from brainlit.utils.upload import *
 from brainlit.utils.Neuron_trace import *
 from brainlit.utils.benchmarking_params import *
+from brainlit.utils import write
 from brainlit.utils.write import *
