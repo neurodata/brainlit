bfca1deb0a7f87ecc983ab0fed5a86694283f10b
Thomas Athey <tom.l.athey@gmail.com>
2023-01-02 15:01:33 -0500

    update

--- archive/test_viterbi2.py
+++ archive/test_viterbi2.py
@@ -24,20 +24,13 @@ def test_viterbi2_bad_input():
 
 
 def test_viterbi2_valid_input():
-<<<<<<< HEAD
-    image = np.ones((10, 10, 10))
-=======
     image = np.zeros((10, 10, 10))
->>>>>>> develop
     labels = np.zeros((10, 10, 10), dtype=int)
 
     labels[0, 0, 0] = 1
     labels[9, 9, 9] = 2
-<<<<<<< HEAD
-=======
     image[0, 0, 0] = 1
     image[9, 9, 9] = 1
->>>>>>> develop
 
     mpnp = dynamic_programming_viterbi2.most_probable_neuron_path(image, labels)
 
@@ -45,30 +38,6 @@ def test_viterbi2_valid_input():
 ############################
 ### functionality checks ###
 ############################
-<<<<<<< HEAD
-
-
-def test_viterbi2():
-    image = 0.5 * np.ones((100, 100, 1))
-    image[50:55, 0:25, 0] = 0.91
-    image[50:55, 30:50, 0] = 0.92
-    image[45:50, 55:75, 0] = 0.93
-    image[60:65, 55:75, 0] = 0.94
-    image[45:60, 85:, 0] = 0.95
-
-    labels = np.zeros((100, 100, 1), dtype=int)
-    labels[50:55, 0:25, 0] = 1
-    labels[50:55, 30:50, 0] = 2
-    labels[45:50, 55:75, 0] = 3
-    labels[60:65, 55:75, 0] = 4
-    labels[45:60, 85:, 0] = 5
-
-    axon_coords = [[52, 2, 0]]
-    soma_coords = [[50, 90, 0]]
-
-    res = [0.1, 0.1, 0.1]
-
-=======
 image = 0.5 * np.ones((100, 100, 1))
 image[50:55, 0:25, 0] = 0.91
 image[50:55, 30:50, 0] = 0.92
@@ -112,22 +81,15 @@ def test_viterbi2():
 
 
 def test_viterbi2_parallel():
->>>>>>> develop
     _, axon_lbls = image_process.label_points(labels, axon_coords, res)
     axon_lbl = axon_lbls[0]
     _, soma_lbls = image_process.label_points(labels, soma_coords, res)
     soma_lbl = soma_lbls[0]
 
     mpnp = dynamic_programming_viterbi2.most_probable_neuron_path(
-<<<<<<< HEAD
-        image, labels, [soma_lbl], res
-    )
-    mpnp.frags_to_lines()
-=======
         image, labels, [soma_lbl], res, parallel=2
     )
     mpnp.compute_states()
->>>>>>> develop
     mpnp.compute_all_costs_dist(
         point_point_func=mpnp.point_point_dist, point_blob_func=mpnp.point_blob_dist
     )
