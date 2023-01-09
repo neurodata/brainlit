0a1db28c41e9e7af6f73fd453cfb673256d578e9
Thomas Athey <tom.l.athey@gmail.com>
2023-01-09 14:15:02 -0500

    ready to trace

--- brainlit/utils/tests/test_write.py
+++ brainlit/utils/tests/test_write.py
@@ -1,8 +1,17 @@
 import pytest
 import zarr
 import numpy as np
-from brainlit.utils.write import zarr_to_omezarr
+from brainlit.utils.write import zarr_to_omezarr, czi_to_zarr
 import os
+import zipfile
+
+@pytest.fixture(scope="session")
+def init_4dczi(tmp_path_factory):
+    data_dir = tmp_path_factory.mktemp("data")
+    czi_path = data_dir / "test.czi"
+    with zipfile.ZipFile("/Users/thomasathey/Documents/mimlab/mouselight/brainlit_parent/brainlit/experiments/sriram/data/test.czi.zip","r") as zip_ref:
+        zip_ref.extractall(data_dir)
+    return czi_path, data_dir
 
 
 @pytest.fixture(scope="session")
@@ -50,6 +59,16 @@ def test_writeome_baddim(init_4dzarr):
 ##################
 ### validation ###
 ##################
+
+
+def test_writezarr(init_4dczi):
+    czi_path, data_dir = init_4dczi
+    zarr_paths = czi_to_zarr(czi_path= czi_path, out_dir= str(data_dir))
+
+    assert len(zarr_paths) == 2
+
+    for zarr_path in zarr_paths:
+        zarr.open(zarr_path)
 
 
 def test_writeome(init_3dzarr):
