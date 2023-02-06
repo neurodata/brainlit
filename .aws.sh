9dc92c1f46b599b2eeb311bd45f7b28a96206f3a
Thomas Athey <tom.l.athey@gmail.com>
2023-02-06 10:12:08 -0500

    no credentials on github actions

--- .aws.sh
+++ .aws.sh
@@ -3,9 +3,7 @@ mkdir -p ~/.cloudvolume/secrets
 
 cat > ~/.cloudvolume/secrets/aws-secret.json << EOL
 {
-    "AWS_ACCESS_KEY_ID": "$1",
-    "AWS_SECRET_ACCESS_KEY": "$2"
+    "AWS_ACCESS_KEY_ID": "${AWS_KEY}",
+    "AWS_SECRET_ACCESS_KEY": "${AWS_SEC_KEY}"
 }
 EOL
-
-cat ~/.cloudvolume/secrets/aws-secret.json | cat
