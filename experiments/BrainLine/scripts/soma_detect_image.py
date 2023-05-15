354928c42c12fcc36eaf93b594ff8e7ca03e0dec
Thomas Athey <tom.l.athey@gmail.com>
2023-05-15 15:27:53 -0400

    ready for analysis computer proc

--- experiments/BrainLine/scripts/soma_detect_image.py
+++ experiments/BrainLine/scripts/soma_detect_image.py
@@ -20,7 +20,7 @@ Inputs
 # -dir_base
 # data_dir and results_dir ARE CLEAR
 # threshold IS CORRECT
-brain = "892"
+brain = "MS25"
 antibody_layer = "Ch_647"
 background_layer = "Ch_561"
 endogenous_layer = "Ch_488"
@@ -36,11 +36,19 @@ results_dir = (
 # Ilastik will run in "headless mode", and the following paths are needed to do so:
 ilastik_path = "/Applications/ilastik-1.4.0b21-OSX.app/Contents/ilastik-release/run_ilastik.sh"  # "/data/tathey1/matt_wright/ilastik/ilastik-1.4.0rc5-Linux/run_ilastik.sh"  # path to ilastik executable
 ilastik_project = "/Users/thomasathey/Documents/mimlab/mouselight/ailey/detection_soma/matt_soma_rabies_pix_3ch.ilp"  # "/data/tathey1/matt_wright/ilastik/soma_model/matt_soma_rabies_pix_3ch.ilp"  # path to ilastik project
+ilastik_path = "/home/user/Documents/ilastik-1.4.0-Linux/run_ilastik.sh"  
+ilastik_project = "/home/user/misc_tommy/ilastik/models/soma/matt_soma_rabies_pix_3ch.ilp" 
 
+
+min_coords = [
+    551,
+    1538,
+    -1,
+]  # max coords or -1 if you want to process everything along that dimension
 max_coords = [
-    3072,
-    4352,
-    1792,
+    6515,
+    -1,
+    -1,
 ]  # max coords or -1 if you want to process everything along that dimension
 ncpu = 10  # 16  # number of cores to use for detection
 chunk_size = [256, 256, 256]  # [256, 256, 300]
@@ -63,6 +71,7 @@ alli.apply_ilastik_parallel(
     threshold=threshold,
     data_dir=data_dir,
     chunk_size=chunk_size,
+    min_coords=min_coords,
     max_coords=max_coords,
 )
 alli.collect_soma_results(brain_id=brain)
