675a3fc14b636c7dba7c8e855711234f7aa1c8fe
Thomas Athey <tom.l.athey@gmail.com>
2023-06-07 11:47:37 -0400

    2

--- experiments/BrainLine/scripts/axon_segment_image.py
+++ experiments/BrainLine/scripts/axon_segment_image.py
@@ -47,7 +47,7 @@ max_coords = [
     -1,
     -1,
 ]  # max coords or -1 if you want to process everything along that dimension
-ncpu = 3  # 16  # number of cores to use for detection
+ncpu = 2  # 16  # number of cores to use for detection
 chunk_size = [512, 1024, 2304]  # [256, 256, 300]
 
 
