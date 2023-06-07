e9a8d4a77c3dbab1d4739a646df531a44187b621
Thomas Athey <tom.l.athey@gmail.com>
2023-06-07 10:39:25 -0400

    fix

--- experiments/BrainLine/scripts/axon_segment_image.py
+++ experiments/BrainLine/scripts/axon_segment_image.py
@@ -19,9 +19,9 @@ Inputs
 """
 
 brain = "MS12"
-antibody_layer = "antibody"
-background_layer = "background"
-endogenous_layer = "endogenous"
+antibody_layer = "Ch_647"
+background_layer = "Ch_561"
+endogenous_layer = "Ch_488"
 
 threshold = 0.54  # threshold to use for ilastik
 brainline_exp_dir = Path(os.getcwd()) / Path(__file__).parents[1]
