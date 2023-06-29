116fb7604f5414995b70ee50e5e55667f6d7fa48
Thomas Athey <tom.l.athey@gmail.com>
2023-06-29 12:44:24 -0400

    collect MS10

--- experiments/BrainLine/scripts/axon_collect_results.py
+++ experiments/BrainLine/scripts/axon_collect_results.py
@@ -5,20 +5,21 @@ import os
 """
 Inputs
 """
-brain = "MS12"
+brain = "MS10"
 brainline_exp_dir = Path(os.getcwd()) / Path(__file__).parents[1]
 data_dir = (
     brainline_exp_dir / "data" / "brain_temp"
 )  # data_dir = "/data/tathey1/matt_wright/brain_temp/"  # directory to store temporary subvolumes for segmentation
 data_file = brainline_exp_dir / "data" / "axon_data.json"
 
+
 min_coords = [
     -1,
-    1024,
+    512,
     -1,
 ]  # max coords or -1 if you want to process everything along that dimension
 max_coords = [
-    6144,
+    6656,
     -1,
     -1,
 ]  # max coords or -1 if you want to process everything along that dimension
