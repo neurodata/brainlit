52fa93a507df334b4b1a70898e6a8c777bea571c
Thomas Athey <tom.l.athey@gmail.com>
2023-10-10 15:14:33 -0400

    ready ms22

--- experiments/BrainLine/scripts/soma_detect_image.py
+++ experiments/BrainLine/scripts/soma_detect_image.py
@@ -9,12 +9,12 @@ Inputs
 # -dir_base
 # data_dir and results_dir ARE CLEAR
 # threshold IS CORRECT
-brain = "8557"
+brain = "MS22"
 antibody_layer = "Ch_647"
 background_layer = "Ch_561"
 endogenous_layer = "Ch_488"
 
-threshold = 0.18  # threshold to use for ilastik
+threshold = 0.72  # threshold to use for ilastik
 brainline_exp_dir = Path(os.getcwd()) / Path(__file__).parents[1]
 data_dir = (
     brainline_exp_dir / "data" / "brainr_temp"
@@ -32,12 +32,12 @@ ilastik_project = brainline_exp_dir / "data" / "models" / "soma" / "matt_soma_ra
 
 
 min_coords = [
-    -1,
+    910,
     -1,
     -1,
 ]  # max coords or -1 if you want to process everything along that dimension
 max_coords = [
-    4658,
+    -1,
     -1,
     -1,
 ]  # max coords or -1 if you want to process everything along that dimension
